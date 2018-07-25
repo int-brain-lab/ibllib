65e2b6cfc74ec3a6c0657e8e8e98cd58c673dae9
Niccolò Bonacchi <nbonacchi@gmail.com>
2018-07-25 16:28:03 +0100

    Added alf docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-# FIXME: this won't build anywhere else
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../python/alf'))
+sys.path.insert(0, os.path.abspath('../python/open_neuro_data'))
+sys.path.insert(0, os.path.abspath('../python/ibllib'))
 print('Python %s on %s' % (sys.version, sys.platform))
-sys.path.extend(['/home/owinter/PycharmProjects/IBL_Main/alyx-client',
-                 '/home/owinter/PycharmProjects/IBL_Main/openNeuroData',
-                 '/home/owinter/PycharmProjects/IBL_Main/test_alyx',
-                 '/home/owinter/PycharmProjects/IBL_Main/wip',
-                 '/home/owinter/PycharmProjects/IBL_Main'
-                 ])
+
 
 
 # -- Project information -----------------------------------------------------
