6e32bb4ea82caf07dd9b6ca0492bd6103a0451db
Olivier Winter <olivier.winter@research.fchampalimaud.org>
2018-07-26 11:52:02 +0100

    Include a dummy params_secret.py for autobuild

---
+++ python/openneurodata/oneibl/params_secret.py
@@ -0,0 +1,3 @@
+# TODO: not very happy with this state of affairs of having unencrypted passwords (keyring ?)
+ALYX_PWD = 'ThisIsNotTheProductionPassword!'
+HTTP_DATA_SERVER_PWD = 'ThoseAreOnlyForGitHub!'
