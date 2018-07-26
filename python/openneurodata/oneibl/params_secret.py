aad564fb1ed5bfd64afc3aa0fcacbf57df30672b
Olivier Winter <olivier.winter@research.fchampalimaud.org>
2018-07-26 12:22:58 +0100

    Revert "Merge branch 'feature/dev-o'"
    This reverts commit b8a974e447798abb54e0a027eb217ce452b5d629.

--- python/openneurodata/oneibl/params_secret.py
+++
@@ -1,3 +0,0 @@
-# TODO: not very happy with this state of affairs of having unencrypted passwords (keyring ?)
-ALYX_PWD = 'ThisIsNotTheProductionPassword!'
-HTTP_DATA_SERVER_PWD = 'ThoseAreOnlyForGitHub!'
