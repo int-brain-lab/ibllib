09c00c6b887763ae5779118b8cd7581bf2540c7d
Niccolò Bonacchi <nbonacchi@gmail.com>
2018-07-27 14:47:58 +0100

    added __init__.py to alf lib folder

---
+++ python/alf/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# @Author: Niccolò Bonacchi
+# @Date: Friday, July 27th 2018, 1:54:49 pm
+# @Last Modified by: Niccolò Bonacchi
+# @Last Modified time: 27-07-2018 01:54:49.4949
+import alf.alf_extractors as extractors
+import alf.scraper as scraper
