057018c5c930928a3f276c83682d5db493e1b2e3
Olivier Winter <olivier.winter@research.fchampalimaud.org>
2018-10-04 16:13:06 +0100

    RegisterLegacySession small cosmetic changes

--- matlab/examples/RegisterLegacySessionsRigbox.m
+++ matlab/examples/RegisterLegacySessionsRigbox.m
@@ -3,10 +3,9 @@
 % you can only register from one repo at a time (this is the price of being portable)
 repository_name = 'mainenlab_rig01' ; 
 excluded_subjects = {'default'};
-chem = '/home/owinter/Documents/IBL/CCU_matlab_data/subjects';
-% those passwords have been changed on the database
-% one = One('alyx_login','root','alyx_pwd','Empusa77','alyx_url','https://dev.alyx.internationalbrainlab.org');
-one = One('alyx_login','root','alyx_pwd','Empusa77','alyx_url','http://localhost:8000');
+chem = 'C:\Globus';
+% one = One('alyx_login','root','alyx_pwd','oldpass','alyx_url','https://dev.alyx.internationalbrainlab.org');
+one = One('alyx_login','olivier','alyx_pwd','superpass','alyx_url','https://alyx.internationalbrainlab.org');
 
 %% Gather info about repository
 dr = one.alyx_client.get('/data-repository');
@@ -14,7 +13,7 @@ host_name =  dr.dns{find(strcmp( dr.name, repository_name))};
 % dataset types, prepare the regexp to check files before register
 dtypes = one.alyx_client.get('/dataset-types');
 dtypes = structfun(@(x) x(~cellfun(@isempty, dtypes.filename_pattern)), dtypes, 'UniformOutput', false);
-dtypes.regexp = regexptranslate('wildcard', dtypes.filename_pattern)
+dtypes.regexp = regexptranslate('wildcard', dtypes.filename_pattern);
 
 %% Gather info about Sessions
 % recursive dir to find all Block files
@@ -30,7 +29,7 @@ alfses.sessionnb = cellfun(@(x) x{end  }, alfses.bigsplit, 'UniformOutput', fals
 alfses = structfun(@(x) x(~ismember(alfses.subjects, excluded_subjects)) , alfses, 'UniformOutput', false);
 
 %% The big loop over sessions
-for m = 28:length(alfses.block_files)
+for m = 1:length(alfses.block_files)
     disp(alfses.block_files{m})
     % get or create session
     [sesid, ses] = one.search('subject', alfses.subjects{m}, 'date_range',...
@@ -45,13 +44,14 @@ for m = 28:length(alfses.block_files)
         ses = [];
         ses.subject = alfses.subjects{m};
         ses.procedures = {'Behavior training/tasks'};
-        ses.narrative =  'auto-generated session';
+        ses.narrative =  'auto-generated legacy session';
         ses.start_time =  datestr(d.block.startDateTime,'yyyy-mm-ddTHH:MM:SS');
         ses.end_time =  datestr(d.block.endDateTime,'yyyy-mm-ddTHH:MM:SS');
-        ses.type =  '';
+        ses.type =  'Experiment';
         ses.number =  alfses.sessionnb{m};
         ses.users =  {one.alyx_client.user};
         ses = one.alyx_client.post('/sessions', ses);
+        warning('Create session')
     else
         ses.data_dataset_session_related;
     end
@@ -59,6 +59,13 @@ for m = 28:length(alfses.block_files)
     alfil.full_path = io.dir(alfses.session_dirs{m}, 'pattern', '*.*.*','recursive', true);
     alfil.folders =cellfun(@(x) fileparts(x), alfil.full_path, 'UniformOUtput', false );
     alfil.name = cellfun(@(x) x(find(x==filesep,1,'last')+1:end), alfil.full_path, 'UniformOutput', false);
+    alfil = sel_struct(alfil, cellfun(@(x) sum(x=='.')>=2, alfil.name));
+    % make sure that all files have a pattern that match an existing type. If not warn
+    dtype_recognized = cellfun(@(x) any(~cellfun(@isempty , regexp(x, dtypes.regexp))), alfil.name);
+    if sum(~dtype_recognized )
+        unrecognized_list = (alfil.name(~dtype_recognized));
+        alfil = structfun(@(x) x(dtype_recognized), alfil, 'UniformOutput', false);    
+    end
     % look at the files that are already up there and do not register them
     if ~isempty(ses.data_dataset_session_related)
         [~,i2reg] = setxor(alfil.name,ses.data_dataset_session_related.name);
@@ -67,12 +74,9 @@ for m = 28:length(alfses.block_files)
         warning('Sessions already exist and found datasets to upload !')
         disp(alfil.name)
     end
-    % make sure that all files have a pattern that match an existing type. If not warn
-    dtype_recognized = cellfun(@(x) any(~cellfun(@isempty , regexp(x, dtypes.regexp))), alfil.name);
     if sum(~dtype_recognized )
         warning('Skipping unknown datasetypes below: ')
-        disp(alfil.name(~dtype_recognized))
-        alfil = structfun(@(x) x(dtype_recognized), alfil, 'UniformOutput', false);    
+        disp(unrecognized_list)
     end
     % loop over each folder containing files to register to batch register all files within the folder
     for folder = unique(alfil.folders)'
--- matlab/examples/weighings_plot.m
+++ matlab/examples/weighings_plot.m
@@ -1,14 +1,21 @@
-one = One('alyx_login','root','alyx_pwd','Pradesh71','alyx_url','https://alyx.internationalbrainlab.org');
+one = One();
 %%
 
 subject = '437';
 wei = one.alyx_client.get(['/weighings?nickname=' subject]);
+
+
+wei = one.alyx_client.get(['/weighings']);
 wei.date_time = time.jsonrest2serial(wei.date_time);
 [~,ordre] = sort(wei.date_time );
 wei = sel_struct(wei, ordre);
 
 
 %%
-figure,plot(wei.date_time, wei.weight)
+figure,plot(wei.date_time, wei.weight,'.')
 
 
+%%
+wa = one.alyx_client.get(['/water-administrations']);
+wa = one.alyx_client.get(['/water-administrations?nickname=' subject]);
+wa.date_time = time.jsonrest2serial(wa.date_time);
