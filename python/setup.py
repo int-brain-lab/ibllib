ef64939e53e070e34ec1448626eae410dcb97cac
Niccolò Bonacchi <nbonacchi@gmail.com>
2018-12-21 17:25:17 +0000

    Bumped version in setup.py

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", 'r') as f:
 
 setup(
     name='ibllib',
-    version='0.3.1',
+    version='0.4.6',
     description='IBL libraries',
     license="MIT",
     long_description=long_description,
