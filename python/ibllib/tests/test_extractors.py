79c7af7f24bf54d763553a118fea6f330d0fba20
Olivier Winter <oliche@gmail.com>
2019-01-08 16:32:08 +0000

    extractors test import ciso8601 to test install

--- python/ibllib/tests/test_extractors.py
+++ python/ibllib/tests/test_extractors.py
@@ -2,9 +2,10 @@ import unittest
 from pathlib import Path
 import numpy as np
 import logging
+import ciso8601
 
+import alf.extractors as ex
 from ibllib.io import raw_data_loaders as loaders
-from alf.extractors import training_trials
 
 
 class TestExtractTrialData(unittest.TestCase):
@@ -16,7 +17,7 @@ class TestExtractTrialData(unittest.TestCase):
         self.logger = logging.getLogger('ibllib').setLevel(50)
 
     def test_stimOn_times(self):
-        st = training_trials.get_stimOn_times('', save=False, data=self.data)
+        st = ex.training_trials.get_stimOn_times('', save=False, data=self.data)
         self.assertTrue(isinstance(st, np.ndarray))
 
     def test_encoder_positions_duds(self):
@@ -26,3 +27,18 @@ class TestExtractTrialData(unittest.TestCase):
     def test_encoder_events_duds(self):
         dy = loaders.load_encoder_events(self.session_path)
         self.assertTrue(dy.shape[0] == 7)
+
+    def test_interpolation(self):
+        # straight test that it returns an usable function
+        ta = np.array([0., 1., 2., 3., 4., 5.])
+        tb = np.array([0., 1.1, 2.0, 2.9, 4., 5.])
+        finterp = ex.time_interpolation(ta, tb)
+        self.assertTrue(np.all(finterp(ta) == tb))
+        # next test if sizes are not similar
+        tc = np.array([0., 1.1, 2.0, 2.9, 4., 5., 6.])
+        finterp = ex.time_interpolation(ta, tc)
+        self.assertTrue(np.all(finterp(ta) == tb))
+
+    def test_ciso8601(self):
+        dt = ciso8601.parse_datetime('2018-01-16T14:21:32')
+        self.assertFalse(not dt)
