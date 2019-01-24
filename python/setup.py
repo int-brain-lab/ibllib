68738525f3f258c1f8279f75fa6e9138c68f3e33
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-01-24 12:49:32 +0000

    version increased to 0.4.7

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", 'r') as f:
 
 setup(
     name='ibllib',
-    version='0.4.6',
+    version='0.4.7',
     description='IBL libraries',
     license="MIT",
     long_description=long_description,
