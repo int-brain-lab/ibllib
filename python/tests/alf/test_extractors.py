3cdf8362abba65d48a489be20b5ca8b4a55b07da
Niccolò Bonacchi <nbonacchi@gmail.com>
2019-02-11 17:39:44 +0000

    Fixed choice extractor for 0% contrast trials
    Added test_choice to unittests

--- python/tests/alf/test_extractors.py
+++ python/tests/alf/test_extractors.py
@@ -49,6 +49,21 @@ class TestExtractTrialData(unittest.TestCase):
         dt = ciso8601.parse_datetime('2018-01-16T14:21:32')
         self.assertFalse(not dt)
 
+    def test_choice(self):
+        choice = ex.get_choice(self.session_path)
+        trial_nogo = np.array(
+            [~np.isnan(t['behavior_data']['States timestamps']['no_go'][0][0])
+             for t in self.data])
+        self.assertTrue(all(choice[trial_nogo]) == 0)
+        signed_contrast = np.array([t['signed_contrast'] for t in self.data])
+        if not all(signed_contrast == 0):
+            return
+        else:
+            # This will only fail is the mouse always answers with no go on a
+            # 0% contrast OR if the choice has been extracted wrong
+            self.assertTrue(any(choice[signed_contrast == 0] != 0))
+
+
 
 class TestTransferRigData(unittest.TestCase):
     def setUp(self):
