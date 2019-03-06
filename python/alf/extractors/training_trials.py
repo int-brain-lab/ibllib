ced28cba2cc301bfe0ca35e9307e82011ac0b243
Niccolo Bonacchi <nbonacchi@gmail.com>
2019-03-06 06:02:50 +0000

    Contrast left right bugfix

--- python/alf/extractors/training_trials.py
+++ python/alf/extractors/training_trials.py
@@ -114,15 +114,10 @@ def get_contrastLR(session_path, save=False, data=False):
     if not data:
         data = raw.load_data(session_path)
 
-    contrastLeft = np.array([t['signed_contrast'] for t in data])
-    contrastRight = contrastLeft.copy()
-    contrastLeft = np.array(
-        [x if np.sign(t['position']) < 0 else np.nan
-         for x, t in zip(contrastLeft, data)])
-    contrastRight = np.array(
-        [x if np.sign(t['position']) > 0 else np.nan
-         for x, t in zip(contrastRight, data)])
-    contrastLeft = np.abs(contrastLeft)
+    contrastLeft = np.array([t['contrast']['value'] if np.sign(
+        t['position']) < 0 else np.nan for t in data])
+    contrastRight = np.array([t['contrast']['value'] if np.sign(
+        t['position']) > 0 else np.nan for t in data])
     # save if needed
     check_alf_folder(session_path)
     if raw.save_bool(save, '_ibl_trials.contrastLeft.npy'):
