250ad0f3ea3aa9dd92b9b9b439139a95dfe29186
Olivier Winter <oliche@gmail.com>
2019-03-18 08:57:59 +0000

    ibllib.dsp hp filter

--- python/tests/ibllib/test_dsp.py
+++ python/tests/ibllib/test_dsp.py
@@ -54,7 +54,7 @@ class TestFFT(unittest.TestCase):
                -181.818181818182, -90.9090909090909],
         self.assertTrue(np.all(np.abs(ft.fscale(11, 0.001) - res) < 1e-6))
 
-    def test_filter_lp(self):
+    def test_filter_lp_hp(self):
         # test 1D time serie: subtracting lp filter removes DC
         ts1 = np.random.rand(500)
         out1 = ft.lp(ts1, 1, [.1, .2])
@@ -67,6 +67,9 @@ class TestFFT(unittest.TestCase):
         ts = mat.repmat(ts1[:, np.newaxis], 1, 11)
         out = ft.lp(ts, 1, [.1, .2], axis=0)
         self.assertTrue(np.allclose(np.transpose(out), out1))
+        # test 1D time serie: subtracting lp filter removes DC
+        out2 = ft.hp(ts1, 1, [.1, .2])
+        self.assertTrue(np.allclose(out1, ts1 - out2))
 
 
 if __name__ == "__main__":
