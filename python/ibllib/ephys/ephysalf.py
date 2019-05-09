106ea3476c5dae5d0643479e0e89d6b55b78f9a4
Cyrille Rossant <rossant@users.noreply.github.com>
2019-05-09 15:07:21 +0200

    Update ephys ALF conversion

--- python/ibllib/ephys/ephysalf.py
+++ python/ibllib/ephys/ephysalf.py
@@ -2,9 +2,12 @@
 ALF ephys data files.
 """
 
+import csv
 import logging
 from pathlib import Path
+import os.path as op
 import shutil
+import sys
 
 import numpy as np
 
@@ -23,11 +26,13 @@ def _move_if_possible(path, new_path):
         logger.info("File %s does not exist, skip moving.", path)
         return
     if Path(new_path).exists():
-        raise FileExistsError()
+        logger.warning("File %s already exists, skip moving.", new_path)
+        return
     # Create the target directory hierarchy if needed.
     new_path.parent.mkdir(parents=True, exist_ok=True)
     shutil.move(path, new_path)
     logger.info("Moved %s to %s.", path, new_path)
+    return True
 
 
 def _copy_if_possible(path, new_path):
@@ -36,6 +41,7 @@ def _copy_if_possible(path, new_path):
         raise FileExistsError()
     shutil.copy(path, new_path)
     logger.info("Copied %s to %s.", path, new_path)
+    return True
 
 
 def _find_file_with_ext(path, ext):
@@ -68,7 +74,6 @@ def _load(path):
 
 
 _FILE_RENAMES = [
-    ('spike_times.npy', 'spikes.times.npy'),
     ('spike_clusters.npy', 'spikes.clusters.npy'),
     ('amplitudes.npy', 'spikes.amps.npy'),
     ('channel_positions.npy', 'channels.sitePositions.npy'),
@@ -105,45 +110,94 @@ def _rename_to_alf(dir_path, renames=None):
 
 def _rename_raw_file(raw_path):
     raw_path = Path(raw_path)
-    assert raw_path.exists()
+    if not raw_path.exists():
+        return
     dir_path = raw_path.parent
-    _move_if_possible(raw_path, dir_path / ('ephys.raw' + raw_path.suffix))
+    new_path = dir_path / ('ephys.raw' + raw_path.suffix)
+    if _move_if_possible(raw_path, new_path):
+        return raw_path.name, new_path.name
 
 
 def _rename_lfp(dir_path):
     # Rename the LFP file if it exists.
     lf = _find_file_with_ext(dir_path, '.lf.bin')
-    if lf:
-        _move_if_possible(lf, dir_path / 'lfp.raw.bin')
-    return lf.name
+    new_path = dir_path / 'lfp.raw.bin'
+    if lf and _move_if_possible(lf, new_path):
+        return lf.name, new_path.name
+
+
+def _write_lines_tsv(path, lines_tsv):
+    path = str(path)
+    if sys.version_info[0] < 3:  # pragma: no cover
+        file = open(path, 'wb')
+    else:
+        file = open(path, 'w', newline='')
+    with file as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerows(lines_tsv)
+
+
+def _read_lines_tsv(path):
+    path = str(path)
+    if not op.exists(path):
+        return
+    if sys.version_info[0] < 3:  # pragma: no cover
+        file = open(path, 'rb')
+    else:
+        file = open(path, 'r', newline='')
+    with file as f:
+        reader = csv.reader(f, delimiter='\t')
+        return list(reader)
 
 
 class EphysAlfCreator(object):
     def __init__(self, model):
         self.model = model
         self.dir_path = Path(model.dir_path)
+        self._rename_path = self.dir_path / '.alf_renames.tsv'
+        self._created_path = self.dir_path / '.alf_created.tsv'
         self.spc = _spikes_per_cluster(model.spike_clusters)
-        self.renames = _FILE_RENAMES
+        self.renames = _read_lines_tsv(self._rename_path) or _FILE_RENAMES
+        self.created = []
 
     def convert(self):
         """Rename the files and generate new ALF files."""
         logger.info("Converting dataset to ALF.")
-        # Renames.
+        # Rename all ALF files.
         _rename_to_alf(self.dir_path)
-        _rename_raw_file(self.model.dat_path)
-        lfpname = _rename_lfp(self.dir_path)
-        self.renames.append((lfpname, 'lfp.raw.bin'))
+        # Rename the raw file.
+        out = _rename_raw_file(self.model.dat_path)
+        if out:
+            self.renames.append(out)
+        # Rename the LFP file.
+        out = _rename_lfp(self.dir_path)  # return old_name, new_name
+        if out:
+            self.renames.append(out)
 
         # New files.
+        self.make_spike_times()
         self.make_cluster_waveforms()
         self.make_depths()
         self.make_mean_waveforms()
+
+        # Write the TSV files with all renames and new files created.
+        _write_lines_tsv(self._rename_path, self.renames)
+        _write_lines_tsv(self._created_path, [(_,) for _ in self.created])
+
+    def _save_npy(self, filename, arr):
+        np.save(self.dir_path / filename, arr)
+        self.created.append(filename)
 
     def rollback(self):
         """Rollback the renames."""
         logger.info("Rolling back dataset conversion.")
         inverse_renames = [(new, old) for (old, new) in self.renames]
         _rename_to_alf(self.dir_path, renames=inverse_renames)
+
+    def make_spike_times(self):
+        """We cannot just rename spike_times.npy because it is in unit of
+        *samples*, and not in seconds."""
+        self._save_npy('spikes.times.npy', self.model.spike_times)
 
     def make_cluster_waveforms(self):
         """Return the channel index with the highest template amplitude, for
@@ -159,7 +213,7 @@ class EphysAlfCreator(object):
             # Compute the peak channels for each template.
             template_peak_channels = np.argmax(tmp.max(axis=1) - tmp.min(axis=1), axis=1)
             assert template_peak_channels.shape == (n_templates,)
-            np.save(peak_channel_path, template_peak_channels)
+            self._save_npy(peak_channel_path.name, template_peak_channels)
         else:
             template_peak_channels = np.load(peak_channel_path)
 
@@ -168,7 +222,7 @@ class EphysAlfCreator(object):
             # Compute the peak channel waveform for each template.
             waveforms = tmp[:, :, template_peak_channels]
             durations = waveforms.argmax(axis=1) - waveforms.argmin(axis=1)
-            np.save(waveform_duration_path, durations)
+            self._save_npy(waveform_duration_path.name, durations)
 
     def make_depths(self):
         """Make spikes.depths.npy and clusters.depths.npy."""
@@ -194,18 +248,18 @@ class EphysAlfCreator(object):
         spikes_depths = clusters_depths[spike_clusters_rel]
         assert spikes_depths.shape == (n_spikes,)
 
-        np.save(p / 'spikes.depths.npy', spikes_depths)
-        np.save(p / 'clusters.depths.npy', clusters_depths)
+        self._save_npy('spikes.depths.npy', spikes_depths)
+        self._save_npy('clusters.depths.npy', clusters_depths)
 
     def make_mean_waveforms(self):
-        p = self.dir_path
-
         spike_ids = select_spikes(
             cluster_ids=self.cluster_ids,
             max_n_spikes_per_cluster=100,
             spikes_per_cluster=lambda clu: self.spc[clu],
             subset='random')
         waveforms = self.model.get_waveforms(spike_ids, np.arange(self.model.n_channels))
-        mean_waveforms = grouped_mean(waveforms, self.model.spike_clusters[spike_ids])
-
-        np.save(p / 'clusters.meanWaveforms.npy', mean_waveforms)
+        try:
+            mean_waveforms = grouped_mean(waveforms, self.model.spike_clusters[spike_ids])
+            self._save_npy('clusters.meanWaveforms.npy', mean_waveforms)
+        except IndexError as e:
+            logger.warning("Failed to create the mean waveforms file: %s.", e)
