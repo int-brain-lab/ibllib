26a86fca6dfecf8d97bfb563dabeb1d84a642391
Olivier Winter <oliche@gmail.com>
2019-06-11 12:04:07 +0100

    bincount2D function does aggregation according to unique values /w tests

--- ibllib/misc/misc.py
+++ ibllib/misc/misc.py
@@ -76,14 +76,14 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1, length=10
         print()
 
 
-def bincount2D(x, y, xbin, ybin, xlim=None, ylim=None, weights=None):
+def bincount2D(x, y, xbin=0, ybin=0, xlim=None, ylim=None, weights=None):
     """
     Computes a 2D histogram by aggregating values in a 2D array.
 
     :param x: values to bin along the 2nd dimension (c-contiguous)
     :param y: values to bin along the 1st dimension
-    :param xbin: bin size along 2nd dimension
-    :param ybin: bin size along 1st dimension
+    :param xbin: bin size along 2nd dimension (set to 0 to aggregate according to unique values)
+    :param ybin: bin size along 1st dimension (set to 0 to aggregate according to unique values)
     :param weights:(optional) defaults to None: weights to apply to each value for aggregation
     :return: 3 numpy arrays: MAP [ny,nx] image, xscale [nx], yscale [ny]
     """
@@ -93,12 +93,20 @@ def bincount2D(x, y, xbin, ybin, xlim=None, ylim=None, weights=None):
     if not ylim:
         ylim = [np.min(y), np.max(y)]
 
-    xscale = np.arange(xlim[0], xlim[1] + xbin / 2, xbin)
-    yscale = np.arange(ylim[0], ylim[1] + ybin / 2, ybin)
-    nx, ny = [xscale.size, yscale.size]
+    # create the indices on which to aggregate: binning is different that aggregating
+    if xbin:
+        xscale = np.arange(xlim[0], xlim[1] + xbin / 2, xbin)
+        xind = (np.floor((x - xlim[0]) / xbin)).astype(np.int64)
+    else:  # if bin size = 0 , aggregate over unique values
+        xscale, xind = np.unique(x, return_inverse=True)
+    if ybin:
+        yscale = np.arange(ylim[0], ylim[1] + ybin / 2, ybin)
+        yind = (np.floor((y - ylim[0]) / ybin)).astype(np.int64)
+    else:  # if bin size = 0 , aggregate over unique values
+        yscale, yind = np.unique(y, return_inverse=True)
+
     # aggregate by using bincount on absolute indices for a 2d array
-    ind2d = np.ravel_multi_index(np.array([np.floor((y - ylim[0]) / ybin),
-                                           np.floor((x - xlim[0]) / xbin)]).astype(np.int64),
-                                 dims=(ny, nx))
+    nx, ny = [xscale.size, yscale.size]
+    ind2d = np.ravel_multi_index(np.c_[yind, xind].transpose(), dims=(ny, nx))
     r = np.bincount(ind2d, minlength=nx * ny, weights=weights).reshape(ny, nx)
     return r, xscale, yscale
