31c48f8862a1535a8dfd5f24f5b9df88b4dc9b79
Olivier Winter <oliche@gmail.com>
2019-07-31 14:24:24 +0100

    dlc_download data example

--- examples/ibllib/dlc_analysis.py
+++ examples/ibllib/dlc_analysis.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import pandas as pd
+
 import alf.io
 from oneibl.one import ONE
 
@@ -13,3 +15,7 @@ dtypes = ['_ibl_leftCamera.dlc', '_iblrig_leftCamera.timestamps']
 d = one.load(eid, dataset_types=dtypes, download_only=True, dclass_output=True)
 ses_path = Path(d.local_path[0]).parent
 segments = alf.io.load_object(ses_path, '_ibl_leftCamera')
+
+
+ts = pd.read_csv(d.local_path[2], delimiter=' ')
+
