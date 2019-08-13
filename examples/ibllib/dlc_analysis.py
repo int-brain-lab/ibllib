81885910eb8b4420e319f1aed8666b0c13b5611c
Zoe <zashwood@princeton.edu>
2019-08-13 16:58:28 -0400

    First try with flake8; used it to make file compliant with PEP8

--- examples/ibllib/dlc_analysis.py
+++ examples/ibllib/dlc_analysis.py
@@ -1,9 +1,7 @@
 from pathlib import Path
-import pandas as pd
-import numpy as np
 import alf.io
 from oneibl.one import ONE
-from dlc_pupil import pupil_features 
+from dlc_pupil import pupil_features
 import matplotlib.pyplot as plt
 
 # Find and load data from ONE
@@ -19,10 +17,10 @@ segments = alf.io.load_object(ses_path, '_ibl_leftCamera')
 # Fit circle on pupil points
 vec_x = [segments['pupil_top_r_x'][0:1000], segments['pupil_left_r_x'][0:1000], segments['pupil_right_r_x'][0:1000], segments['pupil_bottom_r_x'][0:1000]]
 vec_y = [segments['pupil_top_r_y'][0:1000], segments['pupil_left_r_y'][0:1000], segments['pupil_right_r_y'][0:1000], segments['pupil_bottom_r_y'][0:1000]]
-x,y,diameter = pupil_features(vec_x, vec_y)
+x, y, diameter = pupil_features(vec_x, vec_y)
 
-f, (ax1,ax2,ax3) = plt.subplots(1,3,figsize=(10,5))
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
 ax1.plot(x)
 ax2.plot(y)
 ax3.plot(diameter)
-
+plt.show()
