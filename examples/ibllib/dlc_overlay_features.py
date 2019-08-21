d7467f0206af8426c5a232cf44eb1761b979a5a0
Zoe <zashwood@princeton.edu>
2019-08-21 11:52:38 -0400

    updated video overlay code to read in new data and to plot multiple pngs

--- examples/ibllib/dlc_overlay_features.py
+++ examples/ibllib/dlc_overlay_features.py
@@ -6,7 +6,7 @@ from oneibl.one import ONE
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
 
 def get_video_frame(video_path, frame_number):
     """
@@ -37,11 +37,27 @@ def clean_timestamp_dta(timestamps):
     return timestamps_new
 
 
+def create_pupil_array(dlc_frame_dict):
+    """
+    Create x_vec, y_vec with pupil coordinates from dlc dictionary.  x_vec and y_vec will each have four components and will be ordered top, right, left, bottom
+    :param dlc_frame_dict: dictionary of DLC features
+    :return: vec_x and vec_y
+    """
+    vec_x = [dlc_frame_dict['pupil_top_r_x'], dlc_frame_dict['pupil_right_r_x'], dlc_frame_dict['pupil_left_r_x'], dlc_frame_dict['pupil_bottom_r_x']]
+    vec_y = [dlc_frame_dict['pupil_top_r_y'], dlc_frame_dict['pupil_right_r_y'], dlc_frame_dict['pupil_left_r_y'], dlc_frame_dict['pupil_bottom_r_y']]
+    return vec_x, vec_y
+
+
 if __name__ == '__main__':
     one = ONE()
     dtypes = ['_ibl_leftCamera.dlc', '_iblrig_leftCamera.raw', '_iblrig_leftCamera.timestamps']
     eids = one.search(dataset_types=dtypes)
+    print(eids)
     eid = eids[0]
+    # Make directory for session data:
+    dir_id = eid
+    if not os.path.exists(dir_id):
+        os.makedirs(dir_id)
     # Load and download data to local computer
     dlc_meta, dlc_segments, timestamps, raw_vid_meta = one.load(eid, dataset_types=dtypes,
                                                                 dclass_output=True)
@@ -49,26 +65,24 @@ if __name__ == '__main__':
     clean_timestamps = clean_timestamp_dta(timestamps)
     segments = dlc_segments.data
     dlc_columns = dlc_meta.data['columns']
-    # Frame number to work with:
-    frame_number = int(segments.shape[0] / 2)
-    # Print and maximum and minimum x and y values for pupil_top_r across all frames:
-    print("Maximum x value - top pupil = " + str(max(segments[:, 0])))
-    print("Minimum x value - top pupil = " + str(min(segments[:, 0])))
-    print("Maximum y value - top pupil = " + str(max(segments[:, 1])))
-    print("Minimum y value - top pupil = " + str(min(segments[:, 1])))
-    # Extract a single video frame and overlay location of top of pupil onto it
-    video_frame = get_video_frame(raw_vid_meta.local_path, frame_number)
-    dlc_frame_dict = {k: v for k, v in zip(dlc_columns, segments[frame_number])}
-    # Overlay a circle corresponding to top of pupil onto image:
-    top_pupil_x = dlc_frame_dict['pupil_top_r_x']
-    top_pupil_y = dlc_frame_dict['pupil_top_r_y']
-    # Show the image
-    fig, ax = plt.subplots(1)
-    ax.set_aspect('equal')
-    ax.imshow(video_frame)
-    # Create circle at x, y pair extracted by Guido
-    circ = plt.Circle((top_pupil_x, top_pupil_y), 20, color="r", alpha=0.5)
-    ax.add_patch(circ)
-    plt.title("Overlay DLC on video frame - pupil_top_r \n Session: " + str(eid) + "; Frame " + str(frame_number))
-    # Save image:
-    fig.savefig('dlc_video_frame_overlay_1.png')
+    # Loop through frames and produce png:
+    # Randomly sample 100 frames:
+    frames_to_plot = np.random.choice(range(segments.shape[0]), 100, replace=False)
+    for frame_number in frames_to_plot:
+        # Extract a single video frame and overlay location of top of pupil onto it
+        video_frame = get_video_frame(raw_vid_meta.local_path, frame_number)
+        dlc_frame_dict = {k: v for k, v in zip(dlc_columns, segments[frame_number])}
+        # Overlay a circle corresponding to top of pupil onto image:
+        pupil_vec_x, pupil_vec_y = create_pupil_array(dlc_frame_dict)
+        # Show the image
+        fig, ax = plt.subplots(1)
+        ax.set_aspect('equal')
+        ax.imshow(video_frame)
+        # Create circle at x, y pair extracted by Guido
+        #circ = plt.Circle((top_pupil_x, top_pupil_y), 10, color="r", alpha=0.5)
+        #ax.add_patch(circ)
+        for j in range(len(pupil_vec_y)):
+            ax.scatter(pupil_vec_x[j], pupil_vec_y[j], marker ='+', s=5, alpha = 0.9)
+        plt.title("Overlay DLC on video frame - pupil_top_r \n Session: " + str(eid) + "; Frame " + str(frame_number))
+        # Save image:
+        fig.savefig(dir_id +'/frame_' + str(frame_number)+ '.png')
