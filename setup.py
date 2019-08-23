d856484afcccc12071971a17369eab31fc08ba05
Olivier Winter <oliche@gmail.com>
2019-08-23 19:31:52 +0100

    bugfix: empty flags added manually don't prevent video compression

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ibllib',
-    version='1.1.2',
+    version='1.1.3',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='IBL libraries',
     license="MIT",
