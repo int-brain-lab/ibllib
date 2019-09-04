9598cd3cfbfb7edd1d919c2f1d2a4cb443d7a0da
Guido Meijer <gtmeijer@gmail.com>
2019-09-04 11:50:44 -0400

    Added transform pixels into mm

--- ibllib/dlc_analysis/dlc_basis_functions.py
+++ ibllib/dlc_analysis/dlc_basis_functions.py
@@ -25,6 +25,8 @@ def load_dlc(folder_path, camera='left'):
 
     # Load in DLC data
     dlc_data = alf.io.load_object(join(folder_path, 'alf'), '_ibl_%sCamera' % camera)
+    dlc_data['camera'] = camera
+    dlc_data['units'] = 'px'
 
     # Hard-coded hack because extraction of timestamps was wrong
     if camera == 'left':
@@ -45,7 +47,46 @@ def load_dlc(folder_path, camera='left'):
 
 
 def load_event_times(folder_path):
+    """
+    Load in DLC traces and timestamps from FPGA and align them
+
+    Parameters
+    ----------
+    folder_path: path to top-level folder of recording
+    camera:      which camera to use
+
+    """
     stim_on_times = np.load(join(folder_path, 'alf', '_ibl_trials.stimOn_times.npy'))
     feedback_type = np.load(join(folder_path, 'alf', '_ibl_trials.feedbackType.npy'))
     feedback_times = np.load(join(folder_path, 'alf', '_ibl_trials.feedback_times.npy'))
     return stim_on_times, feedback_type, feedback_times
+
+
+def transform_px_to_mm(dlc_data, width_mm=66, height_mm=54):
+    """
+    Transform pixel values to millimeter
+
+    Parameters
+    ----------
+    width_mm:  the width of the video feed in mm
+    height_mm: the height of the video feed in mm
+    """
+
+    # Set pixel dimensions for different cameras
+    if dlc_data['camera'] == 'left':
+        px_dim = [1280, 1024]
+    elif dlc_data['camera'] == 'right' or dlc_data['camera'] == 'body':
+        px_dim = [640, 512]
+
+    # Transform pixels into mm
+    for key in list(dlc_data.keys()):
+        if key[-1] == 'x':
+            dlc_data[key] = dlc_data[key] * (width_mm/px_dim[0])
+        if key[-1] == 'y':
+            dlc_data[key] = dlc_data[key] * (height_mm/px_dim[1])
+    dlc_data['units'] = 'mm'
+
+    return dlc_data
+
+
+
