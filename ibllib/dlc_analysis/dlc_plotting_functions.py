59b8a9b1daabbf33f404d594455d6a885f4edac0
Guido Meijer <gtmeijer@gmail.com>
2019-09-04 15:15:40 -0400

    Added normalization functionality

--- ibllib/dlc_analysis/dlc_plotting_functions.py
+++ ibllib/dlc_analysis/dlc_plotting_functions.py
@@ -10,15 +10,21 @@ List of plotting functions for DLC data
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from scipy.stats import zscore
 
 
-def peri_plot(dlc_traces, timestamps, events, ax, time_win=[-1, 3]):
+def peri_plot(trace, timestamps, events, ax, time_win=[-1, 3], norm='none'):
     """
     Plot a peri-plot centered around a behavioral event
-    :dlc_traces:     list with arrays of DLC traces
-    :timestamps:     array with timestamps in seconds
-    :events: array   with event times to center around
-    :time_window:    time window in seconds
+    :dlc_traces:   list with arrays of DLC traces
+    :timestamps:   array with timestamps in seconds
+    :events:       array   with event times to center around
+    :time_window:  time window in seconds
+    :norm:         how to perform normalization
+                   'none': no normalization (default)
+                   'zscore': z-score the entire trace
+                   'baseline': subtract the baseline from each trial trace
+                               defined as the time before onset of the event
     """
 
     # Transform time window into samples
@@ -27,16 +33,27 @@ def peri_plot(dlc_traces, timestamps, events, ax, time_win=[-1, 3]):
                   np.int(np.round(time_win[1] * sampling_rate))]
     time_trace = np.arange(time_win[0], time_win[1], 1/np.round(sampling_rate))
 
+    # Z-score entire trace
+    if norm == 'zscore':
+        trace = zscore(trace)
+
     # Create dataframe for line plot
     peri_df = pd.DataFrame(columns=['event_nr', 'timepoint', 'trace'])
     for i in np.arange(np.size(events)):
         if np.argmin(np.abs(timestamps-events[i]))+sample_win[0] > 0:
-            this_trace = dlc_traces[np.argmin(np.abs(timestamps-events[i]))+sample_win[0]:
-                                    np.argmin(np.abs(timestamps-events[i]))+sample_win[1]]
+            # Get trace for this trial
+            this_trace = trace[np.argmin(np.abs(timestamps-events[i]))+sample_win[0]:
+                               np.argmin(np.abs(timestamps-events[i]))+sample_win[1]]
+
+            # Perform baseline correction
+            if norm == 'baseline':
+                this_trace = this_trace - np.median(this_trace[time_trace < 0])
+
+            # Add to dataframe
             this_df = pd.DataFrame(data={'event_nr': np.ones(np.size(this_trace),
                                                              dtype=int)*(i+1),
                                          'timepoint': time_trace, 'trace': this_trace})
             peri_df = pd.concat([peri_df, this_df], ignore_index=True)
 
     # Plot
-    sns.lineplot(x='timepoint', y='trace', data=peri_df, ax=ax)
+    sns.lineplot(x='timepoint', y='trace', data=peri_df, ci=68, ax=ax)
