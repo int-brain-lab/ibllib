7ac6486a59b02fb700e21f171f22b7123d1acff6
HernandoMV <hernybiotec@gmail.com>
2019-09-04 14:55:15 -0400

    cca plot
    filled plotting function and created test file

--- brainbox/population/cca.py
+++ brainbox/population/cca.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pylab as plt
+
 
 
 def preprocess(array, smoothing_sd=25, n_pca_dims=20):
@@ -67,15 +69,34 @@ def shuffle_analysis(array_0, array_1, n_shuffles=100, **cca_kwargs):
     pass
 
 
-def plot_correlations(corrs, shuffled=None):
+def plot_correlations(corrs, errors=None, ax=None, **plot_kwargs):
     """
     Correlation vs CCA dimension
 
-    :param corrs:
-    :param shuffled:
-    :return:
+    :param corrs: correlation values for the CCA dimensions
+    :type corrs: 1-D vector
+    :param errors: error values
+    :type shuffled: 1-D array of size len(corrs)
+    :param ax: axis to plot on (default None)
+    :type ax: matplotlib axis object
+    :return: axis if specified, or plot if axis = None
     """
-    pass
+    # evaluate if np.arrays are passed
+    # TODO
+    # create axis if no axis is passed
+    if ax is None:
+        ax = plt.gca()
+    # get the data for the x and y axis
+    y_data = corrs
+    x_data = range(1, (len(corrs) + 1))
+    # create the plot object
+    cplot = ax.plot(x_data, y_data, **plot_kwargs)
+    ax.fill_between(x_data, y_data-errors, y_data+errors, **plot_kwargs, alpha=0.2)
+    # change y and x labels and ticks
+    ax.set_xticks(x_data)
+    ax.set_ylabel("Correlation")
+    ax.set_xlabel("CCA dimension")
+    return cplot
 
 
 if __name__ == '__main__':
@@ -126,3 +147,19 @@ if __name__ == '__main__':
     # plot cca correlations
     corrs = get_correlations(cca, data[0][idxs_time['test'], :], data[0][idxs_time['test'], :])
     plot_correlations(corrs)
+
+    # Shuffle data
+    # ...
+    # fig, ax1 = plt.subplots(1,1,figsize(10,10))
+    # plot_correlations(corrs, ... , ax=ax1, color='blue')
+    # plot_correlations(shuffled_coors, ..., ax=ax1, color='red')
+    # plt.show()
+
+
+    ## test plotting
+    corrs = np.array([.6, .2, .1, .001])
+    errs = np.array([.1, .05, .04, .0005])
+    fig, ax1 = plt.subplots(1,1,figsize=(5,5))
+    plot_correlations(corrs, errs , ax=ax1, color='blue')
+    plot_correlations(corrs*.1, errs , ax=ax1, color='orange')
+    plt.show()
