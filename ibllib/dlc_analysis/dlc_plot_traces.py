659eff1edf6e84801eb9fba14a19214025b4bd11
Guido Meijer <gtmeijer@gmail.com>
2019-09-11 15:18:51 +0100

    Code update

--- ibllib/dlc_analysis/dlc_plot_traces.py
+++ ibllib/dlc_analysis/dlc_plot_traces.py
@@ -6,27 +6,61 @@ Created on Tue Sep  3 15:48:15 2019
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-from dlc_basis_functions import load_dlc, load_dlc_training, load_event_times, load_events, px_to_mm
+from dlc_basis_functions import load_dlc_training, load_event_times, load_events, px_to_mm
 from dlc_analysis_functions import pupil_features
 from dlc_plotting_functions import peri_plot
+from oneibl.one import ONE
+from pathlib import Path
+import cv2
+from time import sleep
 
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\KS005\\2019-08-29\\001'
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\KS005\\2019-08-30\\001'
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\ibl_witten_04\\2019-08-04\\002'
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\ibl_witten_04\\2018-08-11\\001'
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\ZM_1735\\2019-08-01\\001'
-#folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\ZM_1736\\2019-08-09\\004'
 
-folder_path = 'C:\\Users\\guido\\Data\\Flatiron\\Subjects\\ZM_1748\\2019-07-10\\001'
+def get_video_frame(video_path, frame_number):
+    """
+    Obtain numpy array corresponding to a particular video frame in video_path
+    :param video_path: local path to mp4 file
+    :param frame_number: video frame to be returned
+    :return: numpy array corresponding to frame of interest.  Dimensions are (1024, 1280, 3)
+    """
+    cap = cv2.VideoCapture(video_path)
+    cap.set(1, frame_number)  # 0-based index of the frame to be decoded/captured next.
+    ret, frame_image = cap.read()
+    cv2.imshow('Frame', frame_image)
+    sleep(5)
+    while(cap.isOpened()):
+        ret, frame_image = cap.read()
+        if ret is True:
+            cv2.imshow('Frame', frame_image)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    return frame_image
+
+
+# Load in ONE
+one = ONE()
+dtypes = ['_ibl_leftCamera.dlc', '_iblrig_taskData.raw', 'trials.feedback_times',
+          'trials.feedbackType', 'trials.stimOn_times', 'trials.choice', '_iblrig_leftCamera.raw']
+eids = one.search(dataset_types=dtypes)
+eid = eids[1]
+d = one.load(eid, dataset_types=dtypes, download_only=True, dclass_output=True)
+folder_path = str(Path(d.local_path[0]).parent.parent)
 
 # Load in data
-#dlc_dict = load_dlc(folder_path)
 dlc_dict = load_dlc_training(folder_path)
 dlc_dict = px_to_mm(dlc_dict)
 stim_on_times, feedback_times = load_event_times(folder_path)
 choice, feedback_type = load_events(folder_path)
 
+# Get video frames during last reward
+frame_number = np.argmin(np.abs(dlc_dict['timestamps']-feedback_times[feedback_type == 1][-1]))
+frame = get_video_frame(d[7].local_path, frame_number)
+"""
 # Get pupil
 pupil_x, pupil_y, diameter = pupil_features(dlc_dict)
 
@@ -43,18 +77,18 @@ ax2.plot([0, 0], ax2.get_ylim(), 'r')
 ax2.set(ylabel='Baseline subtracted pupil diameter (mm)', xlabel='Time (s)',
         title='Reward delivery')
 plt.tight_layout(pad=2)
-
+"""
 # Plot paws
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 8))
 peri_plot(dlc_dict['middle_finger_r_x'], dlc_dict['timestamps'],
-          feedback_times[(choice == -1) & (feedback_type == 1)], ax1, [-1, 1], 'baseline')
+          feedback_times[(choice == -1) & (feedback_type == 1)], ax1, [-1, 10], 'baseline')
 ax1.plot([0, 0], ax1.get_ylim(), 'r', label='Reward delivery')
 ax1.set(ylabel='Baseline subtracted paw position (mm)', xlabel='Time (s)',
         title='Leftward choices')
 ax1.legend(frameon=False)
 
 peri_plot(dlc_dict['middle_finger_r_x'], dlc_dict['timestamps'],
-          feedback_times[(choice == 1) & (feedback_type == 1)], ax2, [-1, 1], 'baseline')
+          feedback_times[(choice == 1) & (feedback_type == 1)], ax2, [-1, 10], 'baseline')
 ax2.plot([0, 0], ax2.get_ylim(), 'r', label='Reward delivery')
 ax2.set(ylabel='Baseline subtracted paw position (mm)', xlabel='Time (s)',
         title='Rightward choices')
@@ -64,17 +98,16 @@ plt.tight_layout(pad=2)
 # Plot tongue
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 8))
 peri_plot(dlc_dict['tongue_end_l_x'], dlc_dict['timestamps'],
-          feedback_times[feedback_type == -1], ax1, [-1, 4], 'baseline')
+          feedback_times[feedback_type == 1], ax1, [-1, 10], 'none')
 ax1.plot([0, 0], ax1.get_ylim(), 'r', label='Reward delivery')
 ax1.set(ylabel='Baseline subtracted paw position (mm)', xlabel='Time (s)',
-        title='Leftward choices')
+        title='Correct trials')
 ax1.legend(frameon=False)
 
 peri_plot(dlc_dict['tongue_end_r_x'], dlc_dict['timestamps'],
-          feedback_times[feedback_type == -1], ax2, [-1, 4], 'baseline')
+          feedback_times[feedback_type == -1], ax2, [-1, 10], 'none')
 ax2.plot([0, 0], ax2.get_ylim(), 'r', label='Reward delivery')
 ax2.set(ylabel='Baseline subtracted paw position (mm)', xlabel='Time (s)',
-        title='Leftward choices')
+        title='Incorrect trials')
 ax2.legend(frameon=False)
-
 
