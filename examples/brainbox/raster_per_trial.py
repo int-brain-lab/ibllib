7da9574f6f84299869defbc09c6d46c07ecfd27b
mschart <michael.m.schartner@gmail.com>
2019-09-26 14:41:39 +0200

    Create raster_per_trial.py (#104)

---
+++ examples/brainbox/raster_per_trial.py
@@ -0,0 +1,96 @@
+import numpy as np
+import alf.io
+from brainbox.processing import bincount2D
+import matplotlib.pyplot as plt
+import ibllib.plots as iblplt
+
+alf_path = '.../ZM_1735/2019-08-01/001/alf'
+
+spikes = alf.io.load_object(alf_path, 'spikes')
+clusters = alf.io.load_object(alf_path, 'clusters')
+channels = alf.io.load_object(alf_path, 'channels')
+trials = alf.io.load_object(alf_path, '_ibl_trials')
+
+T_BIN = 0.01  # time bin in sec
+
+# just get channels from probe 0, as there are two probes here
+probe_id = clusters['probes'][spikes['clusters']]
+restrict = np.where(probe_id == 0)[0]
+
+# bin spikes
+R, times, Clusters = bincount2D(
+    spikes['times'][restrict], spikes['clusters'][restrict], T_BIN)
+
+# Order activity by cortical depth of neurons
+d = dict(zip(spikes['clusters'][restrict], spikes['depths'][restrict]))
+y = sorted([[i, d[i]] for i in d])
+isort = np.argsort([x[1] for x in y])
+R = R[isort, :]
+
+# get trial number for each time bin
+trial_numbers = np.digitize(times, trials['goCue_times'])
+print('Range of trials: ', [trial_numbers[0], trial_numbers[-1]])
+
+
+def add_stim_off_times(trials):
+    on = 'stimOn_times'
+    off = 'stimOff_times'
+    trials[off] = np.zeros(shape=trials[on].shape)
+    correct_trials = trials['feedbackType'] == 1
+    u = trials['feedback_times'][correct_trials] + 1.0
+    trials[off][correct_trials] = u
+    error_trials = trials['feedbackType'] == -1
+    v = trials['feedback_times'][error_trials] + 2.0
+    trials[off][error_trials] = v
+
+
+add_stim_off_times(trials)
+
+
+def plot_trial(trial_number, R, times):
+    '''
+    Plot a rasterplot for a given trial,
+    ordered by insertion depth, with
+    'stimOn_times','feedback_times' and 'stimOff_times'
+    '''
+
+    a = list(trial_numbers)
+    first = a.index(trial_number)
+    last = len(a) - 1 - a[::-1].index(trial_number)
+
+    plt.imshow(R[:, first:last], aspect='auto',
+               cmap='binary', vmax=T_BIN / 0.001 / 4,
+               extent=np.r_[times[[first, last]],
+               Clusters[[0, -1]]], origin='lower')
+
+    def restrict_timestamplist(q):
+
+        li = []
+        for i in q:
+            if i > times[first] and i < times[last]:
+                li.append(i)
+        return li
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['stimOn_times']), ymin=0, ymax=Clusters[-1],
+        color='m', linewidth=0.5, label='stimOn_times')
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['feedback_times']), ymin=0, ymax=Clusters[-1],
+        color='b', linewidth=0.5, label='feedback_times')
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['stimOff_times']), ymin=0, ymax=Clusters[-1],
+        color='g', linewidth=0.5, label='stimOff_times')
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('Cluster #; ordered by depth')
+    plt.legend()
+    plt.tight_layout()
+
+
+# Get a raster plot
+if __name__ == "__main__":
+    # get a raster plot for a particular trial
+    plot_trial(235, R, times)
+    plt.show()
