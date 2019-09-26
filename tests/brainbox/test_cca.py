44aadf3190c7eee8f69b055e1030ca2c1e3449d6
Matt Whiteway <themattinthehatt@users.noreply.github.com>
2019-09-26 04:06:26 -0400

    cca functions from code camp (#102)
    * outline
    
    * adding example usage
    
    * cca plot
    
    filled plotting function and created test file
    
    * assert if errors
    
    .
    
    * added function for binning and getting trial ids
    
    .
    
    * Auto stash before merge of "feature/correlations" and "origin/feature/correlations"
    
    * bug fixes
    
    * small stuff
    
    .
    
    * update trial/time splits to work with multiple folds
    
    * function for splitting by area and workflow
    
    * jupyter notebook for testing cca
    
    * plot pairwise cca correlations
    
    * worked on notebook
    
    * transposing binned spikes
    
    * more transposing!
    
    * indexing bug fix
    
    * export an extra array in function
    
    * now
    
    * function to get index of bins associated to the event
    
    * comparison of two behavioural events
    
    functions and wrappers for plotting the ccas of two behavioral events in the same plot, comparing different areas
    
    * cca code camp cleanup
    
    * flake8

---
+++ tests/brainbox/test_cca.py
@@ -0,0 +1,18 @@
+from brainbox.population import cca
+import numpy as np
+import matplotlib.pylab as plt
+# test plotting
+corrs = np.array([.6, .2, .1, .001])
+errs = np.array([.1, .05, .04, .0005])
+fig, ax1 = plt.subplots(1, 1, figsize=(5, 5))
+cca.plot_correlations(corrs, errs, ax=ax1, color='blue')
+cca.plot_correlations(corrs * .1, errs, ax=ax1, color='orange')
+plt.show()
+
+# Shuffle data
+# ...
+# fig, ax1 = plt.subplots(1,1,figsize(10,10))
+# plot_correlations(corrs, ... , ax=ax1, color='blue')
+# plot_correlations(shuffled_coors, ..., ax=ax1, color='red')
+# plt.show()
+
