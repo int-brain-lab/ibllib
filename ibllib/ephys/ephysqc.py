5dc62e3097fee3e2e6e979b1efa02d37ba1bdab8
olivier <olivier.winter@hotmail.fr>
2019-11-07 06:17:41 +0000

    move atlas, brainbox tests, remove iblib yaml envs and deflake brainbox

--- ibllib/ephys/ephysqc.py
+++ ibllib/ephys/ephysqc.py
@@ -7,6 +7,7 @@ import logging
 import numpy as np
 import pandas as pd
 from scipy import signal
+from scipy.ndimage import gaussian_filter1d
 
 from phylib.io import model as phymod
 
@@ -14,7 +15,6 @@ from phylib.io import model as phymod
 import alf.io
 from brainbox.core import Bunch
 from brainbox.processing import bincount2D
-import brainbox.quality.ephys as metrics
 from ibllib.ephys import sync_probes
 from ibllib.io import spikeglx
 import ibllib.dsp as dsp
@@ -329,10 +329,86 @@ def spike_sorting_metrics(spike_times, spike_clusters, spike_amplitudes,
         st = spike_times[ispikes]
         sa = spike_amplitudes[ispikes]
         # compute metrics
-        r.isi_viol[ic], _ = metrics.isi_violations(st, tmin, tmax,
-                                                   isi_threshold=params['isi_threshold'],
-                                                   min_isi=params['min_isi'])
-        r.amplitude_cutoff[ic] = metrics.amplitude_cutoff(amplitudes=sa)
+        r.isi_viol[ic], _ = isi_violations(st, tmin, tmax,
+                                           isi_threshold=params['isi_threshold'],
+                                           min_isi=params['min_isi'])
+        r.amplitude_cutoff[ic] = amplitude_cutoff(amplitudes=sa)
         r.amplitude_std[ic] = np.std(sa)
 
     return pd.DataFrame(r)
+
+
+def isi_violations(spike_train, min_time, max_time, isi_threshold, min_isi=0):
+    """Calculate ISI violations for a spike train.
+
+    Based on metric described in Hill et al. (2011) J Neurosci 31: 8699-8705
+
+    modified by Dan Denman from cortex-lab/sortingQuality GitHub by Nick Steinmetz
+
+    Inputs:
+    -------
+    spike_train : array of spike times
+    min_time : minimum time for potential spikes
+    max_time : maximum time for potential spikes
+    isi_threshold : threshold for isi violation
+    min_isi : threshold for duplicate spikes
+
+    Outputs:
+    --------
+    fpRate : rate of contaminating spikes as a fraction of overall rate
+        A perfect unit has a fpRate = 0
+        A unit with some contamination has a fpRate < 0.5
+        A unit with lots of contamination has a fpRate > 1.0
+    num_violations : total number of violations
+
+    """
+
+    duplicate_spikes = np.where(np.diff(spike_train) <= min_isi)[0]
+
+    spike_train = np.delete(spike_train, duplicate_spikes + 1)
+    isis = np.diff(spike_train)
+
+    num_spikes = spike_train.size
+    num_violations = np.sum(isis < isi_threshold)
+    violation_time = 2 * num_spikes * (isi_threshold - min_isi)
+    total_rate = spike_train.size / (max_time - min_time)
+    violation_rate = num_violations / violation_time
+    fpRate = violation_rate / total_rate
+
+    return fpRate, num_violations
+
+
+def amplitude_cutoff(amplitudes, num_histogram_bins=500, histogram_smoothing_value=3):
+    """ Calculate approximate fraction of spikes missing from a distribution of amplitudes
+
+    Assumes the amplitude histogram is symmetric (not valid in the presence of drift)
+
+    Inspired by metric described in Hill et al. (2011) J Neurosci 31: 8699-8705
+
+    Input:
+    ------
+    amplitudes : numpy.ndarray
+        Array of amplitudes (don't need to be in physical units)
+
+    Output:
+    -------
+    fraction_missing : float
+        Fraction of missing spikes (0-0.5)
+        If more than 50% of spikes are missing, an accurate estimate isn't possible
+
+    """
+
+    h, b = np.histogram(amplitudes, num_histogram_bins, density=True)
+
+    pdf = gaussian_filter1d(h, histogram_smoothing_value)
+    support = b[:-1]
+
+    peak_index = np.argmax(pdf)
+    G = np.argmin(np.abs(pdf[peak_index:] - pdf[0])) + peak_index
+
+    bin_size = np.mean(np.diff(support))
+    fraction_missing = np.sum(pdf[G:]) * bin_size
+
+    fraction_missing = np.min([fraction_missing, 0.5])
+
+    return fraction_missing
