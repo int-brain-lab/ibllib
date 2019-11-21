84894fcbb98dabb3e6d60c14a93200e2fcad11b6
mschart <michael.m.schartner@gmail.com>
2019-11-21 17:16:33 +0100

    Create DLC_pupil_event.py

---
+++ brainbox/examples/DLC_pupil_event.py
@@ -0,0 +1,211 @@
+import numpy as np
+import alf.io
+import matplotlib.pyplot as plt
+import ibllib.plots as iblplt
+from oneibl.one import ONE
+from pathlib import Path
+import csv
+import json
+plt.ion()
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+
+def add_stim_off_times(trials):
+
+    on = 'stimOn_times'
+    off = 'stimOff_times'
+    trials[off] = np.zeros(shape=trials[on].shape)
+    correct_trials = trials['feedbackType'] == 1
+    u = trials['feedback_times'][correct_trials] + 1.0
+    trials[off][correct_trials] = u
+    error_trials = trials['feedbackType'] == -1
+    v = trials['feedback_times'][error_trials] + 2.0
+    trials[off][error_trials] = v
+
+
+def plot_pupil_diameter_single_trial(
+        trial_numbers,
+        trial_number,
+        diameter,
+        times,
+        trials):
+
+    a = list(trial_numbers)
+    first = a.index(trial_number)
+    last = len(a) - 1 - a[::-1].index(trial_number)
+
+    plt.plot(times[first:last], diameter[first:last])
+
+    def restrict_timestamplist(q):
+        li = []
+        for i in q:
+            if i > times[first] and i < times[last]:
+                li.append(i)
+        return li
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['stimOn_times']), ymin=10, ymax=20,
+        color='m', linewidth=0.5, label='stimOn_times')
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['feedback_times']), ymin=10, ymax=20,
+        color='b', linewidth=0.5, label='feedback_times')
+
+    iblplt.vertical_lines(restrict_timestamplist(
+        trials['stimOff_times']), ymin=10, ymax=20,
+        color='g', linewidth=0.5, label='stimOff_times')
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('pupil diameter [px]')
+    plt.title('Trial number %s' % trial_number)
+    plt.legend()
+    plt.tight_layout()
+
+
+def convert_pgts(time):
+    """Convert PointGray cameras timestamps to seconds.
+    Use convert then uncycle"""
+    # offset = time & 0xFFF
+    cycle1 = (time >> 12) & 0x1FFF
+    cycle2 = (time >> 25) & 0x7F
+    seconds = cycle2 + cycle1 / 8000.
+    return seconds
+
+
+def uncycle_pgts(time):
+    """Unwrap the converted seconds of a PointGray camera timestamp series."""
+    cycles = np.insert(np.diff(time) < 0, 0, False)
+    cycleindex = np.cumsum(cycles)
+    return time + cycleindex * 128
+
+
+def get_pupil_diameter(alf_path):
+
+    json1_file = open(alf_path / '_ibl_leftCamera.dlc.metadata.json')
+    json1_str = json1_file.read()
+    json1_data = json.loads(json1_str)['columns']
+
+    # check order
+    assert json1_data[0] == 'pupil_top_r_x', 'Order is off!'
+    assert json1_data[11] == 'pupil_left_r_likelihood', 'Order is off!'
+
+    dlc = np.load(alf_path / '_ibl_leftCamera.dlc.npy')
+
+    K = {}
+    K['pupil_top_r'] = dlc[:, :3]
+    K['pupil_right_r'] = dlc[:, 3:6]
+    K['pupil_bottom_r'] = dlc[:, 6:9]
+    K['pupil_left_r'] = dlc[:, 9:12]
+
+    # Set values to nan if likelyhood is too low
+    XYs = {}
+    for part in K:
+        x = np.ma.masked_where(K[part][:, 2] < 0.9, K[part][:, 0])
+        x = x.filled(np.nan)
+        y = np.ma.masked_where(K[part][:, 2] < 0.9, K[part][:, 1])
+        y = y.filled(np.nan)
+        XYs[part] = [x, y]
+
+    # get both diameters (d1 = top - bottom, d2 = left - right)
+    d1 = ((XYs['pupil_top_r'][0] - XYs['pupil_bottom_r'][0])**2 +
+          (XYs['pupil_top_r'][1] - XYs['pupil_bottom_r'][1])**2)**0.5
+    d2 = ((XYs['pupil_left_r'][0] - XYs['pupil_right_r'][0])**2 +
+          (XYs['pupil_left_r'][1] - XYs['pupil_right_r'][1])**2)**0.5
+    d = np.mean([d1, d2], axis=0)
+
+    return d
+
+
+def get_timestamps_from_ssv_file(alf_path):
+
+    loc = alf_path.parent.joinpath('raw_video_data/'
+                                   '_iblrig_leftCamera.timestamps.ssv')
+
+    with open(loc, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+        ssv_times = np.array([line for line in csv_reader])
+
+    return uncycle_pgts([convert_pgts(int(time)) for time in ssv_times[:, 0]])
+
+
+def plot_mean_std_around_event(event, diameter, times, eid):
+    '''
+     
+    event in {'stimOn_times', 'feedback_times', 'stimOff_times'}
+     
+    '''
+    event_times = trials[event]
+
+    window_size = 70
+
+    segments = []
+    # skip first and last trials to get same window length
+    for t in event_times[5:-5]:
+        idx = find_nearest(times, t)
+        segments.append(diameter[idx - window_size: idx + window_size])
+
+    M = np.nanmean(np.array(segments), axis=0)
+    E = np.nanstd(np.array(segments), axis=0)
+
+    fig, ax = plt.subplots()
+    ax.fill_between(
+        range(
+            len(M)),
+        M - E,
+        M + E,
+        alpha=0.5,
+        edgecolor='#CC4F1B',
+        facecolor='#FF9848')
+    plt.plot(range(len(M)), M, color='k', linewidth=3)
+    plt.axvline(x=window_size, color='r', linewidth=1, label=event)
+    plt.legend()
+    plt.ylabel('pupil diameter [px]')
+    plt.xlabel('frames')
+    plt.title(eid)
+    plt.tight_layout()
+
+# Get a raster plot
+if __name__ == "__main__":
+
+    one = ONE()
+
+    # one.list(None, 'dataset-types') # to check dataset types, 'camera.times'?
+    # one.search(dataset_types=['camera.dlc', 'camera.times'])
+    eid = '61393bca-f1ff-4e7d-b2d8-da7475219866'
+
+    D = one.load(eid)
+    D = one.load(
+        eid,
+        dataset_types=[
+            'camera.dlc',
+            '_iblrig_Camera.timestamps'],
+        dclass_output=True)
+    alf_path = Path(D.local_path[0]).parent.parent / 'alf'
+
+    trials = alf.io.load_object(alf_path, '_ibl_trials')
+    add_stim_off_times(trials)
+
+    times = np.load(alf_path / '_ibl_leftCamera.times.npy')
+
+    diameter = get_pupil_diameter(alf_path)
+
+    # get trial number for each time bin
+    trial_numbers = np.digitize(times, trials['goCue_times'])
+    print('Range of trials: ', [trial_numbers[0], trial_numbers[-1]])
+
+    # get a raster plot for a particular trial
+    # plot_pupil_diameter_single_trial(trial_numbers,
+    #                                  15, diameter, times, trials)
+
+    plot_mean_std_around_event('stimOn_times', diameter, times, eid)
+    plot_mean_std_around_event('feedback_times', diameter, times, eid)
+
+    # what's that stim-off times, are they reliable? 
+    # plot_mean_std_around_event('stimOff_times', diameter, times, eid)
+
+    plt.show()
