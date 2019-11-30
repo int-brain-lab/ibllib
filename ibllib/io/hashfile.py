00fedc5c7ba30157fb4f957236e8d26e52973199
olivier <olivier.winter@hotmail.fr>
2019-11-30 21:38:03 +0000

    io.hashfile waitbars

--- ibllib/io/hashfile.py
+++ ibllib/io/hashfile.py
@@ -1,4 +1,7 @@
 import hashlib
+import numpy as np
+
+from tqdm import tqdm
 
 BUF_SIZE = 2 ** 28  # 256 megs
 
@@ -20,9 +23,12 @@ def sha1(file_path):
 
 
 def _hash_file(file_path, hash_obj):
-    b = bytearray(128 * 1024)
+    b = bytearray(BUF_SIZE)
     mv = memoryview(b)
+    pbar = tqdm(total=np.ceil(file_path.stat().st_size / BUF_SIZE))
     with open(file_path, 'rb', buffering=0) as f:
         for n in iter(lambda: f.readinto(mv), 0):
             hash_obj.update(mv[:n])
+            pbar.update(1)
+    pbar.close()
     return hash_obj.hexdigest()
