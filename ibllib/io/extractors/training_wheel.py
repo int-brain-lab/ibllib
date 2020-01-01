e3dd9a89b5c058e7dc48061fd34a5dc78852deb3
olivier <olivier.winter@hotmail.fr>
2020-01-01 13:38:00 +0000

    detection of bpod units mismatch needs to handle large time differences between bpod and rotary encoder

--- ibllib/io/extractors/training_wheel.py
+++ ibllib/io/extractors/training_wheel.py
@@ -59,7 +59,7 @@ def sync_rotary_encoder(session_path, bpod_data=None, re_events=None):
                                  ['closed_loop'][0][0] for tr in bpod_data]),
     }
     # bpod bug that spits out events in ms instead of us
-    if bpod['closed_loop'][-1] / rote['closed_loop'][-1] > 900:
+    if np.diff(bpod['closed_loop'][[-1, 0]])[0] / np.diff(rote['closed_loop'][[-1, 0]])[0] > 900:
         logger_.error("Rotary encoder stores values in ms instead of us. Wheel timing inaccurrate")
         rote['stim_on'] *= 1e3
         rote['closed_loop'] *= 1e3
