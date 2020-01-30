e82b48be93e023623b9b1bea61fe93c12e842a59
Guido Meijer <gtmeijer@gmail.com>
2020-01-30 19:40:25 +0000

    Code update

--- brainbox/task/task.py
+++ brainbox/task/task.py
@@ -3,7 +3,7 @@ Computes task related output
 '''
 
 import numpy as np
-from scipy.stats import ranksums
+from scipy.stats import ranksums, sem, t
 from sklearn.metrics import roc_auc_score
 from brainbox.core import Bunch
 from brainbox.population import xcorr
@@ -25,7 +25,7 @@ def _get_spike_counts_in_bins(spike_times, spike_clusters, intervals):
     Returns
     ---------
     counts : 2D array of shape (n_neurons, n_events)
-        the spike counts of all neurons for all events
+        the spike counts of all neurons ffrom scipy.stats import sem, tor all events
         value (i, j) is the number of spikes of neuron `neurons[i]` in interval #j
     neuron_ids : 1D array
         list of neuron ids
@@ -52,7 +52,7 @@ def _get_spike_counts_in_bins(spike_times, spike_clusters, intervals):
 
 def responsive_units(spike_times, spike_clusters, event_times, pre_time=0.5, post_time=0.5):
     """
-    Determine responsive neurons by doing a Wilcoxon's Signed Rank test between a baseline period
+    Determine responsive neurons by doingfrom scipy.stats import sem, t a Wilcoxon's Signed Rank test between a baseline period
     before a certain task event (e.g. stimulus onset) and a period after the task event
 
     Parameters
@@ -98,21 +98,24 @@ def responsive_units(spike_times, spike_clusters, event_times, pre_time=0.5, pos
 
 
 def calculate_roc(spike_times, spike_clusters, event_times, event_groups,
-                  pre_time=0, post_time=0.25):
+                  pre_time=0, post_time=0.25, bootstrap=False, n_bootstrap=1000):
     """
     Calcluate area under the ROC curve that indicates how well the activity of the neuron
-    distiguishes between the events from the two groups. A value of 0.5 indicates no distinction
-    and a value of 1 indicates maximum distinction
+    distiguishes between two events (e.g. movement to the right vs left). A value of 0.5 indicates
+    the neuron cannot distiguish between the two events. A value of 0 or 1 indicates maximum
+    distinction. Significance is determined by bootstrapping the ROC curves. If 0.5 is not
+    included in the 95th percentile of the bootstrapped distribution, the neuron is deemed
+    to be significant.
 
     Parameters
     ----------
-    spike_times : ndarray
+    spike_times : 1D array
         spike times (in seconds)
-    spike_clusters : ndarray
+    spike_clusters : 1D array
         cluster ids corresponding to each event in `spikes`
-    event_times : ndarray
+    event_times : 1D array
         times (in seconds) of the events from the two groups
-    event_groups : ndarray
+    event_groups : 1D array
         group identities of the events as either 0 or 1
     pre_time : float
         time (in seconds) to precede the event times
@@ -121,17 +124,35 @@ def calculate_roc(spike_times, spike_clusters, event_times, event_groups,
 
     Returns
     -------
-    auc_roc : ndarray
+    auc_roc : 1D array
         an array of the area under the ROC curve for every neuron
+    cluster_ids : 1D array
+        cluster ids of the AUC values
+    significant_units : 1D array
+        neurons which show significant discrimination between the two events
+        (only works with bootstrap=True)
     """
 
     # Get spike counts
     times = np.column_stack(((event_times - pre_time), (event_times + post_time)))
     spike_counts, cluster_ids = _get_spike_counts_in_bins(spike_times, spike_clusters, times)
 
-    # Calculate roc
+    # Calculate area under the ROC curve per neuron
     auc_roc = np.empty(spike_counts.shape[0])
+    significant_units = np.empty(0)
     for i in range(spike_counts.shape[0]):
-        auc_roc[i], roc_auc_score(event_groups, spike_counts)
+        auc_roc[i] = roc_auc_score(event_groups, spike_counts[i, :])
 
-    return auc_roc
+        if bootstrap is True:
+            # Bootstrap ROC curves
+            auc_roc_bootstrap = np.empty(n_bootstrap)
+            for j in range(n_bootstrap):
+                this_sample = np.random.choice(np.arange(len(event_groups)),
+                                               int(len(event_groups)/2))
+                auc_roc_bootstrap[j] = roc_auc_score(event_groups[this_sample],
+                                                     spike_counts[i, this_sample])
+            if not ((np.percentile(auc_roc_bootstrap, 5) < 0.5)
+                    and (np.percentile(auc_roc_bootstrap, 95) > 0.5)):
+                significant_units = np.append(significant_units, cluster_ids[i])
+
+    return auc_roc, cluster_ids, significant_units
