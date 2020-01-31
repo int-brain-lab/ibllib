7b26c191b35626c44d5089919d551ca7e12364cf
Guido Meijer <gtmeijer@gmail.com>
2020-01-31 13:16:47 +0000

    Added differentiate neurons function

--- brainbox/task/task.py
+++ brainbox/task/task.py
@@ -3,10 +3,9 @@ Computes task related output
 '''
 
 import numpy as np
-from scipy.stats import ranksums, sem, t
+from scipy.stats import ranksums, wilcoxon, ttest_ind, ttest_rel
+from statsmodels.stats.multitest import multipletests
 from sklearn.metrics import roc_auc_score
-from brainbox.core import Bunch
-from brainbox.population import xcorr
 
 
 def _get_spike_counts_in_bins(spike_times, spike_clusters, intervals):
@@ -50,10 +49,11 @@ def _get_spike_counts_in_bins(spike_times, spike_clusters, intervals):
     return counts, neuron_ids
 
 
-def responsive_units(spike_times, spike_clusters, event_times, pre_time=0.5, post_time=0.5):
+def responsive_units(spike_times, spike_clusters, event_times,
+                     pre_time=0.5, post_time=0.5, alpha=0.05):
     """
-    Determine responsive neurons by doingfrom scipy.stats import sem, t a Wilcoxon's Signed Rank test between a baseline period
-    before a certain task event (e.g. stimulus onset) and a period after the task event
+    Determine responsive neurons by doing a Wilcoxon Signed-Rank test between a baseline period
+    before a certain task event (e.g. stimulus onset) and a period after the task event.
 
     Parameters
     ----------
@@ -67,6 +67,8 @@ def responsive_units(spike_times, spike_clusters, event_times, pre_time=0.5, pos
         time (in seconds) to precede the event times to get the baseline
     post_time : float
         time (in seconds) to follow the event times
+    alpha : float
+        alpha to use for statistical significance
 
     Returns
     -------
@@ -91,14 +93,93 @@ def responsive_units(spike_times, spike_clusters, event_times, pre_time=0.5, pos
         if (np.sum(baseline_counts[i, :]) == 0) and (np.sum(spike_counts[i, :]) == 0):
             p_values[i] = 1
         else:
-            _, p_values[i] = ranksums(baseline_counts[i, :], spike_counts[i, :])
-    significant_units = cluster_ids[p_values < 0.05]
+            _, p_values[i] = wilcoxon(baseline_counts[i, :], spike_counts[i, :])
+
+    # Perform FDR correction for multiple testing
+    sig_units, p_values, _, _ = multipletests(p_values, alpha)
+    significant_units = cluster_ids[sig_units]
+
+    return significant_units, p_values, cluster_ids
+
+
+def differentiate_units(spike_times, spike_clusters, event_times, event_groups,
+                        pre_time=0, post_time=0.5, test='ranksums', alpha=0.05):
+    """
+    Determine units which significantly differentiate between two task events
+    (e.g. stimulus left/right) by performing a statistical test between the spike rates
+    elicited by the two events. Default is a Wilcoxon Rank Sum test.
+
+    Parameters
+    ----------
+    spike_times : 1D array
+        spike times (in seconds)
+    spike_clusters : 1D array
+        cluster ids corresponding to each event in `spikes`
+    event_times : 1D array
+        times (in seconds) of the events from the two groups
+    event_groups : 1D array
+        group identities of the events as either 0 or 1
+    pre_time : float
+        time (in seconds) to precede the event times to get the baseline
+    post_time : float
+        time (in seconds) to follow the event times
+    test : string
+        which statistical test to use, options are:
+            'ranksums'      Wilcoxon Rank Sums test
+            'signrank'      Wilcoxon Signed Rank test (for paired observations)
+            'ttest'         independent samples t-test
+            'paired_ttest'  paired t-test
+    alpha : float
+        alpha to use for statistical significance
+
+    Returns
+    -------
+    significant_units : ndarray
+        an array with the indices of clusters that are significatly modulated
+    p_values : ndarray
+        the p-values of all the clusters
+    cluster_ids : ndarray
+        cluster ids of the p-values
+    """
+
+    # Check input
+    assert test in ['ranksums', 'signrank', 'ttest', 'paired_ttest']
+    if (test == 'signrank') or (test == 'paired_ttest'):
+        assert np.sum(event_groups == 0) == np.sum(event_groups == 1), \
+            'For paired tests the number of events in both groups needs to be the same'
+
+    # Get spike counts for the two events
+    times_1 = np.column_stack(((event_times[event_groups == 0] - pre_time),
+                               (event_times[event_groups == 0] + post_time)))
+    counts_1, cluster_ids = _get_spike_counts_in_bins(spike_times, spike_clusters, times_1)
+    times_2 = np.column_stack(((event_times[event_groups == 1] - pre_time),
+                               (event_times[event_groups == 1] + post_time)))
+    counts_2, cluster_ids = _get_spike_counts_in_bins(spike_times, spike_clusters, times_2)
+
+    # Do statistics
+    p_values = np.empty(len(cluster_ids))
+    for i in range(len(cluster_ids)):
+        if (np.sum(counts_1[i, :]) == 0) and (np.sum(counts_2[i, :]) == 0):
+            p_values[i] = 1
+        else:
+            if test == 'ranksums':
+                _, p_values[i] = ranksums(counts_1[i, :], counts_2[i, :])
+            elif test == 'signrank':
+                _, p_values[i] = wilcoxon(counts_1[i, :], counts_2[i, :])
+            elif test == 'ttest':
+                _, p_values[i] = ttest_ind(counts_1[i, :], counts_2[i, :])
+            elif test == 'paired_ttest':
+                _, p_values[i] = ttest_rel(counts_1[i, :], counts_2[i, :])
+
+    # Perform FDR correction for multiple testing
+    sig_units, p_values, _, _ = multipletests(p_values, alpha)
+    significant_units = cluster_ids[sig_units]
 
     return significant_units, p_values, cluster_ids
 
 
 def calculate_roc(spike_times, spike_clusters, event_times, event_groups,
-                  pre_time=0, post_time=0.25, bootstrap=False, n_bootstrap=1000):
+                  pre_time=0, post_time=0.25):
     """
     Calcluate area under the ROC curve that indicates how well the activity of the neuron
     distiguishes between two events (e.g. movement to the right vs left). A value of 0.5 indicates
@@ -128,9 +209,6 @@ def calculate_roc(spike_times, spike_clusters, event_times, event_groups,
         an array of the area under the ROC curve for every neuron
     cluster_ids : 1D array
         cluster ids of the AUC values
-    significant_units : 1D array
-        neurons which show significant discrimination between the two events
-        (only works with bootstrap=True)
     """
 
     # Get spike counts
@@ -139,20 +217,7 @@ def calculate_roc(spike_times, spike_clusters, event_times, event_groups,
 
     # Calculate area under the ROC curve per neuron
     auc_roc = np.empty(spike_counts.shape[0])
-    significant_units = np.empty(0)
     for i in range(spike_counts.shape[0]):
         auc_roc[i] = roc_auc_score(event_groups, spike_counts[i, :])
 
-        if bootstrap is True:
-            # Bootstrap ROC curves
-            auc_roc_bootstrap = np.empty(n_bootstrap)
-            for j in range(n_bootstrap):
-                this_sample = np.random.choice(np.arange(len(event_groups)),
-                                               int(len(event_groups)/2))
-                auc_roc_bootstrap[j] = roc_auc_score(event_groups[this_sample],
-                                                     spike_counts[i, this_sample])
-            if not ((np.percentile(auc_roc_bootstrap, 5) < 0.5)
-                    and (np.percentile(auc_roc_bootstrap, 95) > 0.5)):
-                significant_units = np.append(significant_units, cluster_ids[i])
-
-    return auc_roc, cluster_ids, significant_units
+    return auc_roc, cluster_ids
