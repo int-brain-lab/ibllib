d328dd33abafd12f1588bb7264c5ded391c0580a
Berk Gercek <gercek.berk@gmail.com>
2020-02-05 19:07:13 +0100

    Added browser icons and favicon for docs site

--- brainbox/docs/conf.py
+++ brainbox/docs/conf.py
@@ -90,6 +90,7 @@ html_theme_options = {
 html_theme_path = sphinx_material.html_theme_path()
 html_context = sphinx_material.get_html_context()
 html_logo = '_static/IBL_b_n_w.png'
+html_favicon = '_static/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
