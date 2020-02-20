7b2a6bc8b6d407294577c389fc9b6f2fb1665a03
olivier <olivier.winter@hotmail.fr>
2020-02-20 13:31:49 +0000

    brainbox io spikesorting functions

--- brainbox/io/one.py
+++ brainbox/io/one.py
@@ -1,10 +1,15 @@
 import alf.io
 from oneibl.one import ONE
 
-one = ONE()
+
+def load_ephys_session(eid, one=None, dataset_types=None):
+    spikes, clusters = load_spike_sorting(eid, one=None, dataset_types=None)
+    trials = one.load_object(eid, obj='trials')
+
+    return spikes, clusters, trials
 
 
-def load_spike_sorting(eid, dataset_types=None):
+def load_spike_sorting(eid, one=None, dataset_types=None):
     """
     From an eid, hits the Alyx database and downloads a standard set of dataset types to perform
     analysis.
@@ -12,6 +17,8 @@ def load_spike_sorting(eid, dataset_types=None):
     :param dataset_types: additional spikes/clusters objects to add to the standard list
     :return:
     """
+    if not one:
+        one = ONE()
     # This is a first draft, no safeguard, no error handling and a draft dataset list.
     session_path = one.path_from_eid(eid)
     dtypes = [
