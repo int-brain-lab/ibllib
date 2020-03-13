aec8441a6764e449d8263d9e7a3e78d840b64607
Guido Meijer <gtmeijer@gmail.com>
2020-03-13 10:59:24 +0000

    Added classifier function

--- brainbox/population/population.py
+++ brainbox/population/population.py
@@ -2,9 +2,56 @@
 Population functions.
 
 Code from https://github.com/cortex-lab/phylib/blob/master/phylib/stats/ccg.py by C. Rossant.
+Code for decoding by G. Meijer
 '''
 
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold, LeaveOneOut
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, roc_auc_score
+
+
+def _get_spike_counts_in_bins(spike_times, spike_clusters, intervals):
+    """
+    Return the number of spikes in a sequence of time intervals, for each neuron.
+
+    Parameters
+    ----------
+    spike_times : 1D array
+        spike times (in seconds)
+    spike_clusters : 1D array
+        cluster ids corresponding to each event in `spikes`
+    intervals : 2D array of shape (n_events, 2)
+        the start and end times of the events
+
+    Returns
+    ---------
+    counts : 2D array of shape (n_neurons, n_events)
+        the spike counts of all neurons ffrom scipy.stats import sem, tor all events
+        value (i, j) is the number of spikes of neuron `neurons[i]` in interval #j
+    cluster_ids : 1D array
+        list of cluster ids
+    """
+
+    # Check input
+    assert intervals.ndim == 2
+    assert intervals.shape[1] == 2
+
+    # For each neuron and each interval, the number of spikes in the interval.
+    cluster_ids = np.unique(spike_clusters)
+    n_neurons = len(cluster_ids)
+    n_intervals = intervals.shape[0]
+    counts = np.zeros((n_neurons, n_intervals), dtype=np.uint32)
+    for j in range(n_intervals):
+        t0, t1 = intervals[j, :]
+        # Count the number of spikes in the window, for each neuron.
+        x = np.bincount(
+            spike_clusters[(t0 <= spike_times) & (spike_times < t1)],
+            minlength=cluster_ids.max() + 1)
+        counts[:, j] = x[cluster_ids]
+    return counts, cluster_ids
 
 
 def _index_of(arr, lookup):
@@ -139,3 +186,130 @@ def xcorr(spike_times, spike_clusters, bin_size=None, window_size=None):
         shift += 1
 
     return _symmetrize_correlograms(correlograms)
+
+
+def decode(spike_times, spike_clusters, event_times, event_groups,
+           pre_time=0, post_time=0.5, classifier='bayes', num_splits=5, iterations=1):
+    """
+    Use decoding to classify groups of trials (e.g. stim left/right). Classification is done using
+    the population vector of summed spike counts from the specified time window. Cross-validation
+    is achieved using n-fold cross validation or leave-one-out cross validation. Decoders can
+    decode any number of groups. When providing the classfier with an imbalanced dataset (not
+    the same number of trials in each group) the chance level will not be 1/groups. In that case,
+    to compare the classification performance against change one has to either determine chance
+    level by decoding a shuffled dataset or use the 'auroc' metric as readout (this metric is
+    robust against imbalanced datasets)
+
+    Parameters
+    ----------
+    spike_times : 1D array
+        spike times (in seconds)
+    spike_clusters : 1D array
+        cluster ids corresponding to each event in `spikes`
+    event_times : 1D array
+        times (in seconds) of the events from the two groups
+    event_groups : 1D array
+        group identities of the events, can be any number of groups, accepts integers and strings
+    pre_time : float
+        time (in seconds) to precede the event times to get the baseline
+    post_time : float
+        time (in seconds) to follow the event times
+    classifier : string
+        which decoder to use, options are:
+            'bayes'         Naive Bayes
+            'forest'        Random forest (with 100 trees)
+            'regression'    Logistic regression
+    num_splits : integer
+        Number of splits to use for n-fold cross validation, a value of 5 means that the decoder
+        will be trained on 4/5th of the data and used to predict the remaining 1/5th. This process
+        is repeated five times so that all data has been used as both training and test set.
+        To use leave-one-out cross validation use num_splits = 1
+    iterations : integer
+        How often to repeat the n-fold cross validation. Randomly splitting the data into train
+        and test sets can introduce small discrepencies in the decoding performance depending on
+        where the splits are made. This variability is reduced by repeating the cross validation
+        several times and taking the mean of the resulting classification performances.
+        This number is ignored during leave-one-out cross validation
+
+    Returns
+    -------
+    accuracy : float
+        the accuracy of the classifier
+    F1 : float
+        F1 score of the classifier
+    auroc : float
+        the area under the ROC curve of the classification performance
+    """
+
+    # Check input
+    assert classifier in ['bayes', 'forest', 'regression']
+    assert event_times.shape[0] == event_groups.shape[0]
+
+    # Get matrix of all neuronal responses
+    times = np.column_stack(((event_times - pre_time), (event_times + post_time)))
+    pop_vector, cluster_ids = _get_spike_counts_in_bins(spike_times, spike_clusters, times)
+    pop_vector = np.rot90(pop_vector)
+
+    # Initialize classifier
+    if classifier == 'forest':
+        clf = RandomForestClassifier(n_estimators=100)
+    elif classifier == 'bayes':
+        clf = GaussianNB()
+    elif classifier == 'regression':
+        clf = LogisticRegression(solver='liblinear', multi_class='auto')
+
+    # Initialize cross-validation
+    if num_splits == 1:
+        cv = LeaveOneOut()
+        iterations = 1  # only classify once when using leave-one-out
+    else:
+        cv = KFold(n_splits=num_splits, shuffle=True)
+
+    # Pre-allocate variables
+    acc_all = np.zeros(iterations)
+    f1_all = np.zeros(iterations)
+    auroc_all = np.zeros(iterations)
+    cm_all = np.zeros((np.shape(np.unique(event_groups))[0], np.shape(np.unique(event_groups))[0],
+                       iterations))
+
+    # Classify several times, determined by iterations
+    for i in range(iterations):
+
+        # Pre-allocate variables for this iteration
+        y_pred = np.zeros(event_groups.shape)
+        y_probs = np.zeros(event_groups.shape)
+
+        # Loop over the splits into train and test
+        for train_index, test_index in cv.split(pop_vector):
+
+            # Fit the model to the training data
+            clf.fit(pop_vector[train_index], [event_groups[j] for j in train_index])
+
+            # Predict the test data
+            y_pred[test_index] = clf.predict(pop_vector[test_index])
+
+            # Get the probability of the prediction for ROC analysis
+            probs = clf.predict_proba(pop_vector[test_index])
+            y_probs[test_index] = probs[:, 1]  # keep positive only
+
+        # Calculate performance metrics and confusion matrix
+        acc_all[i] = accuracy_score(event_groups, y_pred)
+        f1_all[i] = f1_score(event_groups, y_pred)
+        auroc_all[i] = roc_auc_score(event_groups, y_probs)
+        conf_matrix = confusion_matrix(event_groups, y_pred)
+        cm_all[:, :, i] = conf_matrix / conf_matrix.sum(axis=1)[:, np.newaxis]  # normalize
+
+    # Add to results dictionary
+    if num_splits == 1:
+        results = dict({'accuracy': np.mean(acc_all), 'f1': np.mean(f1_all),
+                        'auroc': np.mean(auroc_all), 'confusion_matrix': np.mean(cm_all, axis=2),
+                        'n_groups': np.shape(np.unique(event_groups))[0],
+                        'classifier': classifier, 'cross_validation': 'leave-one-out'})
+    else:
+        results = dict({'accuracy': np.mean(acc_all), 'f1': np.mean(f1_all),
+                        'auroc': np.mean(auroc_all), 'confusion_matrix': np.mean(cm_all, axis=2),
+                        'n_groups': np.shape(np.unique(event_groups))[0],
+                        'classifier': classifier, 'cross_validation': '%d-fold' % num_splits,
+                        'iterations': iterations})
+
+    return results
