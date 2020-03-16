ccf0061bbd1c421b4637ecba5cde22d2d136eaaf
sbruijns <sebastian.bruijns@tuebingen.mpg.de>
2020-03-16 13:13:58 +0100

    comment about nan's for coeff of var calculation

--- brainbox/metrics/metrics.py
+++ brainbox/metrics/metrics.py
@@ -314,7 +314,7 @@ def firing_rate_coeff_var(ts, hist_win=0.01, fr_win=0.5, n_bins=10):
     # Compute coefficient of variations of firing rate for each bin, and the mean c.v.
     cvs = np.std(fr_binned, axis=1) / np.mean(fr_binned, axis=1)
     # NaNs from zero spikes are turned into 0's
-    cvs[np.isnan(cvs)] = 0
+    # cvs[np.isnan(cvs)] = 0 nan's can happen if neuron doesn't spike in a bin
     cv = np.mean(cvs)
 
     return cv, cvs, fr
