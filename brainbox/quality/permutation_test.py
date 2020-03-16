412c35a59d45223cef8de5544780da79a84486e2
sbruijns <sebastian.bruijns@tuebingen.mpg.de>
2020-03-16 13:42:51 +0100

    all the linting :(((

--- brainbox/quality/permutation_test.py
+++ brainbox/quality/permutation_test.py
@@ -70,7 +70,8 @@ def permut_test(data1, data2, metric, n_permut=1000, show=False, title=None):
         np.random.shuffle(indizes)
         permutations[i] = indizes
 
-    permut_diffs = np.abs(np.mean(diffs[permutations[:, :size1]], axis=1) -
+    permut_diffs =
+        np.abs(np.mean(diffs[permutations[:, :size1]], axis=1) -
         np.mean(diffs[permutations[:, size1:]], axis=1))
     p = len(permut_diffs[permut_diffs > true_diff]) / n_permut
 
