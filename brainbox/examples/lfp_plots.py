27de316462b82533c5343c670e6c8f7e9a64ae8d
Guido Meijer <gtmeijer@gmail.com>
2020-03-17 14:51:16 +0000

    Change loading method to spikeglx.Reader

--- brainbox/examples/lfp_plots.py
+++ brainbox/examples/lfp_plots.py
@@ -2,12 +2,13 @@ import numpy as np
 import brainbox as bb
 import alf.io as ioalf
 from oneibl.one import ONE
-
+from ibllib.io import spikeglx
 
 # Download data
 one = ONE()
 eid = one.search(subject='ZM_2240', date_range=['2020-01-22', '2020-01-22'])
-lf_path = one.load(eid[0], dataset_types=['ephysData.raw.lf'], download_only=True)[0]
-raw = ioalf.load_object(lf_path)
+lf_path = one.load(eid[0], dataset_types=['ephysData.raw.lf', 'ephysData.raw.meta'],
+                   download_only=True)[0]
+raw = spikeglx.Reader(lf_path)
 
 #
