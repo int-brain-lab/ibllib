4cbe7832d522427c6728df4613a5cba0e3e01c0a
Guido Meijer <gtmeijer@gmail.com>
2020-03-17 16:51:19 +0000

    Changed loading method

--- brainbox/examples/lfp_plots.py
+++ brainbox/examples/lfp_plots.py
@@ -6,9 +6,11 @@ from ibllib.io import spikeglx
 
 # Download data
 one = ONE()
-eid = one.search(subject='ZM_2240', date_range=['2020-01-22', '2020-01-22'])
-lf_path = one.load(eid[0], dataset_types=['ephysData.raw.lf', 'ephysData.raw.meta'],
+eid = one.search(subject='ZM_2240', date_range=['2020-01-23', '2020-01-23'])
+lf_path = one.load(eid[0], dataset_types=['ephysData.raw.lf', 'ephysData.raw.meta',
+                                          'ephysData.raw.ch'],
                    download_only=True)[0]
 raw = spikeglx.Reader(lf_path)
+data = raw.read(nsel=slice(0, 100, None))
 
 #
