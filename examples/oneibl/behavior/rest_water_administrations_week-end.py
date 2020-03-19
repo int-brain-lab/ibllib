682d94eb01842751e08d090bdbe52dc73480a189
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-03-19 13:38:51 +0100

    Made internal example function to edit water rest. on Alyx

--- examples/oneibl/behavior/rest_water_administrations_week-end.py
+++ examples/oneibl/behavior/rest_water_administrations_week-end.py
@@ -7,37 +7,51 @@ from oneibl.one import ONE
 
 one = ONE(base_url='https://dev.alyx.internationalbrainlab.org')
 
+# Define an example function to input 'Hydrogel 5% Citric Acid' with user 'valeria' on Alyx dev
+def _example_change_wa(dates, sub):
+    for dat in dates:
+        for s in sub:
+            wa_ = {
+                'subject': s,
+                'date_time': dat,
+                'water_type': 'Hydrogel 5% Citric Acid',
+                'user': 'valeria',
+                'adlib': True}
+            # Do not use the example on anything else than alyx-dev !
+            if one.alyx._base_url == 'https://dev.alyx.internationalbrainlab.org':
+                rep = one.alyx.rest('water-administrations', 'create', data=wa_)
+
+# Define date range
 dates = ['2018-11-19T12:00', '2018-11-22T12:00', '2018-11-23T12:00']
 
-# subject_details = one.alyx.rest('subjects', 'list', '?alive=True&project=ibl_neuropixel_brainwide_01&water_restricted=True')
+# --Option 1-- You can either give manually a list of subject nicknames
 
-sub = ['IBL_1',
-       'IBL_10',
-       'IBL_11',
-       'IBL_12',
-       'IBL_13',
-       'IBL_14',
-       'IBL_15',
-       'IBL_16',
-       'IBL_17',
-       'IBL_33',
-       'IBL_34',
-       'IBL_35',
-       'IBL_36',
-       'IBL_43',
-       'IBL_44',
-       'IBL_45',
-       'IBL_46',
-       'IBL_47']
+sub_manual = ['IBL_1',
+              'IBL_10',
+              'IBL_11',
+              'IBL_12',
+              'IBL_13',
+              'IBL_14',
+              'IBL_15',
+              'IBL_16',
+              'IBL_17',
+              'IBL_33',
+              'IBL_34',
+              'IBL_35',
+              'IBL_36',
+              'IBL_43',
+              'IBL_44',
+              'IBL_45',
+              'IBL_46',
+              'IBL_47']
 
-for dat in dates:
-    for s in sub:
-        wa_ = {
-            'subject': s,
-            'date_time': dat,
-            'water_type': 'Hydrogel 5% Citric Acid',
-            'user': 'valeria',
-            'adlib': True}
-        # Do not use the example on anything else than alyx-dev !
-        if one.alyx._base_url == 'https://dev.alyx.internationalbrainlab.org':
-            rep = one.alyx.rest('water-administrations', 'create', data=wa_)
+# Call function to execute change on Alyx
+_example_change_wa(dates, sub_manual)
+
+# --Option 2-- Or find all subject nicknames programmatically
+
+subjects = one.alyx.rest('subjects', 'list', alive=True, lab='zadorlab', water_restricted=True)
+sub_prog = [s['nickname'] for s in subjects]
+
+# Call function to execute change on Alyx
+_example_change_wa(dates, sub_prog)
