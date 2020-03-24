d07d48c9cbd733622a9051c3d5e2c22ebc094769
olivier <olivier.winter@hotmail.fr>
2020-03-24 23:55:12 +0000

    histology: register channel locations

--- ibllib/pipes/histology.py
+++ ibllib/pipes/histology.py
@@ -4,7 +4,6 @@ import logging
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from ibllib.ephys.neuropixel import SITES_COORDINATES
 from oneibl.one import ONE
@@ -20,27 +19,38 @@ atlas_params = {
     'INDICES_BREGMA': list(np.array([1140 - (570 + 3.9), 540, 0 + 33.2]))
 }
 # origin Allen left, front, up
-brat = atlas.AllenAtlas(res_um=25, par=atlas_params)
+brain_atlas = atlas.AllenAtlas(res_um=25, par=atlas_params)
+
+
+def load_track_csv(file_track):
+    """
+    Loads a lasagna track and convert to IBL-ALlen coordinate framework
+    :param file_track:
+    :return: xyz
+    """
+    # apmldv in the histology file is flipped along x and y directions
+    ixiyiz = np.loadtxt(file_track, delimiter=',')[:, [1, 0, 2]]
+    ixiyiz[:, 1] = 527 - ixiyiz[:, 1]
+    ixiyiz = ixiyiz[np.argsort(ixiyiz[:, 2]), :]
+    xyz = brain_atlas.bc.i2xyz(ixiyiz)
+    xyz[:, 0] = - xyz[:, 0]
+    return xyz
 
 
 def get_picked_tracks(histology_path, glob_pattern="*_pts_transformed.csv"):
     """
     This outputs reads in the Lasagna output and converts the picked tracks in the IBL coordinates
-    :param histology_path:
-    :return:
+    :param histology_path: Path object: folder path containing tracks
+    :return: xyz coordinates in
     """
     xyzs = []
-    files_track = list(histology_path.rglob(glob_pattern))
+    histology_path = Path(histology_path)
+    if histology_path.is_file():
+        files_track = [histology_path]
+    else:
+        files_track = list(histology_path.rglob(glob_pattern))
     for file_track in files_track:
-        print(file_track)
-        # apmldv in the histology file is flipped along x and y directions
-        ixiyiz = np.loadtxt(file_track, delimiter=',')[:, [1, 0, 2]]
-        ixiyiz[:, 1] = 527 - ixiyiz[:, 1]
-        ixiyiz = ixiyiz[np.argsort(ixiyiz[:, 2]), :]
-        xyz = brat.bc.i2xyz(ixiyiz)
-        xyz[:, 0] = - xyz[:, 0]
-        xyzs.append(xyz)
-
+        xyzs.append(load_track_csv(file_track))
     return {'files': files_track, 'xyz': xyzs}
 
 
@@ -93,8 +103,8 @@ def plot_merged_result(probes, tracks, index=None):
     pass
     # fig, ax = plt.subplots(1, 2)
     # # plot the atlas image
-    # brat.plot_cslice(np.mean(xyz[:, 1]) * 1e3, ax=ax[0])
-    # brat.plot_sslice(np.mean(xyz[:, 0]) * 1e3, ax=ax[1])
+    # brain_atlas.plot_cslice(np.mean(xyz[:, 1]) * 1e3, ax=ax[0])
+    # brain_atlas.plot_sslice(np.mean(xyz[:, 0]) * 1e3, ax=ax[1])
     # # plot the full tracks
     # ax[0].plot(xyz[:, 0] * 1e3, xyz[:, 2] * 1e3)
     # ax[1].plot(xyz[:, 1] * 1e3, xyz[:, 2] * 1e3)
@@ -111,14 +121,14 @@ def plot2d_all(trajectories, tracks):
     :return:
     """
     plt.figure()
-    axs = brat.plot_sslice(brat.bc.i2x(190) * 1e3, cmap=plt.get_cmap('bone'))
+    axs = brain_atlas.plot_sslice(brain_atlas.bc.i2x(190) * 1e3, cmap=plt.get_cmap('bone'))
     plt.figure()
-    axc = brat.plot_cslice(brat.bc.i2y(350) * 1e3)
+    axc = brain_atlas.plot_cslice(brain_atlas.bc.i2y(350) * 1e3)
     for xyz in tracks['xyz']:
         axc.plot(xyz[:, 0] * 1e3, xyz[:, 2] * 1e3, 'b')
         axs.plot(xyz[:, 1] * 1e3, xyz[:, 2] * 1e3, 'b')
     for trj in trajectories:
-        ins = atlas.Insertion.from_dict(trj, brain_atlas=brat)
+        ins = atlas.Insertion.from_dict(trj, brain_atlas=brain_atlas)
         xyz = ins.xyz
         axc.plot(xyz[:, 0] * 1e3, xyz[:, 2] * 1e3, 'r')
         axs.plot(xyz[:, 1] * 1e3, xyz[:, 2] * 1e3, 'r')
@@ -132,27 +142,27 @@ def plot3d_all(trajectories, tracks):
     :return:
     """
     from mayavi import mlab
-    src = mlab.pipeline.scalar_field(brat.label)
+    src = mlab.pipeline.scalar_field(brain_atlas.label)
     mlab.pipeline.iso_surface(src, contours=[0.5, ], opacity=0.3)
 
     pts = []
     for xyz in tracks['xyz']:
-        mlapdv = brat.bc.xyz2i(xyz)
+        mlapdv = brain_atlas.bc.xyz2i(xyz)
         pts.append(mlab.plot3d(mlapdv[:, 1], mlapdv[:, 0], mlapdv[:, 2], line_width=3))
 
     plt_trj = []
     for trj in trajectories:
-        ins = atlas.Insertion.from_dict(trj, brain_atlas=brat)
-        mlapdv = brat.bc.xyz2i(ins.xyz)
+        ins = atlas.Insertion.from_dict(trj, brain_atlas=brain_atlas)
+        mlapdv = brain_atlas.bc.xyz2i(ins.xyz)
         plt = mlab.plot3d(mlapdv[:, 1], mlapdv[:, 0], mlapdv[:, 2],
                           line_width=3, color=(1., 0., 1.))
         plt_trj.append(plt)
 
 
-def get_brain_regions(xyz, channels_positions=SITES_COORDINATES, brain_atlas=brat):
+def get_brain_regions(xyz, channels_positions=SITES_COORDINATES, brain_atlas=brain_atlas):
     """
     :param xyz: numpy array of 3D coordinates corresponding to a picked track or a trajectory
-    the deepest point is considered the tip.
+    the deepest point is assumed to be the tip.
     :param channels_positions:
     :param brain_atlas:
     :return:
@@ -176,7 +186,7 @@ def get_brain_regions(xyz, channels_positions=SITES_COORDINATES, brain_atlas=bra
     for m in np.arange(3):
         xyz_channels[:, m] = np.interp(channels_positions[:, 1] / 1e6,
                                        d[ind_depths], xyz[ind_depths, m])
-    brain_regions = brain_atlas.regions.get(brat.get_labels(xyz_channels))
+    brain_regions = brain_atlas.regions.get(brain_atlas.get_labels(xyz_channels))
     brain_regions['xyz'] = xyz_channels
     brain_regions['lateral'] = channels_positions[:, 0]
     brain_regions['axial'] = channels_positions[:, 1]
@@ -185,20 +195,56 @@ def get_brain_regions(xyz, channels_positions=SITES_COORDINATES, brain_atlas=bra
     Get the probe insertion from the coordinates
     """
     insertion = atlas.Insertion.from_track(xyz, brain_atlas)
-
     return brain_regions, insertion
 
 
-def extract_brain_regions(session_path, display=False):
-    # #################### edit those #############################
-    file_track = Path(f'/datadisk/scratch/electrode_01_fit.csv')
-    ses_path = Path('/datadisk/Data/Subjects/KS005/2019-08-29/001')
-    # ##############################################################
+def register_track(probe_id, picks=None, one=None):
+    """
+    Register the user picks to a probe in Alyx
+    Here we update Alyx models on the database in 3 steps
+    1) The user picks converted to IBL coordinates will be stored in the json field of the
+    corresponding probe insertion models
+    2) The trajectory computed from the histology track is created or patched
+    3) Channel locations are set in the table
+    """
+    assert one
+    brain_locations, insertion_histology = get_brain_regions(picks)
+    # 1) update the alyx models, first put the picked points in the insertion json
+    one.alyx.rest('insertions', 'partial_update',
+                  id=probe_id,
+                  data={'json': {'xyz_picks': np.int32(picks * 1e6).tolist()}})
+    # TODO: add the affine transform parameters
 
-    alf_path = ses_path.joinpath('alf')
-    channels = alf.io.load_object(alf_path, 'channels')
-    # probes = alf.io.load_object(alf_path, 'probes')  # this will be needed to compare planned
-    DISPLAY = True
+    # 2) patch or create the trajectory coming from histology track
+    tdict = {'probe_insertion': probe_id,
+             'x': insertion_histology.x * 1e6,
+             'y': insertion_histology.y * 1e6,
+             'z': insertion_histology.z * 1e6,
+             'phi': insertion_histology.phi,
+             'theta': insertion_histology.theta,
+             'depth': insertion_histology.depth * 1e6,
+             'roll': insertion_histology.beta,
+             'provenance': 'Histology track',
+             'coordinate_system': 'IBL-Allen',
+             }
+    hist_traj = one.alyx.rest('trajectories', 'list',
+                              probe_insertion=probe_id,
+                              provenance='Histology track')
+    # if the trajectory exists, remove it, this will cascade delete existing channel locations
+    if len(hist_traj):
+        one.alyx.rest('trajectories', 'delete', id=hist_traj[0]['id'])
+    hist_traj = one.alyx.rest('trajectories', 'create', data=tdict)
 
-    brain_regions, track_fit = get_brain_regions(file_track, channels, display=DISPLAY)
-    pd.DataFrame.from_dict(brain_regions).to_csv()
+    # 3) create channel locations
+    channel_dict = []
+    for i in np.arange(brain_locations.id.size):
+        channel_dict.append({
+            'x': brain_locations.xyz[i, 0] * 1e6,
+            'y': brain_locations.xyz[i, 1] * 1e6,
+            'z': brain_locations.xyz[i, 2] * 1e6,
+            'axial': brain_locations.axial[i],
+            'lateral': brain_locations.lateral[i],
+            'brain_region': int(brain_locations.id[i]),
+            'trajectory_estimate': hist_traj['id']
+        })
+    one.alyx.rest('channels', 'create', data=channel_dict)
