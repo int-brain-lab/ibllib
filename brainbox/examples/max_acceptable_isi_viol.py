efc274358f0915faef35cd87fc8e8317074238a8
noamroth <39137729+noamroth@users.noreply.github.com>
2020-03-25 22:16:21 -0700

    changed to flat maximum cont acceptable without poisson rate

--- brainbox/examples/max_acceptable_isi_viol.py
+++ brainbox/examples/max_acceptable_isi_viol.py
@@ -12,21 +12,6 @@ import brainbox as bb
 def max_acceptable_cont(FR, RP, rec_duration,acceptableCont, thresh ):
     trueContRate = np.linspace(0,FR,100)
     timeForViol = RP*2*(FR-acceptableCont)*rec_duration
-    expectedCountAtFullCont = FR*timeForViol
-    obsContCountVec = np.arange(0,expectedCountAtFullCont,1)
+    max_acceptable = acceptableCont*timeForViol
     
-    probUnaccept=np.empty(len(obsContCountVec))
-    obsidx=0
-    for obsContCount in obsContCountVec:
-        pObs = poisson.pmf(obsContCount,trueContRate*timeForViol)
-        pObsNorm = pObs/sum(pObs)*100
-        probUnaccept[obsidx]=sum(pObsNorm[np.where(trueContRate>acceptableCont)])
-        obsidx +=1
-   
-    
-    if(np.where(probUnaccept<thresh)[0].size == 0):
-        max_acceptable = -1 #if there are no places where the probability is below the acc
-    else:     
-        max_acceptable_ind = np.where(probUnaccept/100<thresh)[0][-1]
-        max_acceptable = obsContCountVec[max_acceptable_ind]
     return max_acceptable
