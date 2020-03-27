40c6c7169ec5954e83c71350073226586765da5a
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-03-27 11:44:05 +0100

    WIP how to return spike data

--- examples/one/ephys/get_spikeData.py
+++ examples/one/ephys/get_spikeData.py
@@ -6,7 +6,8 @@ Get spikes data for all probes in a single session via ONE and brainbox.
 from oneibl.one import ONE
 import alf.io as aio
 import brainbox as bb
-import os
+import brainbox.io.one as bbone
+# import os
 
 one = ONE()
 
@@ -15,7 +16,7 @@ eid = 'da188f2c-553c-4e04-879b-c9ea2d1b9a93'
 
 # --- Download spikes data
 # 1. either a specific subset of dataset types via the one command
-# 2. either the whole spikes object via the #TODO command
+# 2. either the whole spikes object via the one
 
 # Option 1
 dataset_types = ['spikes.times',
@@ -24,7 +25,8 @@ one.load(eid, dataset_types=dataset_types)
 session_path = one.path_from_eid(eid)
 alf_dir = os.path.join(session_path, 'alf')
 
-# Option 2
+# Option 2 -- Download and load into memory
+spks_b1 = one.load_object(eid, 'spikes')  # TODO DOES NOT WORK for multiple probes -- which probe
 
 
 # --- Get probe directory either by
@@ -38,8 +40,12 @@ n_probe = len(prob_des[0])
 i_probe = 1
 label1 = prob_des[0][i_probe].get('label')
 
-probe_dir = os.path.join(alf_dir, label1)
+probe_dir = os.path.join(alf_dir, label1) # session_path.joinpath('alf', 'label1')
 
 # Make bunch per probe using brainbox
 spks_b = aio.load_object(probe_dir, 'spikes')
 units_b = bb.processing.get_units_bunch(spks_b)
+
+# TODO list of bunch for several probes
+
+dic_spk_bunch, dic_clus = bbone.load_spike_sorting(eid, one=one, dataset_types=dataset_types)
