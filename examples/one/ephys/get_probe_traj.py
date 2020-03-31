487b296880ab778ada5ee5881f7970da60ed0a79
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-03-31 02:34:48 +0200

    flake8, TODO in get_spiking_data

--- examples/one/ephys/get_probe_traj.py
+++ examples/one/ephys/get_probe_traj.py
@@ -10,12 +10,10 @@ one = ONE()
 # proj_list = [p['name'] for p in proj]
 traj = one.alyx.rest('trajectories', 'list', provenance='Planned',
                      x=-2243, y=-2000)  # repeated site coordinate
-                     # TODO add filter: project='ibl_neuropixel_brainwide_01'
-
+# TODO add filter: project='ibl_neuropixel_brainwide_01'
 
 # Display subjects names
 sess = [p['session'] for p in traj]
 sub = [p['subject'] for p in sess]
 
 print(f'Subject: {sub}')
-
--- examples/one/ephys/get_spikeData.py
+++ examples/one/ephys/get_spikeData.py
@@ -1,5 +1,7 @@
 '''
 Get spikes data for all probes in a single session via ONE and brainbox.
+TODO return list of bunch via one.load_object and bbone.load_spike_sorting
+TODO clarify what loading method to use between the two
 '''
 # Author: Gaelle Chapuis
 
@@ -7,7 +9,6 @@ from oneibl.one import ONE
 import alf.io as aio
 import brainbox as bb
 import brainbox.io.one as bbone
-# import os
 
 one = ONE()
 
@@ -18,29 +19,32 @@ eid = 'da188f2c-553c-4e04-879b-c9ea2d1b9a93'
 # 1. either a specific subset of dataset types via the one command
 # 2. either the whole spikes object via the one
 
-# Option 1
+# Option 1 -- Download only subset of dataset in spike object
 dataset_types = ['spikes.times',
                  'spikes.clusters']
 one.load(eid, dataset_types=dataset_types)
-session_path = one.path_from_eid(eid)
-alf_dir = os.path.join(session_path, 'alf')
 
-# Option 2 -- Download and load into memory
-spks_b1 = one.load_object(eid, 'spikes')  # TODO DOES NOT WORK for multiple probes -- which probe
+# Option 2 -- Download and load into memory the whole spikes object
+spks_b1 = one.load_object(eid, 'spikes')
+# TODO OUTPUT DOES NOT WORK for multiple probes,  which probe returned unknown
+# TODO return list of bunch
 
 
-# --- Get probe directory either by
+# --- Get single probe directory filename either by
 # 1. getting probe description in alf
 # 2. using alyx rest end point
 
 # Option 1.
-prob_des = one.load(eid, dataset_types='probes.description')
+prob_des = one.load(eid, dataset_types=['probes.description'])
 n_probe = len(prob_des[0])
 # i_probe can be 0:n_probe-1 ; in this example = 1 (2 probes)
 i_probe = 1
 label1 = prob_des[0][i_probe].get('label')
 
-probe_dir = os.path.join(alf_dir, label1) # session_path.joinpath('alf', 'label1')
+
+# -- Set single probe directory path
+session_path = one.path_from_eid(eid)
+probe_dir = session_path.joinpath('alf', label1)
 
 # Make bunch per probe using brainbox
 spks_b = aio.load_object(probe_dir, 'spikes')
