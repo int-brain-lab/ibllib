04f63337621877494d891c275665422d171f746b
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-03-31 17:50:44 +0200

    Get mice certif > dataset type

--- examples/one/ephys/get_list_mice_certif.py
+++ examples/one/ephys/get_list_mice_certif.py
@@ -6,5 +6,20 @@ Get list of subjects associated to the certification recording project.
 from oneibl.one import ONE
 one = ONE()
 
-eid, det = one.search(project='ibl_certif_neuropix_recording', details=True)
+dataset_types = ['spikes.times',
+                 'spikes.clusters']
+
+eid, det = one.search(project='ibl_certif_neuropix_recording',
+                      dataset_types=dataset_types,  details=True )
 sub = [p['subject'] for p in det]
+sub_unique = list(set(sub))
+
+lab = [p['lab'] for p in det]
+lab_unique = list(set(lab))
+
+task = [p['task_protocol'] for p in det]
+task_unique = list(set(task))
+
+# How many recording sessions were done per lab
+
+# How many animals were used per lab
