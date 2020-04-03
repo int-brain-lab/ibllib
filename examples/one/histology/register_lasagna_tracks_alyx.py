f5934f4b64d9aa790383e5040dc6d37f64f463d5
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-04-03 13:49:25 +0200

    Edited docstring for users

--- examples/one/histology/register_lasagna_tracks_alyx.py
+++ examples/one/histology/register_lasagna_tracks_alyx.py
@@ -1,13 +1,40 @@
+'''
+Register single histology track session.
+
+All your tracks should be in a single folder, and the files names should follow the nomenclature
+{yyyy-mm-dd}_{SubjectName}_{SessionNumber}_{ProbeLabel}_pts.csv
+
+Edit only the variable 'path_tracks'(set it either to your local directory [example given here],
+either to the Google folder if synched to your machine).
+
+To check if the registration worked, go to the admin interface
+> experiments > trajectory estimates > search for the subject
+
+If you want to test first, use  ALYX_URL = "https://dev.alyx.internationalbrainlab.org"
+And check on:
+https://dev.alyx.internationalbrainlab.org/admin/experiments/trajectoryestimate/?
+
+When ready, change to the       ALYX_URL = "https://alyx.internationalbrainlab.org"
+
+'''
+# Author: Olivier Winter
 from pathlib import Path
 from ibllib.pipes import histology
 
 from oneibl.one import ONE
-path_tracks = "/Users/gaelle/Downloads/electrodetracks"
-glob_pattern = "*_probe*_pts*.csv"
 
+# ======== EDIT FOR USERS ====
+
+# Edit so as to reflect the directory containing your electrode tracks
+path_tracks = "/Users/gaelle/Downloads/electrodetracks_lic3"
+
+ALYX_URL = "https://dev.alyx.internationalbrainlab.org"  # FOR TESTING
+# ALYX_URL = "https://alyx.internationalbrainlab.org"  # UNCOMMENT WHEN READY
+
+
+# ======== DO NOT EDIT BELOW ====
 # ALYX_URL = "http://localhost:8000"
-ALYX_URL = "https://dev.alyx.internationalbrainlab.org"
-# ALYX_URL = "https://alyx.internationalbrainlab.org"
+glob_pattern = "*_probe*_pts*.csv"
 
 one = ONE(base_url=ALYX_URL)
 
