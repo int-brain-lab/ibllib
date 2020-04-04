5039b7e9b73b45b852a471fa543a614759b0ec19
noamroth <39137729+noamroth@users.noreply.github.com>
2020-04-03 17:13:54 -0700

    added tsv outputs for each metric separately

--- brainbox/examples/defined_metrics.py
+++ brainbox/examples/defined_metrics.py
@@ -49,28 +49,22 @@ def FP_RP(ts):
     
 def noise_cutoff(amps,quartile_length=.25,std_cutoff = 2):
         nbins = 500
-        bins_list= np.linspace(0, np.max(amps), nbins)
-        n,bins,patches = plt.hist(amps,bins = bins_list ,facecolor = 'blue',alpha = 0.5)
-        # plt.xticks(np.arange(10,40,5))
-        # plt.yticks(np.arange(0,1000,200))
-        # plt.xlim(10,37)
-        # plt.ylim(0, 200)
-        # plt.show()
-               
-        dx = np.diff(n)
-        
-        idx_nz = np.nonzero(dx) #indices of nonzeros
-        length_nonzeros = idx_nz[0][-1]-idx_nz[0][0] #length of the entire stretch, from first nonzero to last nonzero
-        high_quartile = 1-quartile_length
-        high_quartile_start_ind = int(np.ceil(high_quartile*(length_nonzeros)))
-        high_quartile_end_ind = idx_nz[0][-1]
-        mean_high_quartile = np.mean(dx[high_quartile_start_ind:high_quartile_end_ind])
-        std_high_quartile = np.std(dx[high_quartile_start_ind:high_quartile_end_ind])
-        
-        first_low_quartile = dx[idx_nz[0][0]]
-        #statistical test? can ask whether this is within 2 std's of high quartile dx's
-        within_2stds = first_low_quartile<mean_high_quartile + std_cutoff*std_high_quartile or first_low_quartile<mean_high_quartile - std_cutoff*std_high_quartile
-        cutoff = 0 if within_2stds else 1
+        if(len(amps)>0):
+            bins_list= np.linspace(0, np.max(amps), nbins)
+            n,bins = np.histogram(amps,bins = bins_list) 
+            dx = np.diff(n) 
+            idx_nz = np.nonzero(dx) #indices of nonzeros
+            length_nonzeros = idx_nz[0][-1]-idx_nz[0][0] #length of the entire stretch, from first nonzero to last nonzero
+            high_quartile = 1-quartile_length
+            high_quartile_start_ind = int(np.ceil(high_quartile*(length_nonzeros)))
+            high_quartile_end_ind = idx_nz[0][-1]
+            mean_high_quartile = np.mean(dx[high_quartile_start_ind:high_quartile_end_ind])
+            std_high_quartile = np.std(dx[high_quartile_start_ind:high_quartile_end_ind])
+            first_low_quartile = dx[idx_nz[0][0]]
+            within_2stds = first_low_quartile<mean_high_quartile + std_cutoff*std_high_quartile or first_low_quartile<mean_high_quartile - std_cutoff*std_high_quartile
+            cutoff = 0 if within_2stds else 1
+        else:
+            cutoff=1
         return cutoff 
     
     
--- brainbox/examples/metrics_new.py
+++ brainbox/examples/metrics_new.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
+r"""
 Created on Wed Apr  1 21:46:59 2020
 
 @author: Steinmetz Lab User
+Example:
+  # define alf and ks directories:
+>>>alf_dir = r'C:\Users\Steinmetz Lab User\Documents\Lab\SpikeSortingOutput\Hopkins_CortexLab\test_path_alf'
+>>>ks_dir = r'C:\Users\Steinmetz Lab User\Documents\Lab\SpikeSortingOutput\Hopkins_CortexLab'
+
+  # run gen_metrics_labels
+>>>from metrics_new import gen_metrics_labels
+>>>gen_metrics_labels(alf_dir, ks_dir)
 """
 
 
@@ -17,39 +25,56 @@ from phylib.stats import correlograms
 import pandas as pd
 from defined_metrics import FP_RP, noise_cutoff, peak_to_peak_amp
 
-alf_dir = r'C:\Users\Steinmetz Lab User\Documents\Lab\SpikeSortingOutput\Hopkins_CortexLab\test_path_alf'
-ks_dir = r'C:\Users\Steinmetz Lab User\Documents\Lab\SpikeSortingOutput\Hopkins_CortexLab'
-ephys_file_path = r'C:\Users\Steinmetz Lab User\Documents\Lab\SpikeSortingOutput\Hopkins_CortexLab\Hopkins_20160722_g0_t0.imec.ap_CAR'
-
-spks_b = aio.load_object(alf_dir, 'spikes')
-clstrs_b = aio.load_object(alf_dir, 'clusters')
-units_b = bb.processing.get_units_bunch(spks_b)
-units = list(units_b.amps.keys())
-n_units = np.max(spks_b.clusters) + 1
 
 
-uidx=0
-label=np.empty([len(units)])
-for unit in units:
-    #unit = units[685] #635 max spike rate #681 1.43
-    ts = units_b['times'][unit]
-    amps = units_b['amps'][unit]
-    ch = clstrs_b['channels'][int(unit)]  # channel of max amplitude
 
-    RefPViol[int(unit)] = FP_RP(ts)
-    NoiseCutoff[int(unit)] = 
-    if (FP_RP(ts) and not noise_cutoff(amps,quartile_length=.25,std_cutoff = 2)) : #to do: add and mean(amps)>50microvolts?
-        label[int(unit)] = 1
-    else:
-        label[int(unit)] = 0
+def gen_metrics_labels(alf_dir, ks_dir):
+    spks_b = aio.load_object(alf_dir, 'spikes')
+    clstrs_b = aio.load_object(alf_dir, 'clusters')
+    units_b = bb.processing.get_units_bunch(spks_b)
+    units = list(units_b.amps.keys())
+    n_units = np.max(spks_b.clusters) + 1
     
     
-try:
-    labeldf = pd.DataFrame(label)
-    labeldf.to_csv(Path(ks_dir, 'label.tsv'),
-                            sep='\t', header=['label'])
-except Exception as err:
-    print("Could not save 'label' to .tsv. Details: \n {}".format(err))
+    uidx=0
+    label=np.empty([len(units)])
+    RefPViol = np.empty([len(units)])
+    NoiseCutoff = np.empty([len(units)])
+    for unit in units:
+        #unit = units[685] #635 max spike rate #681 1.43
+        ts = units_b['times'][unit]
+        amps = units_b['amps'][unit]
+        ch = clstrs_b['channels'][int(unit)]  # channel of max amplitude
     
-numpass=int(sum(label))
-print("Number of units that pass: ", numpass)
+        RefPViol[int(unit)] = FP_RP(ts)
+        NoiseCutoff[int(unit)] = noise_cutoff(amps,quartile_length=.25,std_cutoff = 2)
+        
+        if (FP_RP(ts) and not noise_cutoff(amps,quartile_length=.25,std_cutoff = 2)) : #to do: add and mean(amps)>50microvolts?
+            label[int(unit)] = 1
+        else:
+            label[int(unit)] = 0
+        
+    try:
+        refpvioldf = pd.DataFrame(RefPViol)
+        refpvioldf.to_csv(Path(ks_dir, 'RefPViol.tsv'),
+                                sep='\t', header=['RefPViol'])
+    except Exception as err:
+        print("Could not save 'RefPViol' to .tsv. Details: \n {}".format(err))
+        
+    try:
+        noisecutoffdf = pd.DataFrame(NoiseCutoff)
+        noisecutoffdf.to_csv(Path(ks_dir, 'NoiseCutoff.tsv'),
+                                sep='\t', header=['NoiseCutoff'])
+    except Exception as err:
+        print("Could not save 'NoiseCutoff' to .tsv. Details: \n {}".format(err))
+        
+   
+    try:
+        labeldf = pd.DataFrame(label)
+        labeldf.to_csv(Path(ks_dir, 'label.tsv'),
+                                sep='\t', header=['label'])
+    except Exception as err:
+        print("Could not save 'label' to .tsv. Details: \n {}".format(err))
+        
+    numpass=int(sum(label))
+    print("Number of units that pass: ", numpass)
