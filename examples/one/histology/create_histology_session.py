e8fe532030c66a262f1511d4d714a4cbcf1f1a6a
olivier <olivier.winter@hotmail.fr>
2020-04-06 13:49:38 +0100

    overwrites sessin if it already exists

--- examples/one/histology/create_histology_session.py
+++ examples/one/histology/create_histology_session.py
@@ -18,8 +18,7 @@ TASK_PROTOCOL = 'SWC_Histology_Serial2P_v0.0.1'
 sample_imaging_date = datetime.date(2020, 2, 1)  # Format: y - m - d
 sample_reception_date = datetime.date(2020, 4, 1)
 
-json_note = {
-        'sample_reception_date': ibllib.time.date2isostr(sample_reception_date)
+json_note = {'sample_reception_date': ibllib.time.date2isostr(sample_reception_date)[:10]}
 #     'elastix_affine_transform': np.zeros((4, 4)),
 #     'tilt': 0,
 #     'yaw': 0,
@@ -27,10 +26,9 @@ json_note = {
 #     'dv_scale': 1,
 #     'ap_scale': 1,
 #     'ml_scale': 1
-}
 
-
-ses_ = {'subject': subject,
+ses_ = {
+        'subject': subject,
         'users': ['steven.west'],
         'location': 'serial2P_01',
         'procedures': ['Histology'],
@@ -44,5 +42,13 @@ ses_ = {'subject': subject,
         # 'n_correct_trials': n_correct_trials,
         # 'n_trials': n_trials,
         'json': json_note
-        }
+}
+
+# overwrites the session if it already exists
+ses_date = ibllib.time.date2isostr(sample_imaging_date)[:10]
+ses = one.alyx.rest('sessions', 'list', subject=subject, number=1,
+                    date_range=[ses_date, ses_date])
+if len(ses) > 0:
+        one.alyx.rest('sessions', 'delete', ses[0]['url'])
 session = one.alyx.rest('sessions', 'create', data=ses_)
+
