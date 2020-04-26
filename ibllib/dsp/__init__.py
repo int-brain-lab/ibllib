851be0cfb328307010a6cf29ff4565a81dd6979d
olivier <olivier.winter@hotmail.fr>
2020-04-26 19:41:07 +0100

    fit phase and dephas functions

--- ibllib/dsp/__init__.py
+++ ibllib/dsp/__init__.py
@@ -1,2 +1,2 @@
-from .fourier import fscale, freduce, fexpand, lp, hp, bp, shift
+from .fourier import fscale, freduce, fexpand, lp, hp, bp, shift, dephas, fit_phase
 from .utils import rms, WindowGenerator, rises, falls, fronts
