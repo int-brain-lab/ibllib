4e86cb0ae5339bc015b7937cd4bb0a77db310222
noamroth <39137729+noamroth@users.noreply.github.com>
2020-04-29 17:47:05 -0700

    new code to plot wfs and compute amp, snr, etc. for metrics

---
+++ brainbox/examples/get_amplitudes_metrics.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 17:38:20 2020
+
+@author: Noam Roth
+
+Code to load an eid, get the true amplitudes in order to  (for now:)
+1) test two ways of getting waveforms
+2) plot waveforms
+3) compute mean amplitude and SNR for a neuron
+
+"""
+
