a551da09ff385de914b6be029bc62263c0113510
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-04-30 11:11:46 +0200

    Count 58s performance time for behavior examples

--- tests/oneibl/test_one_tutorial.py
+++ tests/oneibl/test_one_tutorial.py
@@ -10,30 +10,19 @@ from time import perf_counter
 start = perf_counter()
 
 # ----- IMPORT EXAMPLES -----
-# Ibllib tutorial example page
+# -- Ibllib tutorial example page
 import examples.one.tutorial_script
 
-# Ibllib ONE examples
+# -- Ibllib ONE examples
 import examples.one.behavior.number_mice_inproject
-import examples.one.behavior.plot_weight_curve
 import examples.one.behavior.plot_microphone_spectrogram
+import examples.one.behavior.plot_weight_curve
 import examples.one.behavior.print_water_administrations
+import examples.one.behavior.water_administrations_add_new
+import examples.one.behavior.water_administrations_weekend
 
-# Brainbox examples
+# -- Brainbox examples
 # from brainbox.examples.titi import toto
-
-# # ----- PRINT EXAMPLES -----
-# # One examples
-# print(examples.one.behavior.number_mice_inproject)
-# print(examples.one.behavior.plot_weight_curve)
-# print(examples.one.behavior.plot_microphone_spectrogram)
-# print(examples.one.behavior.print_water_administrations)
-#
-# # Tutorial example page
-# print(examples.one.tutorial_script)
-#
-# # Brainbox examples
-# # print()
 
 # ------ COMPUTE RUN TIME ----
 end = perf_counter()
