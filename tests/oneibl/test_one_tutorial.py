3af9851fab5b7304a6ba1a20eb978b751141e79f
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-04-30 11:07:22 +0200

    Remove printing, water admin from dev instead of test

--- tests/oneibl/test_one_tutorial.py
+++ tests/oneibl/test_one_tutorial.py
@@ -10,27 +10,32 @@ from time import perf_counter
 start = perf_counter()
 
 # ----- IMPORT EXAMPLES -----
-# Tutorial example page
+# Ibllib tutorial example page
 import examples.one.tutorial_script
 
-# One examples
-from examples.one.behavior import plot_weight_curve
+# Ibllib ONE examples
+import examples.one.behavior.number_mice_inproject
+import examples.one.behavior.plot_weight_curve
+import examples.one.behavior.plot_microphone_spectrogram
+import examples.one.behavior.print_water_administrations
 
 # Brainbox examples
 # from brainbox.examples.titi import toto
 
-# ----- PRINT EXAMPLES -----
-# One examples
-print(plot_weight_curve)
-
-# Tutorial example page
-print(examples.one.tutorial_script)
-
-# Brainbox examples
-# print()
+# # ----- PRINT EXAMPLES -----
+# # One examples
+# print(examples.one.behavior.number_mice_inproject)
+# print(examples.one.behavior.plot_weight_curve)
+# print(examples.one.behavior.plot_microphone_spectrogram)
+# print(examples.one.behavior.print_water_administrations)
+#
+# # Tutorial example page
+# print(examples.one.tutorial_script)
+#
+# # Brainbox examples
+# # print()
 
 # ------ COMPUTE RUN TIME ----
-
 end = perf_counter()
 execution_time = (end - start)
 print(execution_time)
