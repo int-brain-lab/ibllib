a9f76f8919258b1780dac22fdda705595d05719a
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2020-05-05 13:39:54 +0200

    added overwrite out option in example

--- examples/one/ephys/get_LFP_decompress.py
+++ examples/one/ephys/get_LFP_decompress.py
@@ -40,5 +40,6 @@ dat_volt = sr_op1[:10000, :]
 # === Decompress the data ===
 # Used by client code, e.g. Matlab for spike sorting
 # Give new path output name
-sr_op1.decompress_file(keep_original=True,
+sr_op1.decompress_file(keep_original=True,  # Keeps the original file
+                       overwrite=True,  # Overwrite the out file in case it already exists
                        out=session_path.joinpath('efile_lfp_decompressed'))
