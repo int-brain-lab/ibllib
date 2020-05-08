85210a12d7841bb2a456d0c920e56960a7cac7d5
Niccolo Bonacchi <nbonacchi@gmail.com>
2020-05-08 19:27:37 +0100

    extended_qc methods write, read, delete etc... TOD: Write Tests!

--- ibllib/qc/extended_qc.py
+++ ibllib/qc/extended_qc.py
@@ -1,14 +1,21 @@
+import json
+
 import numpy as np
 
 import ibllib.qc.bpodqc_metrics as bpodqc
 import ibllib.qc.oneqc_metrics as oneqc
 from ibllib.qc.bpodqc_extractors import load_bpod_data
-from ibllib.qc.oneutils import random_ephys_session
+from ibllib.qc.oneutils import get_details, random_ephys_session
 from oneibl.one import ONE
 
+# one = ONE(base_url="https://dev.alyx.internationalbrainlab.org")
 one = ONE()
 
-eid, det = random_ephys_session()
+# eid, det = random_ephys_session()
+
+eid = '4153bd83-2168-4bd4-a15c-f7e82f3f73fb'
+det = get_details(eid)
+details = get_details(eid, full=True)
 
 
 def build_extended_qc_frame(eid, data=None):
@@ -17,9 +24,9 @@ def build_extended_qc_frame(eid, data=None):
 
     # Get bpod and one qc frames
     extended_qc = {}
-    print("Running QC on ONE DatasetTypes...")
+    print(f"Session {eid}: Running QC on ONE DatasetTypes...")
     one_frame = oneqc.get_oneqc_metrics_frame(eid, data=data, apply_criteria=True)
-    print("Running QC on Bpod data...")
+    print(f"Session {eid}: Running QC on Bpod data...")
     bpod_frame = bpodqc.get_bpodqc_metrics_frame(eid, data=data, apply_criteria=True)
     # Make average bool pass
     # def average_frame(frame):
@@ -31,18 +38,80 @@ def build_extended_qc_frame(eid, data=None):
     return extended_qc
 
 
-# ad-hoc aggregation for some variables
-extended_qc = {
-    "feedback": np.sum(qc_frame.n_feedback != 0) / ntrials,
-}
-# here we average all bools
-for k in qc_frame.keys():
-    if qc_frame[k].dtype == np.dtype("bool"):
-        extended_qc[k] = np.mean(qc_frame[k])
-    elif k.endswith("_times"):
-        # Todo remove intervals_start from there if it's an absolute time
-        # std + mean ?
-        pass
+def write_extended_qc(eid: str, eqc_data: dict) -> dict:
+    """write_extended_qc
+    Write data to extended_qc WILL NOT CHECK IF DATA EXISTS
+    NOTE: Destructive write!
+
+    :param eid: Valid uuid sting for a session
+    :type eid: str
+    :param eqc_data: dict to upload
+    :type eqc_data: dict
+    :return: uploaded dict
+    :rtype: dict
+    """
+    # Prepare data to patch
+    patch_dict = {"extended_qc": json.dumps(eqc_data, indent=1)}
+    # Upload new extended_qc to session
+    session_details = one.alyx.rest("sessions", "partial_update", id=eid, data=patch_dict)
+    return json.loads(session_details['extended_qc'])
+
+
+def update_extended_qc(eid: str, eqc_data: dict) -> dict:
+    """update_extended_qc_on_session
+    Non destructive update of extended_qc field for session "eid"
+    Will update the extended_qc field of the session with the eqc_data dict inputted
+    If eqc_data has fields with the same name of existing fields it will squash the old
+    values (uses the dict.update() method)
+
+    :param eid: Valid uuid sting for a session
+    :type eid: str
+    :param data: dict with etended_qc frame to be uploaded
+    :type data: dict
+    :return: new patched extended_qc dict
+    :rtype: dict
+    """
+    # Load current extended_qc field
+    current = read_extended_qc(eid)
+    # Patch current dict of extended_qc with new data
+    if current is not None:
+        # NOTE: Check here if we want to enforce a specific structure
+        current.update(eqc_data)
+    extended_qc = write_extended_qc(eid, current)
+    return extended_qc
+
+
+def read_extended_qc(eid: str) -> dict:
+    """ Query the extended_qc field of session eid"""
+    eqc = one.alyx.rest("sessions", "read", eid)['extended_qc']
+    if eqc is None:
+        return
+    return json.loads(eqc)
+
+
+def delete_extended_qc(eid: str) -> None:
+    _ = one.alyx.rest("sessions", "partial_update", id=eid, data={'extended_qc': None})
+
+
+def remove_extended_qc_key(eid: str, key: str) -> dict:
+    current = read_extended_qc(eid)
+    if current is None:
+        return
+    if current.get(key, None) is None:
+        print(f"{key}: Key not found in extended_qc session field")
+        return current
+    print(f"Removing {key}")
+    current.pop(key)
+    extended_qc = write_extended_qc(eid, current)
+    return extended_qc
+
+
+def build_and_upload_extended_qc(eid, data=None):
+    if data is None:
+        data = load_bpod_data(eid)
+
+    eqc_data = build_extended_qc_frame(eid, data=data)
+    new_eqc_data = update_extended_qc(eid, eqc_data)
 
 
 if __name__ == "__main__":
@@ -85,4 +154,7 @@ if __name__ == "__main__":
         "_bpod_correct_trial_event_sequence": None,
         "_bpod_trial_length": None,
     }
-
+    data = {
+        "test_qc_field": 0.999827,
+        "_some_other_qc_field": 3.0
+    }
