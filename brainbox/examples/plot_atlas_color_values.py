9f3b2baedecdadfd33cc4b7fb0ddf7c1fda1a293
Guido <guido.meijer@research.fchampalimaud.org>
2020-09-22 11:05:06 +0100

    Finished example script

--- brainbox/examples/plot_atlas_color_values.py
+++ brainbox/examples/plot_atlas_color_values.py
@@ -6,6 +6,22 @@ Created on Mon Sep 21 16:31:58 2020
 @author: guido
 """
 
-from brainbox.atlas.atlas import plot_atlas
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ibllib import atlas
+from brainbox.atlas import plot_atlas
 
-plot_atlas()
+# Coordinates of slices in mm
+ML = -0.5
+AP = 1
+DV = -2
+
+# Generate some mock data
+ba = atlas.AllenAtlas(25)
+regions = np.random.choice(ba.regions.acronym, size=500, replace=False)  # pick 500 random regions
+values = np.random.uniform(-1, 1, 500)  # generate 500 random values
+
+# Plot atlas 
+f, axs = plt.subplots(1, 3, figsize=(40, 10))
+plot_atlas(regions, values, ML, AP, DV, color_palette='RdBu_r', minmax=[-1, 1], axs=axs)
