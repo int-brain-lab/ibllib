ebf51f0af1035ef3a265974d9b094dcc464b936b
Niccolo Bonacchi <nbonacchi@gmail.com>
2020-10-29 16:13:15 +0000

    Linted w/ black for retrigger of build on new Travis

--- brainbox/examples/plot_atlas_color_values.py
+++ brainbox/examples/plot_atlas_color_values.py
@@ -5,10 +5,10 @@ from brainbox.atlas import plot_atlas
 
 
 def combine_layers_cortex(regions, delete_duplicates=False):
-    remove = ['1', '2', '3', '4', '5', '6a', '6b', '/']
+    remove = ["1", "2", "3", "4", "5", "6a", "6b", "/"]
     for i, region in enumerate(regions):
         for j, char in enumerate(remove):
-            regions[i] = regions[i].replace(char, '')
+            regions[i] = regions[i].replace(char, "")
     if delete_duplicates:
         regions = list(set(regions))
     return regions
@@ -27,12 +27,21 @@ values = np.random.uniform(-1, 1, 500)  # generate 500 random values
 
 # Plot atlas
 f, axs = plt.subplots(2, 3, figsize=(20, 10))
-plot_atlas(regions, values, ML, AP, DV, color_palette='RdBu_r', minmax=[-1, 1], axs=axs[0])
+plot_atlas(regions, values, ML, AP, DV, color_palette="RdBu_r", minmax=[-1, 1], axs=axs[0])
 
 # Now combine all layers of cortex
 plot_regions = combine_layers_cortex(regions)
 combined_cortex = combine_layers_cortex(all_regions)
 
 # Plot atlas
-plot_atlas(plot_regions, values, ML, AP, DV, color_palette='RdBu_r', minmax=[-1, 1], axs=axs[1],
-           custom_region_list=combined_cortex)
+plot_atlas(
+    plot_regions,
+    values,
+    ML,
+    AP,
+    DV,
+    color_palette="RdBu_r",
+    minmax=[-1, 1],
+    axs=axs[1],
+    custom_region_list=combined_cortex,
+)
