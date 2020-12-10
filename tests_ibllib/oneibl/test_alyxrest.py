a3c3f6bbe7536099721b1d00c6cb303e3665bc50
olivier <olivier.winter@hotmail.fr>
2020-12-10 14:52:42 +0000

    add histology rest endpoints test for channel serialization in webclient

--- tests_ibllib/oneibl/test_alyxrest.py
+++ tests_ibllib/oneibl/test_alyxrest.py
@@ -11,6 +11,9 @@ from oneibl.one import ONE
 one = ONE(username='test_user', password='TapetesBloc18',
           base_url='https://test.alyx.internationalbrainlab.org')
 
+EID = 'cf264653-2deb-44cb-aa84-89b82507028a'
+EID_EPHYS = 'b1c968ad-4874-468d-b2e4-5ffa9b9964e9'
+
 
 class Tests_REST(unittest.TestCase):
 
@@ -40,10 +43,9 @@ class Tests_REST(unittest.TestCase):
         self.assertEqual(ses, ses_)
 
     def test_note_with_picture_upload(self):
-        eid = 'cf264653-2deb-44cb-aa84-89b82507028a'
         my_note = {'user': 'olivier',
                    'content_type': 'session',
-                   'object_id': eid,
+                   'object_id': EID,
                    'text': "gnagnagna"}
 
         with tempfile.NamedTemporaryFile(mode="wb", suffix='.png') as png:
@@ -54,3 +56,36 @@ class Tests_REST(unittest.TestCase):
         self.assertTrue(len(ar_note['image']))
         self.assertTrue(ar_note['content_type'] == 'actions.session')
         one.alyx.rest('notes', 'delete', id=ar_note['id'])
+
+    def test_channels(self):
+        # need to build insertion + trajectory + channels to test the serialization of a
+        # record array in the channel endpoint
+        probe_insertions = one.alyx.rest('insertions', 'list', session=EID_EPHYS, name='tutu')
+        for pi in probe_insertions:
+            one.alyx.rest('insertions', 'delete', pi['id'])
+        probe_insertion = one.alyx.rest(
+            'insertions', 'create', data={'session': EID_EPHYS, 'name': 'tutu'})
+        trajectory = one.alyx.rest('trajectories', 'create', data={
+            'probe_insertion': probe_insertion['id'],
+            'x': 1500,
+            'y': -2000,
+            'z': 0,
+            'depth': 4500,
+            'phi': 0,
+            'theta': 0,
+            'provenance': 'Histology track',
+        })
+        channel_records = []
+        for _ in np.arange(3):
+            channel_records.append({
+                'x': np.random.randint(-2000, 2000),
+                'y': np.random.randint(-2000, 2000),
+                'z': np.random.randint(-2000, 2000),
+                'axial': np.random.randint(1, 40) * 20,
+                'lateral': np.random.randint(1, 4) * 4,
+                'brain_region': 889,
+                'trajectory_estimate': trajectory['id']
+            })
+        channels = one.alyx.rest('channels', 'create', data=channel_records)
+        self.assertTrue(len(channels) == 3)
+        one.alyx.rest('insertions', 'delete', id=probe_insertion['id'])
