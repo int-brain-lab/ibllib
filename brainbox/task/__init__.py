7eb04aa5128ffd40d52ff8efacca6227dd95546d
Guido <guido.meijer@research.fchampalimaud.org>
2021-01-05 15:58:41 +0000

    Function to generate pseudo sessions

--- brainbox/task/__init__.py
+++ brainbox/task/__init__.py
@@ -1,3 +1,3 @@
 from brainbox.task.task import (responsive_units, differentiate_units, roc_single_event,
                                 roc_between_two_events, generate_pseudo_blocks,
-                                generate_pseudo_session)
+                                generate_pseudo_session, generate_pseudo_stimuli)
