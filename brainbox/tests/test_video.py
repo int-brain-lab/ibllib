4000af52af5c2050b2705e6d0c5db206f4698c56
Miles Wells <k1o0@3tk.co>
2021-01-19 12:05:40 +0200

    Video brainbox module

---
+++ brainbox/tests/test_video.py
@@ -0,0 +1,61 @@
+import unittest
+
+import numpy as np
+
+from brainbox.video import video
+
+
+class TestVideo(unittest.TestCase):
+    def setUp(self) -> None:
+        """Test frames are 12 2x2x3 arrays, where
+            frames[0][...,0] =
+                [[1, 1],
+                 [1, 1]]
+
+            frames[1][...,0] =
+                [[2, 2],
+                 [2, 2]]
+
+            [...]
+
+            frames[-1][...,0] =
+                [[12, 12],
+                 [12, 12]]
+            """
+        self.frames = np.cumsum(np.ones((12, 2, 2, 3)), axis=0).astype(np.uint8)
+
+    def test_frame_diff(self):
+        # Test with three colour channels (2x2x3)
+        frame1, frame2 = [self.frames[i] for i in range(2)]
+        df = video.frame_diff(frame1, frame2)
+        expected = np.ones(frame1.shape[:2], dtype=np.uint8)
+        np.testing.assert_equal(df, expected)
+
+        # Test with single channel (2x2)
+        df = video.frame_diff(frame1[..., 0], frame2[..., 0])
+        np.testing.assert_equal(df, expected)
+
+        # Test shape validation
+        with self.assertRaises(ValueError):
+            video.frame_diff(frame1[..., 0], frame2)
+
+    def test_frame_diffs(self):
+        # Test frame diffs defaults
+        df = video.frame_diffs(self.frames)
+        expected = np.ones((self.frames.shape[0] - 1, *self.frames.shape[1:-1]), dtype=np.uint8)
+        np.testing.assert_equal(df, expected)
+
+        # Test shape validation
+        with self.assertRaises(ValueError):
+            video.frame_diffs(self.frames, diff=20)
+
+        # Test frames diff every 2nd frame with intensity frames
+        d = 2  # Take difference every two frames
+        df = video.frame_diffs(self.frames[..., 0], d)
+        expected_shape = (self.frames.shape[0] - d, *self.frames.shape[1:-1])
+        expected = np.ones(expected_shape, dtype=np.uint8) * 2
+        np.testing.assert_equal(df, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- brainbox/video/video.py
+++ brainbox/video/video.py
@@ -14,21 +14,34 @@ def frame_diff(frame1, frame2):
     return np.uint8(norm32 * 255)
 
 
-def frame_diffs(frames):
-    # Ensure array
-    # frames = np.array(frames)
-    # frames = np.array(
-    #     [np.ones((5, 9, 3)), np.zeros((5, 9, 3)), np.ones((5, 9, 3)), np.zeros((5, 9, 3))],
-    #     dtype=np.int32)
-    df = np.empty((len(frames) - 2, *frames[0].shape[:2]), dtype=np.uint8)
-    for i in range(len(frames) - 2):
-        df[i] = frame_diff(frames[i], frames[i + 2])
-        # dist = cv2.GaussianBlur(dist, (9, 9), 0)
-        # calculate st dev test
-        # _, stDev = cv2.meanStdDev(dist)
-        # thresh.append(bool(stDev > sd_thresh))
+def frame_diffs(frames, diff=1):
+    frames = np.array(frames, dtype=np.float32)
+    if frames.shape[0] < diff:
+        raise ValueError('Difference must be less than number of frames')
+    diff32 = frames[diff:] - frames[:-diff]
+    # Normalize
+    if frames.ndim == 4:
+        norm32 = np.sqrt((diff32 ** 2).sum(axis=3)) / np.sqrt(255 ** 2 * 3)
+    else:
+        norm32 = np.sqrt(diff32 ** 2 * 3) / np.sqrt(255 ** 2 * 3)
+    return np.uint8(norm32 * 255)
 
-    # Feature scaling
-    df_ = df.sum(axis=(1, 2))
-    df_ = (df_ - np.min(df_)) / (np.max(df_) - np.min(df_))
-    return df_
+
+# def frame_diffs(frames):
+#     # Ensure array
+#     # frames = np.array(frames)
+#     # frames = np.array(
+#     #     [np.ones((5, 9, 3)), np.zeros((5, 9, 3)), np.ones((5, 9, 3)), np.zeros((5, 9, 3))],
+#     #     dtype=np.int32)
+#     df = np.empty((len(frames) - 2, *frames[0].shape[:2]), dtype=np.uint8)
+#     for i in range(len(frames) - 2):
+#         df[i] = frame_diff(frames[i], frames[i + 2])
+#         # dist = cv2.GaussianBlur(dist, (9, 9), 0)
+#         # calculate st dev test
+#         # _, stDev = cv2.meanStdDev(dist)
+#         # thresh.append(bool(stDev > sd_thresh))
+#
+#     # Feature scaling
+#     df_ = df.sum(axis=(1, 2))
+#     df_ = (df_ - np.min(df_)) / (np.max(df_) - np.min(df_))
+#     return df_
