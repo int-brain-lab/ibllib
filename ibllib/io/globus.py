a233b83dd337d0d81f3e84f3e5a0fc63442d8bfb
Miles Wells <k1o0@3tk.co>
2021-01-26 19:04:58 +0200

    Added test fixture

--- ibllib/io/globus.py
+++ ibllib/io/globus.py
@@ -1,5 +1,6 @@
 import re
-from sys import platform
+import sys
+import os
 from pathlib import Path
 
 import globus_sdk as globus
@@ -30,7 +31,7 @@ def as_globus_path(path):
     path = str(path)
     if (
         re.match(r'/[A-Z]($|/)', path)
-        if platform in ('win32', 'cygwin')
+        if sys.platform in ('win32', 'cygwin')
         else Path(path).is_absolute()
     ):
         return path
@@ -54,8 +55,8 @@ def _login(globus_client_id, refresh_tokens=False):
     globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
 
     token = dict(refresh_token=globus_transfer_data['refresh_token'],
-                 transfer_token=globus_transfer_data['access_token'],
-                 expires_at_s=globus_transfer_data['expires_at_seconds'],
+                 access_token=globus_transfer_data['access_token'],
+                 expires_at_seconds=globus_transfer_data['expires_at_seconds'],
                  )
     return token
 
@@ -67,19 +68,29 @@ def login(globus_client_id):
     return tc
 
 
-def setup(globus_client_id, str_app='globus'):
+def setup(globus_client_id, str_app='globus/default'):
     # Lookup and manage consents there
     # https://auth.globus.org/v2/web/consents
     gtok = _login(globus_client_id, refresh_tokens=True)
     params.write(str_app, gtok)
 
 
-def login_auto(globus_client_id, str_app='globus'):
+def login_auto(globus_client_id, str_app='globus/default'):
     token = params.read(str_app)
-    required_fields = {'refresh_token', 'transfer_token', 'expires_at_s'}
+    required_fields = {'refresh_token', 'access_token', 'expires_at_seconds'}
     if not (token and required_fields.issubset(token.as_dict())):
         raise ValueError("Token file doesn't exist, run ibllib.io.globus.setup first")
     client = globus.NativeAppAuthClient(globus_client_id)
     client.oauth2_start_flow(refresh_tokens=True)
     authorizer = globus.RefreshTokenAuthorizer(token.refresh_token, client)
     return globus.TransferClient(authorizer=authorizer)
+
+
+def get_local_endpoint():
+    if sys.platform == 'win32' or sys.platform == 'cygwin':
+        id_path = Path(os.environ['LOCALAPPDATA']).joinpath("Globus Connect")
+    else:
+        id_path = Path.home().joinpath(".globusonline", "lta")
+    with open(id_path / "client-id.txt", 'r') as fid:
+        globus_id = fid.read()
+    return globus_id.strip()
