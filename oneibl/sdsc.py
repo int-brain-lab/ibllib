20b1230d589ac66d02ccb7de0e60931c717085b2
olivier <olivier.winter@hotmail.fr>
2021-02-25 12:11:47 +0000

    some sdsc utils

--- oneibl/sdsc.py
+++ oneibl/sdsc.py
@@ -8,9 +8,13 @@ SERVER_URL = "https://ibl.flatironinstitute.org/"
 
 
 def path_from_dataset(dset):
+    if isinstance(dset, list):
+        return [path_from_dataset(d) for d in dset]
     fr = next((fr for fr in dset['file_records'] if fr['data_url']))
     return path_from_filerecord(fr)
 
 
 def path_from_filerecord(fr):
+    if isinstance(fr, list):
+        return [path_from_filerecord(f) for f in fr]
     return Path(fr['data_url'].replace(SERVER_URL, str(ROOT_PATH) + '/'))
