03ceb0c060ba42dbc030541319d5ec7f33fa6329
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2021-02-26 11:06:54 +0000

    correct indexing in example

--- brainbox/examples/docs_scatter_raster_plot.py
+++ brainbox/examples/docs_scatter_raster_plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from brainbox.plot.ephys_plots import scatter_raster_plot
+from brainbox.plot.plot_base import plot_scatter
 from oneibl.one import ONE
 import matplotlib.pyplot as plt
 import matplotlib
@@ -17,7 +18,8 @@ good_clusters = np.where(metrics.label == 1)
 spike_idx = np.where(np.isin(spikes['clusters'], good_clusters))[0]
 
 # Also filter for nans in amplitude and depth
-kp_idx = np.where(~np.isnan(spikes['depths'][spike_idx]) & ~np.isnan(spikes['amps'][spike_idx]))[0]
+kp_idx = spike_idx[np.where(~np.isnan(spikes['depths'][spike_idx])
+                            & ~np.isnan(spikes['amps'][spike_idx]))[0]]
 
 # Get ScatterPlot object
 data = scatter_raster_plot(spikes['amps'][kp_idx], spikes['depths'][kp_idx],
@@ -26,19 +28,11 @@ data = scatter_raster_plot(spikes['amps'][kp_idx], spikes['depths'][kp_idx],
 # Add v lines 10s after start and 10s before end or recording
 x1 = np.min(spikes['times'][kp_idx] + 100)
 x2 = np.max(spikes['times'][kp_idx] - 100)
-data.add_lines(pos=x1, orientation='v', style='solid', width=10, color='r')
-data.add_lines(pos=x2, orientation='v', style='dashed', width=8, color='k')
+data.add_lines(pos=x1, orientation='v', style='dashed', width=3, color='k')
+data.add_lines(pos=x2, orientation='v', style='dashed', width=3, color='k')
+
 
 plot_dict = data.convert2dict()
 
-fig, ax = plt.subplots()
-scat = ax.scatter(x=plot_dict['data']['x'], y=plot_dict['data']['y'], c=plot_dict['color']/255,
-                  s=plot_dict['marker_size'], cmap=plot_dict['cmap'],
-                  vmin=plot_dict['clim'][0], vmax=plot_dict['clim'][1])
-ax.hlines([20, 3840], *ax.get_xlim(), linestyles='dashed', linewidth=3, colors='k')
-
-norm = matplotlib.colors.Normalize(vmin=plot_dict['clim'][0], vmax=plot_dict['clim'][1], clip=True)
-cbar = fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=plot_dict['cmap']), ax=ax)
-cbar.set_label(plot_dict['labels']['clabel'])
-plt.show()
+fig, ax = plot_scatter(plot_dict)
 
