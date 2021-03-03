2bee289dd4b30bc2349879ce7a304fd9bc828847
Niccolo Bonacchi <nbonacchi@gmail.com>
2021-03-03 18:45:51 +0000

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if CURRENT_PYTHON < REQUIRED_PYTHON:
 ==========================
 Unsupported Python version
 ==========================
-This version of idtrackerai requires Python {}.{}, but you're trying to
+This version of ibllib requires Python {}.{}, but you're trying to
 install it on Python {}.{}.
 """.format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
     sys.exit(1)
