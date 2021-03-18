4264f49948e32646a30cd36a4d0e85fd963e7db5
juhuntenburg <ju.huntenburg@gmail.com>
2021-03-18 15:28:47 +0000

    Update and cleanup DLC QC

--- ibllib/qc/dlc.py
+++ ibllib/qc/dlc.py
@@ -2,257 +2,119 @@
 This module runs a list of quality control metrics on the dlc traces.
 
 Example - Run DLC QC
-    qc = DlcQC(eid, download_data=True)
+    qc = DlcQC(eid, 'left', download_data=True)
     qc.run()
 Question:
     We're not extracting the audio based on TTL length.  Is this a problem?
 """
-from ibllib.qc import base
-from brainbox.core import Bunch
-import alf.io
-import numpy as np
 import logging
 from inspect import getmembers, isfunction
+
+import numpy as np
+
+from ibllib.qc import base
+import alf.io as alfio
+from brainbox.core import Bunch
+from oneibl.one import OneOffline
 
 _log = logging.getLogger('ibllib')
 
 
 class DlcQC(base.QC):
     """A class for computing camera QC metrics"""
-    # TODO You can add some constants here, which can be accessed without
-    # instantiating the class
 
-    # Datasets to check for (or download if download_data == True)
     dstypes = ['camera.dlc', 'camera.times']
+    bbox = {
+        'body': {
+            'xrange': range(201, 500),
+            'yrange': range(81, 330)
+        },
+        'left': {
+            'xrange': range(301, 700),
+            'yrange': range(181, 470)
+        },
+        'right': {
+            'xrange': range(301, 600),
+            'yrange': range(110, 275)
+        },
+    }
 
-    def __init__(self, session_path_or_eid, video_type, **kwargs):
+    def __init__(self, session_path_or_eid, side, **kwargs):
         """
         :param session_path_or_eid: A session eid or path
-        :param log: A logging.Logger instance,
-                    if None the 'ibllib' logger is used
+        :param log: A logging.Logger instance, if None the 'ibllib' logger is used
         :param one: An ONE instance for fetching and setting the QC on Alyx
-        TODO Add optional kwargs specific to DLC
+        :param camera: The camera to run QC on, if None QC is run for all three cameras.
         """
-        # When an eid is provided, we will download the required data by
-        # default (if necessary)
-        download_data = not alf.io.is_session_path(session_path_or_eid)
+        # When an eid is provided, we will download the required data by default (if necessary)
+        download_data = not alfio.is_session_path(session_path_or_eid)
         self.download_data = kwargs.pop('download_data', download_data)
         super().__init__(session_path_or_eid, **kwargs)
         self.data = Bunch()
+        self.side = side
+
         # QC outcomes map
         self.metrics = None
         self.outcome = 'NOT_SET'
-        self.video_type = video_type
 
     def load_data(self, download_data: bool = None) -> None:
-        """Extract the data from raw data files
-        Extracts all the required task data from the raw data files.
+        """Extract the data from data files
+        Extracts all the required task data from the data files.
+
+        Data keys:
+            - camera_times (float array): camera frame timestamps extracted from frame headers
+            - dlc_coords (dict): keys are the points traced by dlc, items are x-y coordinates of
+                                 these points over time, those with likelihood <0.9 set to NaN
 ​
-        :param download_data: if True, any missing raw data
-                              is downloaded via ONE.
+        :param download_data: if True, any missing raw data is downloaded via ONE.
         """
         if download_data is not None:
             self.download_data = download_data
-        if self.one:
+        if self.one and not isinstance(self.one, OneOffline):
             self._ensure_required_data()
         _log.info('Gathering data for QC')
 
         alf_path = self.session_path / 'alf'
 
-        cam0 = alf.io.load_object(
-            alf_path,
-            '%sCamera' %
-            self.video_type,
-            namespace='ibl')
-
-        cam = cam0['dlc']
-        points = np.unique(['_'.join(x.split('_')[:-1]) for x in cam.keys()])
-
-        # Set values to nan if likelyhood is too low # for pqt: .to_numpy()
-        XYs = {}
-        for point in points:
-            x = np.ma.masked_where(
-                cam[point + '_likelihood'] < 0.9, cam[point + '_x'])
-            x = x.filled(np.nan)
-            y = np.ma.masked_where(
-                cam[point + '_likelihood'] < 0.9, cam[point + '_y'])
-            y = y.filled(np.nan)
-            XYs[point] = np.array([x, y])
-
-        self.data['camera_times'], self.data['XYs'] = cam0['times'], XYs
-
-    def check_time_trace_length_match(self):
-        XYs = self.data['XYs']
-        Times = self.data['camera_times']
-        t = 0
-        for point in XYs:
-            for coordinate in [0, 1]:
-                if len(Times) < len(XYs[point][coordinate]):
-                    _log.error(f'{self.video_type} dlc time<{point} \
-                                  lengths mismatch')
-                    t += 1
-
-        if t == 0:
-            return 'PASS'
-        else:
-            return 'FAIL'
-
-    def check_if_points_all_nan(self):
-        XYs = self.data['XYs']
-        t = 0
-        for point in XYs:
-            if 'tube' in point:
-                continue
-            for coordinate in [0, 1]:
-                if all(np.isnan(XYs[point][coordinate])):
-                    t += 1
-                    _log.error(f'{self.video_type} {point} \
-                                 all coord {coordinate} nan')
-
-        if t == 0:
-            return 'PASS'
-        else:
-            return 'FAIL'
-
-    def get_mean_positions(self):
-        XYs = self.data['XYs']
-        mloc = {}  # mean locations
-        for point in XYs:
-            mloc[point] = [np.nanmean(XYs[point][0]),
-                           np.nanmean(XYs[point][1])]
-        return mloc
-
-    def check_if_mean_in_box(self):
-        '''
-        Empirical bounding boxes around average
-        dlc points, averaged across time and points;
-        sessions with points out of this box were
-        often faulty in terms of raw videos
-        '''
-
-        mloc = self.get_mean_positions()
-        x = np.nanmean([i[0] for i in mloc.values()])
-        y = np.nanmean([i[1] for i in mloc.values()])
-
-        if self.video_type == 'left':
-            a = any(~np.array([300 < x < 700, 180 < y < 470]))
-        if self.video_type == 'right':
-            a = any(~np.array([300 < x < 600, 110 < y < 275]))
-        if self.video_type == 'body':
-            a = any(~np.array([200 < x < 500, 80 < y < 330]))
-
-        if a:
-            _log.error(f'{self.video_type} average position is out of box')
-            return 'FAIL'
-        else:
-            return 'PASS'
-
-    def get_diameter(self):
-        '''get mean of both pupil diameters
-        d1 = top - bottom, d2 = left - right
-        '''
-
-        XYs = self.data['XYs']
-        pt = XYs['pupil_top_r']
-        pb = XYs['pupil_bottom_r']
-        pl = XYs['pupil_left_r']
-        pr = XYs['pupil_right_r']
-
-        d1 = ((pt[0] - pb[0])**2 +
-              (pt[1] - pb[1])**2)**0.5
-        d2 = ((pl[0] - pr[0])**2 +
-              (pl[1] - pr[1])**2)**0.5
-
-        return np.nanmean([d1, d2], axis=0)
-
-    def check_whisker_pupil_block(self):
-        '''
-        Check if pupil diameter is nan for
-        more than 60 % of the frames;
-        (might be blocked by a whisker)
-        Check if standard deviation is above
-        a threshold, found for bad sessions
-        '''
-
-        if self.video_type == 'body':
-            return 'PASS'
-
-        d = self.get_diameter()
-
-        if np.mean(np.isnan(d)) > 0.9:
-            _log.error(f'{self.eid}, {self.video_type}, \
-                          diameter too often nan, \
-                          {np.mean(np.isnan(d))}')
-            return 'FAIL'
-
-        if self.video_type == 'left':
-            THR = 10  # threshold for std of diameter
-
-        elif self.video_type == 'right':
-            THR = 5
-
-        if np.nanstd(d) > THR:
-            _log.error(f'{self.eid}, {self.video_type}, \
-                          diameter too jumpy')
-            return 'FAIL'
-
-        return 'PASS'
-
-    def check_lick_detection(self):
-        '''
-        check if both of the two tongue edge points are
-        less than 10 % nan, indicating that
-        wrong points are detected (spout edge, mouth edge)
-        '''
-
-        if self.video_type == 'body':
-            return 'PASS'
-
-        XYs = self.data['XYs']
-        points = ['tongue_end_r', 'tongue_end_l']
-        ms = []
-        for point in points:
-            ms.append(np.mean(np.isnan(XYs[point][0])))
-        if (ms[0] < 0.1) and (ms[1] < 0.1):
-            return 'FAIL'
-        return 'PASS'
+        # Load times
+        self.data['camera_times'] = alfio.load_object(alf_path, f'{self.side}Camera')['times']
+        # Load dlc traces
+        dlc_df = alfio.load_object(alf_path, f'{self.side}Camera', namespace='ibl')['dlc']
+        targets = np.unique(['_'.join(col.split('_')[:-1]) for col in dlc_df.columns])
+        # Set values to nan if likelyhood is too low
+        dlc_coords = {}
+        for t in targets:
+            idx = dlc_df.loc[dlc_df[f'{t}_likelihood'] < 0.9].index
+            dlc_df.loc[idx, [f'{t}_x', f'{t}_y']] = np.nan
+            dlc_coords[t] = np.array((dlc_df[f'{t}_x'], dlc_df[f'{t}_y']))
+        self.data['dlc_coords'] = dlc_coords
 
     def _ensure_required_data(self):
         """
-        Ensures the datasets required for QC are local.
-        If the download_data attribute is True,
-        any missing data are downloaded.  If all the data are
-        not present locally at the end of it an exception is raised.
-        If the stream attribute is True, the video file is not
-        required to be local, however it must be remotely accessible.
-        TODO Simply check that the required data is present.
-        If download_data is True, it should download the data via ONE.
-        Should raise an error if there isn't the required
-        data for the session
+        Ensures the datasets required for QC are local.  If the download_data attribute is True,
+        any missing data are downloaded.  If all the data are not present locally at the end of
+        it an exception is raised.
         :return:
         """
         assert self.one is not None, 'ONE required to download data'
-        # Get extractor type
         for dstype in self.dstypes:
-            dataset = self.one.datasets_from_type(self.eid, dstype)
-            kwargs = {'download_only': True}
+            dataset = self.one.datasets_from_type(self.eid, dstype, full=True)
             present = (
-                (self.one.load_dataset(self.eid, d, **kwargs) for d in dataset)
+                self.one.download_datasets(dataset)
                 if self.download_data
-                else (next(self.session_path.rglob(d), None) for d in dataset)
+                else (next(self.session_path.rglob(d['name']), None) for d in dataset)
             )
             assert (dataset and all(present)), f'Dataset {dstype} not found'
 
     def run(self, update: bool = False, **kwargs) -> (str, dict):
         """
-        Run video QC checks and return outcome
+        Run DLC QC checks and return outcome
         :param update: if True, updates the session QC fields on Alyx
         :param download_data: if True, downloads any missing data if required
         :returns: overall outcome as a str, a dict of checks and their outcomes
-        TODO Shouldn't need to change this method
         """
-        _log.info('Computing QC outcome')
-        namespace = f'dlc{self.video_type.capitalize()}'
+        _log.info(f'Computing DLC QC outcome for {self.side} camera, session {self.eid}')
+        namespace = f'dlc{self.side.capitalize()}'
         if all(x is None for x in self.data.values()):
             self.load_data(**kwargs)
 
@@ -261,27 +123,114 @@ class DlcQC(base.QC):
 
         checks = getmembers(DlcQC, is_metric)
         self.metrics = {f'_{namespace}_' + k[6:]: fn(self) for k, fn in checks}
-        outcome = self.overall_outcome(self.metrics.values())
+
+        values = [x if isinstance(x, str) else x[0] for x in self.metrics.values()]
+        code = max(base.CRITERIA[x] for x in values)
+        outcome = next(k for k, v in base.CRITERIA.items() if v == code)
 
         if update:
-            bool_map = {k: None if v is None else v == 'PASS' for k, v in self.metrics.items()}
-            self.update_extended_qc(bool_map)
+            extended = {
+                k: None if v is None or v == 'NOT_SET'
+                else base.CRITERIA[v] < 3 if isinstance(v, str)
+                else v[1:] if len(v) > 2 else v[-1]  # Otherwise store custom value(s)
+                for k, v in self.metrics.items()
+            }
+            self.update_extended_qc(extended)
             self.update(outcome, namespace)
         return outcome, self.metrics
 
+    def check_time_trace_length_match(self):
+        dlc_coords = self.data['dlc_coords']
+        times = self.data['camera_times']
+        for target in dlc_coords.keys():
+            if times.shape[0] != dlc_coords[target].shape[1]:
+                _log.error(f'{self.side}Camera length of camera.times does not match '
+                           'length of camera.dlc {target}')
+                return 'FAIL'
+        return 'PASS'
 
-def run_all_qc(session, update=False,
-               cameras=('left', 'right', 'body'), **kwargs):
-    """Run QC for all cameras
-    Run the camera QC for left, right and body cameras.
+    def check_trace_all_nan(self):
+        dlc_coords = self.data['dlc_coords']
+        for target in dlc_coords.keys():
+            if 'tube' not in target:
+                if all(np.isnan(dlc_coords[target][0])) or all(np.isnan(dlc_coords[target][1])):
+                    _log.error(f'{self.side}Camera dlc trace {target} all NaN')
+                    return 'FAIL'
+        return 'PASS'
+
+    def check_mean_in_bbox(self):
+        '''
+        Empirical bounding boxes around average dlc points, averaged across time and points;
+        sessions with points out of this box were often faulty in terms of raw videos
+        '''
+
+        dlc_coords = self.data['dlc_coords']
+        x_mean = np.nanmean([np.nanmean(dlc_coords[k][0]) for k in dlc_coords.keys()])
+        y_mean = np.nanmean([np.nanmean(dlc_coords[k][1]) for k in dlc_coords.keys()])
+
+        xrange = self.bbox[self.side]['xrange']
+        yrange = self.bbox[self.side]['yrange']
+        if int(x_mean) not in xrange or int(y_mean) not in yrange:
+            return 'FAIL'
+        else:
+            return 'PASS'
+
+    def get_diameter(self):
+        '''get mean of both pupil diameters d1 = top - bottom, d2 = left - right
+        '''
+        dlc_coords = self.data['dlc_coords']
+        d1 = ((dlc_coords['pupil_top_r'][0] - dlc_coords['pupil_bottom_r'][0])**2 +
+              (dlc_coords['pupil_top_r'][1] - dlc_coords['pupil_bottom_r'][1])**2)**0.5
+        d2 = ((dlc_coords['pupil_left_r'][0] - dlc_coords['pupil_right_r'][0])**2 +
+              (dlc_coords['pupil_left_r'][1] - dlc_coords['pupil_right_r'][1])**2)**0.5
+
+        return np.nanmean([d1, d2], axis=0)
+
+    def check_pupil_blocked(self):
+        '''
+        Check if pupil diameter is nan for more than 60 % of the frames
+        (might be blocked by a whisker)
+        Check if standard deviation is above a threshold, found for bad sessions
+        '''
+
+        if self.side != 'body':
+            d = self.get_diameter()
+            if np.mean(np.isnan(d)) > 0.9:
+                _log.error(f'{self.eid}, {self.side}Camera, pupil diameter too often NaN')
+                return 'FAIL'
+
+            thr = 5 if self.side == 'right' else 10
+            if np.nanstd(d) > thr:
+                _log.error(f'{self.eid}, {self.side}Camera, pupil diameter too unstable')
+                return 'FAIL'
+        return 'PASS'
+
+    def check_lick_detection(self):
+        '''
+        Check if both of the two tongue edge points are less than 10 % NaN, indicating that
+        wrong points are detected (spout edge, mouth edge)
+        '''
+
+        if self.side != 'body':
+            dlc_coords = self.data['dlc_coords']
+            nan_l = np.mean(np.isnan(dlc_coords['tongue_end_l'][0]))
+            nan_r = np.mean(np.isnan(dlc_coords['tongue_end_r'][0]))
+            if (nan_l < 0.1) and (nan_r < 0.1):
+                return 'FAIL'
+        return 'PASS'
+
+
+def run_all_qc(session, cameras=('left', 'right', 'body'), one=None, **kwargs):
+    """Run DLC QC for all cameras
+    Run the DLC QC for left, right and body cameras.
     :param session: A session path or eid.
     :param update: If True, QC fields are updated on Alyx.
     :param cameras: A list of camera names to perform QC on.
-    :return: dict of CameraCQ objects
+    :return: dict of DlcQC objects
     """
     qc = {}
-    one = kwargs.pop('one', None)
+    run_args = {k: kwargs.pop(k) for k in ('download_data', 'update') if k in kwargs.keys()}
     for camera in cameras:
-        qc[camera] = DlcQC(session, video_type=camera, one=one)
-        qc[camera].run(update=update, **kwargs)
+        qc[camera] = DlcQC(session, side=camera, one=one, **kwargs)
+        qc[camera].run(**run_args)
     return qc
