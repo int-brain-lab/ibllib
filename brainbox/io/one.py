9e049f3a4f4b51d3001a990f0d7864e32a7c95ae
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2021-03-22 21:22:40 +0000

    fix trajectory bug

--- brainbox/io/one.py
+++ brainbox/io/one.py
@@ -112,7 +112,7 @@ def load_channel_locations(eid, one=None, probe=None, aligned=False):
                 # Otherwise we just get the channels from alyx. Shouldn't happen often, only if
                 # data is still inbetween ftp and flatiron after being resolved
                 else:
-                    traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe_name=label,
+                    traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe=label,
                                             provenance='Ephys aligned histology track')[0]['id']
                     chans = one.alyx.rest('channels', 'list', trajectory_estimate=traj_id)
 
@@ -132,7 +132,7 @@ def load_channel_locations(eid, one=None, probe=None, aligned=False):
                             f' locations will be obtained from latest available ephys aligned '
                             f'histology track.')
                 # get the latest user aligned channels
-                traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe_name=label,
+                traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe=label,
                                         provenance='Ephys aligned histology track')[0]['id']
                 chans = one.alyx.rest('channels', 'list', trajectory_estimate=traj_id)
 
@@ -149,7 +149,7 @@ def load_channel_locations(eid, one=None, probe=None, aligned=False):
                 logger.info(f'Channel locations for {label} have not been resolved. '
                             f'Channel and cluster locations obtained from histology track.')
                 # get the channels from histology tracing
-                traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe_name=label,
+                traj_id = one.alyx.rest('trajectories', 'list', session=eid, probe=label,
                                         provenance='Histology track')[0]['id']
                 chans = one.alyx.rest('channels', 'list', trajectory_estimate=traj_id)
 
