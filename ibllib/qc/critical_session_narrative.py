a01d4167f601561ec193d1adb56195ba94886301
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2021-04-28 15:52:41 +0200

    WIP insertion critical

--- ibllib/qc/critical_session_narrative.py
+++ ibllib/qc/critical_session_narrative.py
@@ -1,22 +1,15 @@
 '''
-Prompt experimenter for reason for marking session as CRITICAL
-Choices are:
-- within experiment system crash
-- synching impossible
-- dud or mock session
-- essential dataset missing
-- other
-
-Multiple reasons can be selected
-Place info in Alyx session note in a format that is machine retrievable (text->json)
+Prompt experimenter for reason for marking session/insertion as CRITICAL
+Choices are listed in the global variables. Multiple reasons can be selected.
+Places info in Alyx session note in a format that is machine retrievable (text->json)
 '''
 # Author: Gaelle
 import json
 from oneibl.one import ONE
 
 # Global var
-STR_NOTES_STATIC = '=== EXPERIMENTER REASON(S) FOR MARKING THE SESSION AS CRITICAL ==='
 
+# Reasons for marking a session as critical
 REASONS_SESS_CRIT = (
     'within experiment system crash',
     'synching impossible',
@@ -25,26 +18,54 @@ REASONS_SESS_CRIT = (
     'other'
 )
 
+# Reasons for marking an insertion as critical
+# Note: Split the reasons labelled in the GUI versus those to be seen
+# when marking the insertion programmatically
+REASONS_INS_CRIT_GUI = (
+    'Noise and artifact',
+    'Drift',
+    'Poor neural yield',
+    'Brain Damage',
+    'Other'
+)
+
+REASONS_INS_CRIT = tuple(['Histological images missing',
+                          'Track not visible on imaging data']
+                         + list(REASONS_INS_CRIT_GUI))
+
 
 # Util functions
-def _reason_addnumberstr(reason_list=REASONS_SESS_CRIT):
+def _create_note_str(ins_or_sess):
+    '''
+    :param ins_or_sess: str containing either 'insertion' or 'session'
+    :return:
+    '''
+    str_static = f'=== EXPERIMENTER REASON(S) FOR MARKING THE {ins_or_sess.upper()} AS CRITICAL ==='
+    return str_static
+
+
+def _reason_addnumberstr(reason_list):
     """
     Adding number at the beginning of the str for ease of selection by user
-    :param reason_list : list of str ; default: REASONS_SESS_CRIT
+    :param reason_list : list of str ; default: None
     """
     return [f'{i}) {r}' for i, r in enumerate(reason_list)]
 
 
-REASONS_WITH_NUMBERS = _reason_addnumberstr()
+REASONS_SESS_WITH_NUMBERS = _reason_addnumberstr(reason_list=REASONS_SESS_CRIT)
+REASONS_INS_WITH_NUMBERS = _reason_addnumberstr(reason_list=REASONS_INS_CRIT)
 
 
-def _reason_question_prompt():
+def _reason_question_prompt(reason_list, reasons_with_numbers, ins_or_sess):
     """
-    Function asking the user to enter the criteria for marking a sess as CRITICAL.
+    Function asking the user to enter the criteria for marking a session/insertion as CRITICAL.
+    :param reason_list: list of reasons (str)
+    :param reasons_with_numbers: list of reasons (str), with added number in front
+    :param ins_or_sess: str containing either 'insertion' or 'session'
     """
 
-    prompt = f'Select from this list the reason(s) why you are marking the session as CRITICAL:' \
-             f' \n {REASONS_WITH_NUMBERS} \n' \
+    prompt = f'Select from this list the reason(s) why you are marking the {ins_or_sess} as CRITICAL:' \
+             f' \n {reasons_with_numbers} \n' \
              f'and enter the corresponding numbers separated by commas, e.g. 1,3 -> enter: '
     ans = input(prompt).strip().lower()
     # turn str into numbers
@@ -56,8 +77,8 @@ def _reason_question_prompt():
         print(f'{ans} is invalid, please try again...')
         return _reason_question_prompt()
 
-    if all(elem in range(0, len(REASONS_WITH_NUMBERS)) for elem in integer_list):
-        reasons_out = [REASONS_SESS_CRIT[integer_n] for integer_n in integer_list]
+    if all(elem in range(0, len(reasons_with_numbers)) for elem in integer_list):
+        reasons_out = [reason_list[integer_n] for integer_n in integer_list]
         print(f'You selected reason(s): {reasons_out}')
         return reasons_out
     else:
@@ -71,7 +92,7 @@ def _enquire_why_other():
     return ans
 
 
-def _create_note_json(reasons_selected, reason_for_other, note_title=STR_NOTES_STATIC):
+def _create_note_json(reasons_selected, reason_for_other, note_title):
     note_session = {
         "title": note_title,
         "reasons_selected": reasons_selected,
@@ -96,21 +117,22 @@ def _delete_note_yesno(notes):
         return ans
 
 
-def _upload_note_alyx(eid, note_text, one=None, str_notes_static=STR_NOTES_STATIC):
+def _upload_note_alyx(eid, note_text, content_type, str_notes_static, one=None):
     """
     Function to upload a note to Alyx.
     It will check if notes with STR_NOTES_STATIC already exists for this session,
     and ask if OK to overwrite.
-    :param eid: session eid
+    :param eid: session or isnertion eid
     :param note_text: text to enter within the note object
     :param one: default: None -> ONE()
     :param str_notes_static: string within the notes that will be searched for
+    :param content_type: 'session' or 'insertion'
     :return:
     """
     if one is None:
         one = ONE()
     my_note = {'user': one._par.ALYX_LOGIN,
-               'content_type': 'session',
+               'content_type': content_type,
                'object_id': eid,
                'text': f'{note_text}'}
     # check if such a note already exists, ask if OK to overwrite
@@ -133,7 +155,7 @@ def _upload_note_alyx(eid, note_text, one=None, str_notes_static=STR_NOTES_STATI
 
 def main(eid, one=None):
     """
-    Main function to call to input a reason for marking a session as CRITICAL. It will:
+    Main function to call to input a reason for marking a session/insertion as CRITICAL. It will:
     - ask reasons for selection of critical status
     - check if 'other' reason has been selected, inquire why (free text)
     - create note text, checking whether similar notes exist already
@@ -152,14 +174,35 @@ def main(eid, one=None):
                                  f'object_id,{eid}')
     test_json_read = json.loads(notes[0]['text'])
 
-    :param eid: session eid
+    :param eid: session/insertion eid
     :param one: default: None -> ONE()
     :return:
     """
     if one is None:
         one = ONE()
     # ask reasons for selection of critical status
-    reasons_selected = _reason_question_prompt()
+
+    # hit the database to know if eid is insertion or session eid
+
+    sess_list = one.alyx.rest('sessions', 'list', id=eid)
+    ins_list = one.alyx.rest('insertions', 'list', id=eid)
+
+    if len(sess_list) > 0 and len(ins_list) == 0:  # session
+        reason_list = REASONS_SESS_CRIT
+        reasons_with_numbers = REASONS_SESS_WITH_NUMBERS
+        ins_or_sess = 'session'
+    elif len(ins_list) > 0 and len(sess_list) == 0:  # insertion
+        reason_list = REASONS_INS_CRIT
+        reasons_with_numbers = REASONS_INS_WITH_NUMBERS
+        ins_or_sess = 'insertion'
+    else:
+        raise ValueError(f'Inadequate number of session (n={len(sess_list)}) '
+                         f'or insertion (n={len(ins_list)}) found for eid {eid}.'
+                         f'The query output should be of length 1.')
+
+    reasons_selected = _reason_question_prompt(reason_list=reason_list,
+                                               reasons_with_numbers=reasons_with_numbers,
+                                               ins_or_sess=ins_or_sess)
 
     # check if 'other' reason has been selected, inquire why
     if 'other' in reasons_selected:
@@ -167,9 +210,12 @@ def main(eid, one=None):
     else:
         reason_for_other = []
 
-    # create note text
+    # create note title and text
+    note_title = _create_note_str(ins_or_sess)
+
     note_text = _create_note_json(reasons_selected=reasons_selected,
-                                  reason_for_other=reason_for_other)
+                                  reason_for_other=reason_for_other,
+                                  note_title=note_title)
 
     # upload note to Alyx
-    _upload_note_alyx(eid, note_text, one=one)
+    _upload_note_alyx(eid, note_text, content_type=ins_or_sess, str_notes_static=note_title, one=one)
