4e664878fec7929ef8e3b05f99d975f9548f0c14
Miles Wells <k1o0@3tk.co>
2021-06-09 17:51:31 +0300

    one silent flag in test_io

--- brainbox/tests/test_io.py
+++ brainbox/tests/test_io.py
@@ -1,13 +1,11 @@
 import json
 from pathlib import Path
 import unittest
-import uuid
 import tempfile
 import shutil
 
 import numpy as np
 
-from iblutil.io.parquet import uuid2np
 from brainbox.io import one as bbone
 from one.api import ONE
 from one.alf.onepqt import make_parquet_db
@@ -23,7 +21,8 @@ class TestIO_ALF(unittest.TestCase):
         self.tmpdir = Path(tempfile.gettempdir()) / 'test_bbio'
         self.tmpdir.mkdir(exist_ok=True)
         self.one = ONE(base_url='https://test.alyx.internationalbrainlab.org', mode='local',
-                       username='test_user', password='TapetesBloc18', cache_dir=self.tmpdir)
+                       username='test_user', password='TapetesBloc18', cache_dir=self.tmpdir,
+                       silent=True)
         self.alf_path = self.tmpdir.joinpath(
             'lab', 'Subjects', 'subject', '2019-08-12', '001', 'alf'
         )
@@ -56,21 +55,6 @@ class TestIO_ALF(unittest.TestCase):
         np.save(self.alf_path.joinpath('trials.gnagnag.npy'), np.random.rand(50))
         # Add some fake records to the cache
         if not self.one.search(subject='subject', date='2019-08-12', number='001'):
-            eid = uuid2np([uuid.uuid4()])
-            session_rec = {
-                'lab': 'lab',
-                'subject': 'subject',
-                'date': '2019-08-12',
-                'number': 1,
-                'id_0': eid[0, 0],
-                'id_1': eid[0, 1]
-            }
-            self.one._cache['sessions'] = (
-                self.one._cache['sessions']
-                    .reset_index()
-                    .append(session_rec, ignore_index=True)
-                    .set_index(['id_0', 'id_1'])
-            )
             make_parquet_db(self.tmpdir)
             self.one._load_cache(cache_dir=self.tmpdir)
 
