d3604e8bd02be1f6241542f1056954b2d6fce9f4
Miles Wells <k1o0@3tk.co>
2021-06-28 15:27:29 +0300

    correct params module in globus

--- ibllib/io/globus.py
+++ ibllib/io/globus.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 import globus_sdk as globus
-from ibllib.io import params
+from iblutil.io import params
 
 
 def as_globus_path(path):
@@ -76,7 +76,7 @@ def setup(globus_client_id, str_app='globus/default'):
 
 
 def login_auto(globus_client_id, str_app='globus/default'):
-    token = params.read(str_app)
+    token = params.read(str_app, {})
     required_fields = {'refresh_token', 'access_token', 'expires_at_seconds'}
     if not (token and required_fields.issubset(token.as_dict())):
         raise ValueError("Token file doesn't exist, run ibllib.io.globus.setup first")
