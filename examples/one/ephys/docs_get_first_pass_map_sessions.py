fd09885bea7170e25b72fb79f97c3505bdba0b1b
Miles Wells <k1o0@3tk.co>
2021-09-30 15:27:08 +0300

    Move basic ONE examples to ONE

--- examples/one/ephys/docs_get_first_pass_map_sessions.py
+++ examples/one/ephys/docs_get_first_pass_map_sessions.py
@@ -5,6 +5,6 @@ Use ONE to get information about sessions included in first pass map
 
 """
 from one.api import ONE
-one = ONE(base_url='https://openalyx.internationalbrainlab.org', silent=True)
+one = ONE(base_url='https://openalyx.internationalbrainlab.org')
 
-first_pass_map_sessions = one.search(project='ibl_neuropixel_brainwide_01')
+first_pass_map_sessions = one.search(project='ibl_neuropixel_brainwide_01', query_type='remote')
