27f7aca8e5e6f7d010d21e11d5caf78165381b6c
Miles Wells <k1o0@3tk.co>
2022-05-27 15:36:29 +0300

    Clean up fibrephotometry module

--- ibllib/io/extractors/fibrephotometry.py
+++ ibllib/io/extractors/fibrephotometry.py
@@ -1,158 +1,118 @@
-"""Data extraction from fibrephotometry DAQ files"""
-from collections import OrderedDict
-import logging
-from pathlib import Path, PureWindowsPath
-import uuid
+"""Data extraction from fibrephotometry DAQ files.
 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+Below is the expected folder structure for a fibrephotometry session:
+
+    subject/
+    ├─ 2021-06-30/
+    │  ├─ 001/
+    │  │  ├─ alf/
+    │  │  │  ├─ raw_photometry_data/
+    │  │  │  │  ├─ fpData.raw.csv
+    │  │  │  │  ├─ fpData.channels.csv
+
+fpData.raw.csv is a copy of the 'FPdata' file, the output of the Neuophotometrics Bonsai workflow.
+fpData.channels.csv is table of frame flags for deciphering LED and GPIO states. The default table,
+copied from the Neurophotometrics manual can be found in iblscripts/deploy/fppc/
+"""
+import logging
+
+import pandas as pd
 import numpy as np
-from pkg_resources import parse_version
 
 import one.alf.io as alfio
-from iblutil.util import Bunch
-import ibllib.dsp as dsp
-import ibllib.exceptions as err
-from ibllib.io.extractors import biased_trials, training_trials
-from ibllib.io import raw_daq_loaders
 from ibllib.io.extractors.base import BaseExtractor
-from ibllib.dsp.utils import sync_timestamps
-from ibllib.io.extractors.ephys_fpga import get_main_probe_sync, get_sync_fronts
-from ibllib.io.video import get_video_meta
 
-_logger = logging.getLogger('ibllib')
-FILENAME_MAP = {
-    'motioncorrect_*.bin': 'widefield.raw_motionCorrected.bin',
-    'motion_correction_shifts.npy': 'widefield.motionCorrentionShifts.npy',
-    'motion_correction_rotation.npy': 'widefield.motionCorrentionRotation.npy',
-    # 'frames_average.npy': 'frameAverage.widefield.npy',
-    'U.npy': 'widefieldU.images.npy',
-    'SVT.npy': 'widefieldSVT.uncorrected.npy',
-    # 'rcoeffs.npy': '',
-    # 'T.npy': 'widefieldT.uncorrected.npy',
-    'SVTcorr.npy': 'widefieldSVT.haemoCorrected.npy'
-}
+_logger = logging.getLogger(__name__)
 
-# MC USB-201
-DEFAULT_CHMAP = {
-    'mcdaq': {
-        'bpod': 'ai1',
-        'fp': 'ai0'  # Fibrephotometry
-    },
+
+"""Available LEDs on the Neurophotometrics FP3002"""
+LIGHT_SOURCE_MAP = {
+    0: 'None',
+    415: 'Violet',
+    470: 'Blue',
+    560: 'Green'
 }
 
 
 class FibrePhotometry(BaseExtractor):
-    save_names = ('_ibl_photometry.green.npy', '_ibl_photometry.red.npy',
-                  'ibl_photometry.isosbestic.npy', 'ibl_photometry.timestamps.npy')
-    var_names = ('green', 'red', 'isobestic', 'timestamps')
+    save_names = ('photometry.signal.npy', 'photometry.photometryLightSource.npy',
+                  'photometryLightSource.properties.tsv', 'photometry.times.npy')
+    var_names = ('signal', 'lightSource', 'lightSource_properties', 'timestamps')
 
     def __init__(self, *args, **kwargs):
         """An extractor for all widefield data"""
         super().__init__(*args, **kwargs)
 
-    def _extract(self, **kwargs):
+    def _channel_meta(self, light_source_map=None):
+        """
+        Return table of light source wavelengths and corresponding colour labels.
+
+        Parameters
+        ----------
+        light_source_map : dict
+            An optional map of light source wavelengths (nm) used and their corresponding colour name.
+
+        Returns
+        -------
+        pandas.DataFrame
+            A sorted table of wavelength and colour name.
+        """
+        light_source_map = light_source_map or LIGHT_SOURCE_MAP
+        names = ('wavelength', 'color')
+        meta = pd.DataFrame(sorted(light_source_map.items()), columns=names)
+        meta.index.rename('channel_id', inplace=True)
+        return meta
+
+    def _extract(self, light_source_map=None, **kwargs):
         """
 
         Parameters
         ----------
+        light_source_map : dict
+            An optional map of light source wavelengths (nm) used and their corresponding colour name.
 
         Returns
         -------
+        numpy.ndarray
+            A 1D array of signal values.
+        numpy.ndarray
+            A 1D array of ints corresponding to the active light source during a given frame.
+        pandas.DataFrame
+            A table of light source IDs, their wavelength in nm and corresponding colour name.
         """
-        out = {}
+        fp_data = alfio.load_object(self.session_path / 'raw_photometry_data', 'fpData')
+        ts = self.extract_timestamps(fp_data, **kwargs)
 
-        # Extract signal per channel for each ROI
-        fp_raw_path = next(self.session_path.joinpath('raw_fp_data').glob('fpData.raw*'))
-        fp_data = alfio.load_file_content(fp_raw_path)
-        # Column name 'Flags' in older Bonsai workflow
-        state = fp_data.get('LedState', fp_data.get('Flags', None))
-        # Load channel map
-        fp_chmap_path = next(self.session_path.joinpath('raw_fp_data').glob('fpData.channels*'))
-        chmap = alfio.load_file_content(fp_chmap_path).set_index('Condition')
+        # Load channels and
+        channel_meta_map = self._channel_meta(kwargs.get('light_source_map'))
+        channel_map = fp_data['channels'].set_index('Condition')
+        # Extract signal columns into 2D array
+        signal = fp_data['raw'].filter(like='Region', axis=1).sort_index(axis=1).values
+        # Extract channel index
+        state = fp_data['raw'].get('LedState', fp_data['raw'].get('Flags', None))
+        channel_id = np.empty_like(state)
+        for (label, ch), (id, wavelength) in zip(channel_map.items(), channel_meta_map['wavelength'].items()):
+            mask = state.isin(ch)
+            channel_id[mask] = id
+            if wavelength != 0:
+                assert str(wavelength) in label
 
-        for wavelength, colour in (('L470', 'green'), ('L560', 'red'), ('L415', 'isosbestic')):
-            mask = state.isin(chmap[wavelength])
-            out[colour] = fp_data[mask].iloc[:, 3:].values
+        return signal, channel_id, channel_meta_map, ts
 
-        # Extract DAQ data
-        chmap = kwargs.get('chmap', DEFAULT_CHMAP['mcdaq'])
-        fp_path = self.session_path.joinpath('raw_fp_data')
-        daq_data = raw_daq_loaders.load_daq_tdms(fp_path, chmap)
+    def extract_timestamps(self, fp_data, **kwargs):
+        """Extract the photometry.timestamps array.
 
-        # return [out[k] for k in out] + [wheel['timestamps'], wheel['position'],
-        #                                 moves['intervals'], moves['peakAmplitude']]
+        This is dependant on the DAQ and task synchronization protocol.
 
-    def sync_timestamps(self):
+        Parameters
+        ----------
+        fp_data : dict
+            A Bunch of raw fibrephotometry data, with the keys ('raw', 'channels').
+
+        Returns
+        -------
+        numpy.ndarray
+            An array of timestamps, one per frame.
+        """
         raise NotImplementedError  # To subclass
 
-    def _symlink_files(self):
-        ...
-
-    def preprocess(self, fs=30, functional_channel=0, nbaseline_frames=30, k=200):
-        data_path = self.session_path.joinpath('raw_widefield_data')
-
-        # MOTION CORRECTION
-        wfield_cli._motion(data_path)
-        # COMPUTE AVERAGE FOR BASELINE
-        wfield_cli._baseline(data_path, nbaseline_frames)
-        # DATA REDUCTION
-        wfield_cli._decompose(data_path, k=k)
-        # HAEMODYNAMIC CORRECTION
-        # check if it is 2 channel
-        dat = wfield_cli.load_stack(data_path)
-        if dat.shape[1] == 2:
-            del dat
-            wfield_cli._hemocorrect(data_path, fs=fs, functional_channel=functional_channel)
-
-    def rename_files(self, session_folder) -> bool:
-        """
-        Rename the raw widefield data for a given session.
-        Parameters
-        ----------
-        session_folder : str, pathlib.Path
-            A session path containing widefield data.
-        Returns
-        -------
-        success : bool
-            True if all files were successfully renamed.
-        TODO Double-check filenames and call this function
-        """
-        session_path = Path(session_folder).joinpath('raw_widefield_data')
-        if not session_path.exists():
-            _logger.warning(f'Path does not exist: {session_path}')
-            return False
-        renames = (
-            ('dorsal_cortex_landmarks.json', 'widefieldLandmarks.dorsalCortex.json'),
-            ('*.dat', 'widefield.raw.dat'),
-            ('*.camlog', 'widefieldEvents.raw.camlog')
-        )
-        success = True
-        for before, after in renames:
-            try:
-                filename = next(session_path.glob(before))
-                filename.rename(after)
-                # TODO Save nchannels and frame size from filename?
-            except StopIteration:
-                _logger.warning(f'File not found: {before}')
-                success = False
-        return success
-
-    def sync_timestamps(self, bin_exists=False):
-        filepath = next(self.session_path.joinpath('raw_widefield_data').glob('*.camlog'))
-        fpga_sync, chmap = get_main_probe_sync(self.session_path, bin_exists=bin_exists)
-        bpod = get_sync_fronts(fpga_sync, chmap['bpod'])
-        logdata, led, sync, ncomm = parse_cam_log(filepath, readTeensy=True)
-        if bpod.times.size == 0:
-            raise err.SyncBpodFpgaException('No Bpod event found in FPGA. No behaviour extraction. '
-                                            'Check channel maps.')
-
-        fcn, drift, iteensy, ifpga = dsp.utils.sync_timestamps(sync.timestamp.values, bpod['times'], return_indices=True)
-        _logger.debug(f'Widefield-FPGA clock drift: {drift} ppm')
-        assert led.frame.is_monotonic_increasing  # FIXME Need to check whether to use logdata instead
-        video_path = next(self.session_path.joinpath('raw_widefield_data').glob('widefield.raw*.mov'))
-        video_meta = get_video_meta(video_path)
-        assert video_meta.length == len(led)  # FIXME This fails
-        widefield_times = fcn(led.timestamp.values)
-        return widefield_times  # FIXME Need to sort frame mismatch
-    # TODO Add QC check for
