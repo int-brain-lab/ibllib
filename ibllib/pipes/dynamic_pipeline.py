2bddd05fc88e40ec535b53ef5eb7dacf374c0a11
olivier <olivier.winter@hotmail.fr>
2022-06-14 19:10:11 +0100

    Create pipelines for set of test sessions

--- ibllib/pipes/dynamic_pipeline.py
+++ ibllib/pipes/dynamic_pipeline.py
@@ -1,14 +1,112 @@
+from collections import OrderedDict
+import ibllib.pipes.tasks as mtasks
+import ibllib.pipes.ephys_preprocessing as epp
+import ibllib.pipes.training_preprocessing as tpp
+
+
+def get_acquisition_description(experiment):
+    """"
+    This is a set of example acqusition descriptions for experiments:
+    -   choice_world_recording
+    -   choice_world_biased
+    -   choice_world_training
+    -   choice_world_habituation
+    That are part of the IBL pipeline
+    """
+    if experiment == 'choice_world_recording':   # canonical ephys
+        acquisition_description = {  # this is the current ephys pipeline description
+            'cameras': [
+                'raw_video_data/_iblrig_rightCamera.raw',
+                'raw_video_data/_iblrig_bodyCamera.raw.mp4',
+                'raw_video_data/_iblrig_leftCamera.raw.mp4'
+            ],
+            'neuropixel': {
+                'probe00': 'raw_ephys_data/probe00',
+                'probe01': 'raw_ephys_data/probe01',
+            },
+            'tasks': {
+                'choice_world_recording': 'raw_behavior_data',
+                'choice_world_passive': 'raw_passive_data'
+            },
+            'microphone': {},
+            'sync': 'nidq'
+        }
+    else:
+        acquisition_description = {  # this is the current ephys pipeline description
+            'cameras': [
+                'raw_video_data/_iblrig_leftCamera.raw.mp4'
+            ],
+            'tasks': {
+                experiment: 'raw_passive_data'
+            },
+            'microphone': {},
+            'sync': 'bpod'
+        }
+    return acquisition_description
+
+
+class DummyTask(mtasks.Task):
+    def _run(self):
+        pass
+
+
+def make_pipeline(pipeline_description, session_path=None):
+    # NB: this pattern is a pattern for dynamic class creation
+    # tasks['SyncPulses'] = type('SyncPulses', (epp.EphysPulses,), {})(session_path=session_path)
+    assert session_path
+    tasks = OrderedDict()
+    sync = pipeline_description['sync']
+    if sync == 'nidq':
+        tasks[f'SyncPulses{sync}'] = type(f'SyncPulses{sync}', (epp.EphysPulses,), {})(session_path=session_path)
+    elif sync == 'bpod':
+        tasks[f'SyncPulses{sync}'] = type(f'SyncPulses{sync}', (epp.EphysPulses,), {})(session_path=session_path)
+
+
+
+    if 'neuropixel' in pipeline_description:
+        for pname, rpath in pipeline_description['neuropixel'].items():
+            kwargs = {'session_path': session_path, 'args': dict(pname=pname)}
+            tasks[f'Compression_{pname}'] = type(
+                f'Compression_{pname}', (epp.EphysMtscomp,), {})(**kwargs)
+
+            tasks[f'SpikeSorting_{pname}'] = type(
+                f'SpikeSorting_{pname}', (epp.SpikeSorting,), {})(
+                **kwargs, parents=[tasks[f'Compression_{pname}'], tasks[f'SyncPulses{sync}']])
+
+            tasks[f'CellsQC_{pname}'] = type(
+                f'CellsQC_{pname}', (epp.EphysCellsQc,), {})(
+                **kwargs, parents=[tasks[f'SpikeSorting_{pname}']])
+
+    for protocol in pipeline_description.get('tasks', []):
+        tasks[protocol] = type(protocol, (DummyTask,), {})(session_path=session_path, parents=[tasks[f'SyncPulses{sync}']])
+        if protocol in ['']:
+            tasks[protocol] = type(protocol, (DummyTask,), {})(session_path=session_path, parents=[tasks[f'SyncPulses{sync}']])
+        if protocol.startswith('choice_world') and protocol != 'choice_world_passive':
+            tasks["Training Status"] = type("Training Status", (DummyTask,), {})(session_path=session_path, parents=[tasks[protocol]])
+
+    if 'cameras' in pipeline_description:
+        tasks['VideoCompress'] = type('VideoCompress', (epp.EphysVideoCompress,), {})(session_path=session_path)
+        tasks['DLC'] = type('DLC', (epp.EphysDLC,), {})(session_path=session_path, parents=[tasks['VideoCompress']])
+        tasks['VideoSync'] = type('VideoSync', (epp.EphysVideoSyncQc,), {})(
+            session_path=session_path, parents=[tasks['VideoCompress'], tasks[f'SyncPulses{sync}']])
+        tasks['PostDLC'] = type('PostDLC', (epp.EphysPostDLC,), {})(
+            session_path=session_path, parents=[tasks['DLC'], tasks['VideoSync']])
+
+    if 'microphone' in pipeline_description:
+        tasks['Audio'] = type('Audio', (epp.EphysAudio,), {})(session_path=session_path)
+
+    p = mtasks.Pipeline(session_path=session_path)
+    p.tasks = tasks
+
+    return p
 
 
 session_meta = {
     'protocols': {'biasedChoiceWorld': 'raw_behaviour_data',
-                  'passiveChoiceWorld': 'raw_passive_data'} #TODO how do we get the collection from the rig brahhh
+                  'passiveChoiceWorld': 'raw_passive_data'},  #TODO how do we get the collection from the rig brahhh
     'devices': ['ephys', 'video', 'widefield'],
     'sync': 'fpga'
 }
-
-
-
 
 
 def read_session_meta(session_path):
@@ -44,7 +142,7 @@ def get_device_pipeline(device, session_path, sync):
     return tasks
 
 
-class DynamicPipeline(tasks.Pipeline):
+class DynamicPipeline(mtasks.Pipeline):
     """
     This is trying to replicate the current IBL
     """
@@ -82,4 +180,20 @@ class DynamicPipeline(tasks.Pipeline):
         # How do we do audio??
 
 
+if __name__ == '__main__':
+    from one.api import ONE
+    from ibllib.pipes.dynamic_pipeline import make_pipeline, get_acquisition_description
 
+    one = ONE(base_url="https://alyx.internationalbrainlab.org", cache_dir="/media/olivier/F1C9-7D73/one")
+
+    examples = {
+        "451bc9c0-113c-408e-a924-122ffe44306e": 'choice_world_habituation',
+        "cad382e5-7b1a-4e73-b892-318b5e9decc4": 'choice_world_training',
+        "47e53f41-a928-4b6d-b8cc-78fd10d03456": 'choice_world_biased',
+        "aed404ce-b3fb-454b-ac43-2f12198c9eaf": 'choice_world_recording',
+    }
+
+    for eid in examples:
+        ad = get_acquisition_description(examples[eid])
+        p = make_pipeline(ad, session_path='wpd')
+        p.make_graph()
