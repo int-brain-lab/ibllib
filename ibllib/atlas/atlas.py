904c0d992b5d402df9239d4507dc45764781d1c9
juhuntenburg <ju.huntenburg@gmail.com>
2022-06-24 17:25:27 +0200

    Dont replace z by surface if surface is nan

--- ibllib/atlas/atlas.py
+++ ibllib/atlas/atlas.py
@@ -796,7 +796,10 @@ class Insertion:
         if brain_atlas:
             iy = brain_atlas.bc.y2i(d['y'] / 1e6)
             ix = brain_atlas.bc.x2i(d['x'] / 1e6)
-            z = brain_atlas.top[iy, ix]
+            # Only use the brain surface value as z if it isn't NaN (this happens when the surface touches the edges
+            # of the atlas volume
+            if not np.isnan(brain_atlas.top[iy, ix]):
+                z = brain_atlas.top[iy, ix]
         return Insertion(x=d['x'] / 1e6, y=d['y'] / 1e6, z=z,
                          phi=d['phi'], theta=d['theta'], depth=d['depth'] / 1e6,
                          beta=d.get('beta', 0), label=d.get('label', ''))
