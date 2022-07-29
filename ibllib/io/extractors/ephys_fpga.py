c23a5abed365bcaa54ac156c7fd04294ece4ed13
Miles Wells <k1o0@3tk.co>
2022-07-29 16:59:51 +0300

    Typo

--- ibllib/io/extractors/ephys_fpga.py
+++ ibllib/io/extractors/ephys_fpga.py
@@ -738,7 +738,7 @@ def load_sync(session_path, sync_collection):
     """
     Load sync files from session path and collection
     :param session_path:
-    :param collection:
+    :param sync_collection:
     :return:
     """
     sync = alfio.load_object(session_path.joinpath(sync_collection), 'sync', namespace='spikeglx', short_keys=True)
