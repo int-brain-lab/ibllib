7d08d7e51ba682cddd5ae5efde2f04c09223617b
olivier <olivier.winter@hotmail.fr>
2022-08-10 17:01:22 +0200

    move logger config to iblutils - fix bug where multiples handlers were set

--- ibllib/misc/misc.py
+++ ibllib/misc/misc.py
@@ -22,33 +22,6 @@ def structarr(names, shape=None, formats=None):
     return np.zeros(shape, dtype=dtyp)
 
 
-def logger_config(name=None):
-    import logging
-    import colorlog
-    """
-        Setup the logging environment
-    """
-    if not name:
-        log = logging.getLogger()  # root logger
-    else:
-        log = logging.getLogger(name)
-    log.setLevel(logging.INFO)
-    format_str = '%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'
-    date_format = '%Y-%m-%d %H:%M:%S'
-    cformat = '%(log_color)s' + format_str
-    colors = {'DEBUG': 'green',
-              'INFO': 'cyan',
-              'WARNING': 'bold_yellow',
-              'ERROR': 'bold_red',
-              'CRITICAL': 'bold_purple'}
-    formatter = colorlog.ColoredFormatter(cformat, date_format,
-                                          log_colors=colors)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    log.addHandler(stream_handler)
-    return log
-
-
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
     """
     Call in a loop to create terminal progress bar
