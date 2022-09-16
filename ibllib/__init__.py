40c162c3467e76ae2d4929b147e6079527fd39d9
Miles Wells <k1o0@3tk.co>
2022-09-16 18:09:27 +0300

    Remove whitespace in version

--- ibllib/__init__.py
+++ ibllib/__init__.py
@@ -1,5 +1,5 @@
 """Library implementing the International Brain Laboratory data pipeline."""
-__version__ = "2.14.1   "
+__version__ = "2.14.1"
 import warnings
 
 from ibllib.misc import logger_config
