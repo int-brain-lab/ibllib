bdaf288a22a4001694b216be6580eca637cb9730
chris-langfield <christopher.langfield@internationalbrainlab.org>
2023-06-27 17:07:56 -0400

    last flake

--- ibllib/tests/test_io.py
+++ ibllib/tests/test_io.py
@@ -646,7 +646,7 @@ class TestRawDaqLoaders(unittest.TestCase):
         self.n_ttl = 6
         pulse_width = int(np.floor(50 * Fs))
         for i in np.arange(1, N, int(np.floor(N / self.n_ttl))):
-            a0_clean[i : i + pulse_width] = 1
+            a0_clean[i: i + pulse_width] = 1
         a0 = (
             (a0_clean * np.full(N, 5)) + np.random.rand(N) + 1
         )  # 0 -> 5V w/ noise and 1V DC offset
