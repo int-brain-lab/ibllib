48e24d9eafe97bade50bcaf0bd43acaafe6a36e8
Miles Wells <k1o0@5tk.co>
2023-07-27 19:14:52 +0300

    flake8

--- ibllib/atlas/flatmaps.py
+++ ibllib/atlas/flatmaps.py
@@ -7,7 +7,6 @@ import nrrd
 import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
-import matplotlib
 
 from iblutil.util import Bunch
 from iblutil.io.hashfile import md5
