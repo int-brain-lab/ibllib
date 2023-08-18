e4224d38b4902da06a3b42e9cbd8a7fd6ee41e3b
Miles Wells <k1o0@5tk.co>
2023-08-18 13:21:38 +0300

    Handle sessions without task data

--- ibllib/__init__.py
+++ ibllib/__init__.py
@@ -2,13 +2,12 @@
 import logging
 import warnings
 
-__version__ = '2.25.0-dev6+mesoscopeBrainLocation'
+__version__ = '2.25.0-dev7+mesoscopeBrainLocation'
 warnings.filterwarnings('always', category=DeprecationWarning, module='ibllib')
 
 # if this becomes a full-blown library we should let the logging configuration to the discretion of the dev
 # who uses the library. However since it can also be provided as an app, the end-users should be provided
-# with an useful default logging in standard output without messing with the complex python logging system
-# -*- coding:utf-8 -*-
+# with a useful default logging in standard output without messing with the complex python logging system
 USE_LOGGING = True
 #%(asctime)s,%(msecs)d
 if USE_LOGGING:
