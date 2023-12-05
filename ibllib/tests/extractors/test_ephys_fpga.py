d2294982f5c7462f16fee375c4d44ea9767fc9f1
Miles Wells <k1o0@5tk.co>
2023-12-05 14:54:49 +0200

    Remove test reference to module constant

--- ibllib/tests/extractors/test_ephys_fpga.py
+++ ibllib/tests/extractors/test_ephys_fpga.py
@@ -208,14 +208,15 @@ class TestEphysFPGA_TTLsExtraction(unittest.TestCase):
         switches under a given threshold
         """
         DISPLAY = False  # for debug purposes
-        diff = ephys_fpga.F2TTL_THRESH * np.array([0.5, 10])
+        F2TTL_THRESH = 0.01
+        diff = F2TTL_THRESH * np.array([0.5, 10])
 
         # flicker ends with a polarity switch - downgoing pulse is removed
         t = np.r_[0, np.cumsum(diff[np.array([1, 1, 0, 0, 1])])] + 1
         frame2ttl = {'times': t, 'polarities': np.mod(np.arange(t.size) + 1, 2) * 2 - 1}
         expected = {'times': np.array([1., 1.1, 1.2, 1.31]),
                     'polarities': np.array([1, -1, 1, -1])}
-        frame2ttl_ = ephys_fpga._clean_frame2ttl(frame2ttl, display=DISPLAY)
+        frame2ttl_ = ephys_fpga._clean_frame2ttl(frame2ttl, display=DISPLAY, threshold=F2TTL_THRESH)
         assert all([np.all(frame2ttl_[k] == expected[k]) for k in frame2ttl_])
 
         # stand-alone flicker
