5176c978a981a6a18b5b7796e8e23608647a4865
Miles Wells <k1o0@5tk.co>
2024-01-18 17:52:12 +0200

    Update release notes

--- ibllib/__init__.py
+++ ibllib/__init__.py
@@ -2,7 +2,7 @@
 import logging
 import warnings
 
-__version__ = '2.28'
+__version__ = '2.28.1'
 warnings.filterwarnings('always', category=DeprecationWarning, module='ibllib')
 
 # if this becomes a full-blown library we should let the logging configuration to the discretion of the dev
