c0beb868cb189d3d43e147b80dc475ad4d942f7f
chris-langfield <christopher.langfield@internationalbrainlab.org>
2024-02-08 12:28:05 -0500

    drop in ibldsp

--- examples/one/histology/coverage_map.py
+++ examples/one/histology/coverage_map.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from one.api import ONE
-from neurodsp.utils import fcn_cosine
+from ibldsp.utils import fcn_cosine
 
 import iblatlas.atlas as atlas
 from ibllib.pipes.histology import coverage
