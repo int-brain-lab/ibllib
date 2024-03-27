9dc8199dc03419e2359467d4c6f19c86b0c97668
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-03-27 13:55:23 +0000

    partial update

--- ibllib/tests/qc/test_alignment_qc.py
+++ ibllib/tests/qc/test_alignment_qc.py
@@ -200,8 +200,8 @@ class TestAlignmentQcExisting(unittest.TestCase):
                       self.alignments['2020-06-26T16:40:14_Karolina_Socha'],
                       '2020-06-12T00:39:15_nate': self.alignments['2020-06-12T00:39:15_nate']}
         trajectory = copy.deepcopy(self.trajectory)
-        trajectory.update({'json': alignments})
-        traj = one.alyx.rest('trajectories', 'update', id=self.prev_traj_id, data=trajectory)
+        trajectory.update({'probe_insertion': self.probe_id, 'json': alignments})
+        traj = one.alyx.rest('trajectories', 'partial_update', id=self.prev_traj_id, data=trajectory)
         align_qc = AlignmentQC(self.probe_id, one=one, brain_atlas=brain_atlas, channels=False)
         align_qc.load_data(prev_alignments=traj['json'], xyz_picks=np.array(self.xyz_picks) / 1e6,
                            cluster_chns=self.cluster_chns, depths=SITES_COORDINATES[:, 1],
@@ -217,8 +217,8 @@ class TestAlignmentQcExisting(unittest.TestCase):
                       self.alignments['2020-06-19T10:52:36_noam.roth'],
                       '2020-06-12T00:39:15_nate': self.alignments['2020-06-12T00:39:15_nate']}
         trajectory = copy.deepcopy(self.trajectory)
-        trajectory.update({'json': alignments})
-        traj = one.alyx.rest('trajectories', 'update', id=self.prev_traj_id, data=trajectory)
+        trajectory.update({'probe_insertion': self.probe_id, 'json': alignments})
+        traj = one.alyx.rest('trajectories', 'partial_update', id=self.prev_traj_id, data=trajectory)
         self.assertEqual(self.prev_traj_id, traj['id'])
         align_qc = AlignmentQC(self.probe_id, one=one, brain_atlas=brain_atlas, channels=False)
         align_qc.load_data(cluster_chns=self.cluster_chns, depths=SITES_COORDINATES[:, 1],
@@ -231,8 +231,8 @@ class TestAlignmentQcExisting(unittest.TestCase):
     def _05_not_latest_alignments_agree(self):
         alignments = copy.deepcopy(self.alignments)
         trajectory = copy.deepcopy(self.trajectory)
-        trajectory.update({'json': alignments})
-        traj = one.alyx.rest('trajectories', 'update', id=self.prev_traj_id, data=trajectory)
+        trajectory.update({'probe_insertion': self.probe_id, 'json': alignments})
+        traj = one.alyx.rest('trajectories', 'partial_update', id=self.prev_traj_id, data=trajectory)
         self.assertEqual(self.prev_traj_id, traj['id'])
         align_qc = AlignmentQC(self.probe_id, one=one, brain_atlas=brain_atlas, channels=False)
         align_qc.load_data(prev_alignments=traj['json'], xyz_picks=np.array(self.xyz_picks) / 1e6,
