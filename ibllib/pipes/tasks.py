30607a8eb84d512877303b19d55825a90815cb44
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-04-11 10:10:08 +0100

    allow for None collection

--- ibllib/pipes/tasks.py
+++ ibllib/pipes/tasks.py
@@ -389,7 +389,8 @@ class Task(abc.ABC):
             actual_files = list(Path(self.session_path).rglob(str(Path(*filter(None, reversed(expected_file[:2]))))))
             # Account for revisions
             if len(actual_files) == 0:
-                expected_revision = (expected_file[0], expected_file[1] + '/#*', expected_file[2])
+                collection = expected_file[1] + '/#*' if expected_file[1] != '' else expected_file[1] + '#*'
+                expected_revision = (expected_file[0], collection, expected_file[2])
                 actual_files = list(
                     Path(self.session_path).rglob(str(Path(*filter(None, reversed(expected_revision[:2]))))))
 
