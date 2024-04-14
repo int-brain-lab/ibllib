096fd4cbd4fd530b3be96c405115b41b0825930f
olivier <olivier.winter@hotmail.fr>
2024-04-14 14:39:34 +0100

    reverse the bitwise qc labels so 0 is always passin

--- brainbox/metrics/single_units.py
+++ brainbox/metrics/single_units.py
@@ -42,11 +42,12 @@ METRICS_PARAMS = {
     'missed_spikes_est': dict(spks_per_bin=10, sigma=4, min_num_bins=50),
     'acceptable_contamination': 0.1,
     'bin_size': 0.25,
-    'med_amp_thresh_uv': 50,
+    'med_amp_thresh_uv': 50,  # units below this threshold are considered noise
     'min_isi': 0.0001,
     'presence_window': 10,
     'refractory_period': 0.0015,
     'RPslide_thresh': 0.1,
+    'RPmax_confidence': 90,  # a unit needs to pass with at least this confidence percentage (0 - 100)
 }
 
 
@@ -1008,39 +1009,35 @@ def quick_unit_metrics(spike_clusters, spike_times, spike_amps, spike_depths,
         r.missed_spikes_est[ic], _, _ = missed_spikes_est(amps, **params['missed_spikes_est'])
         # wonder if there is a need to low-cut this
         r.drift[ic] = np.sum(np.abs(np.diff(depths))) / (tmax - tmin) * 3600
-
-    r.label = compute_labels(r)
+    r.label, r.bitwise_labels = compute_labels(r, return_bitwise=True)
     return r
 
 
-def compute_labels(r, params=METRICS_PARAMS, return_details=False):
+def compute_labels(r, params=METRICS_PARAMS, return_bitwise=False):
     """
-    From a dataframe or a dictionary of unit metrics, compute a lablel
+    From a dataframe or a dictionary of unit metrics, compute a label
     :param r: dictionary or pandas dataframe containing unit qcs
-    :param return_details: False (returns a full dictionary of metrics)
+    :param return_bitwise: True (returns a full dictionary of metrics)
     :return: vector of proportion of qcs passed between 0 and 1, where 1 denotes an all pass
     """
-    # right now the score is a value between 0 and 1 denoting the proportion of passing qcs
+    # right now the score is a value between 0 and 1 denoting the proportion of passing qcs,
+    # where 1 means passing and 0 means failing
     labels = np.c_[
-        r.slidingRP_viol,
+        r['max_confidence'] >= params['RPmax_confidence'],
         r.noise_cutoff < params['noise_cutoff']['nc_threshold'],
         r.amp_median > params['med_amp_thresh_uv'] / 1e6,
     ]
-
-    # The first column takes binary values 000 or 001 to represent fail or pass,
+    # The first column takes binary values 001 or 000 to represent fail or pass,
     # the second, 010 or 000, the third, 100 or 000 etc.
-    # The bitwise or "sum" produces 111 if all metrics pass, or 000 if all metrics fail
+    # The bitwise or "sum" produces 111 if all metrics fail, or 000 if all metrics pass
     # All other permutations are also captured, i.e. 110 ==  000 || 010 || 100 means
-    # the second and third metrics passed but the first metric was a fail
-    n_criteria = labels.shape[1]
-    powers2 = np.array([2**i for i in range(n_criteria)]).T
-    if not return_details:
+    # the second and third metrics failed and the first metric was a pass
+    score = np.mean(labels, axis=1)
+    if return_bitwise:
         # note the cast to uint8 casts nan to 0
         # a nan implies no metrics was computed which we mark as a failure here
-        return np.bitwise_or.reduce(powers2 * labels.astype(np.uint8), axis=1)
-
-    column_names = ['slidingRP_viol', 'noise_cutoff', 'amp_median']
-    qcdict = {}
-    for c in np.arange(labels.shape[1]):
-        qcdict[column_names[c]] = labels[:, c]
-    return labels, qcdict
+        n_criteria = labels.shape[1]
+        bitwise = np.bitwise_or.reduce(2 ** np.arange(n_criteria) * (~ labels.astype(bool)).astype(np.uint8), axis=1)
+        return score, bitwise
+    else:
+        return score
