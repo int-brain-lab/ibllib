04ef5bc1a5101e17164e81ea2d1d31be0c5e7c84
Florian Rau <bimac@users.noreply.github.com>
2024-10-04 11:11:33 +0100

    sort dataframe, store UI settings

--- ibllib/qc/task_qc_viewer/ViewEphysQC.py
+++ ibllib/qc/task_qc_viewer/ViewEphysQC.py
@@ -3,7 +3,16 @@
 import logging
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt, QModelIndex, QPoint, pyqtSignal, pyqtSlot, QCoreApplication, QSettings
+from PyQt5.QtCore import (
+    Qt,
+    QModelIndex,
+    pyqtSignal,
+    pyqtSlot,
+    QCoreApplication,
+    QSettings,
+    QSize,
+    QPoint,
+)
 from PyQt5.QtGui import QPalette, QShowEvent
 from PyQt5.QtWidgets import QMenu, QAction
 from iblqt.core import ColoredDataFrameTableModel
@@ -24,14 +33,10 @@ class PlotCanvas(FigureCanvasQTAgg):
         FigureCanvasQTAgg.__init__(self, fig)
         self.setParent(parent)
 
-        FigureCanvasQTAgg.setSizePolicy(
-            self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
-        )
+        FigureCanvasQTAgg.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         FigureCanvasQTAgg.updateGeometry(self)
         if wheel:
-            self.ax, self.ax2 = fig.subplots(
-                2, 1, gridspec_kw={"height_ratios": [2, 1]}, sharex=True
-            )
+            self.ax, self.ax2 = fig.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]}, sharex=True)
         else:
             self.ax = fig.add_subplot(111)
         self.draw()
@@ -53,12 +58,10 @@ class GraphWindow(QtWidgets.QWidget):
     def __init__(self, parent=None, wheel=None):
         QtWidgets.QWidget.__init__(self, parent=parent)
 
-        # Store layout changes to QSettings
-        self.settings = QSettings()
-
         self.columnPinned = pyqtSignal(int, bool)
 
-        self.pushButtonLoad = QtWidgets.QPushButton("Select File", self)
+        # load button
+        self.pushButtonLoad = QtWidgets.QPushButton('Select File', self)
         self.pushButtonLoad.clicked.connect(self.loadFile)
 
         # define table model & view
@@ -70,6 +73,7 @@ class GraphWindow(QtWidgets.QWidget):
         self.tableView.horizontalHeader().setSectionsMovable(True)
         self.tableView.horizontalHeader().setContextMenuPolicy(Qt.CustomContextMenu)
         self.tableView.horizontalHeader().customContextMenuRequested.connect(self.contextMenu)
+        self.tableView.verticalHeader().hide()
         self.tableView.doubleClicked.connect(self.tv_double_clicked)
 
         # define colors for highlighted cells
@@ -98,6 +102,7 @@ class GraphWindow(QtWidgets.QWidget):
 
         # slider for alpha values
         self.sliderAlpha = QtWidgets.QSlider(Qt.Horizontal, self)
+        self.sliderAlpha.setMaximumWidth(100)
         self.sliderAlpha.setMinimum(0)
         self.sliderAlpha.setMaximum(255)
         self.sliderAlpha.setValue(self.tableModel.alpha)
@@ -111,7 +116,7 @@ class GraphWindow(QtWidgets.QWidget):
         hLayout.addWidget(self.comboboxColormap)
         hLayout.addWidget(QtWidgets.QLabel('Alpha', self))
         hLayout.addWidget(self.sliderAlpha)
-        hLayout.addStretch(1)
+        hLayout.addSpacing(50)
         hLayout.addWidget(self.pushButtonLoad)
 
         # Vertical layout
@@ -119,13 +124,27 @@ class GraphWindow(QtWidgets.QWidget):
         vLayout.addLayout(hLayout)
         vLayout.addWidget(self.tableView)
 
-        self.setMinimumSize(500, 400)
+        # Recover layout from QSettings
+        self.settings = QSettings()
+        self.settings.beginGroup('MainWindow')
+        self.resize(self.settings.value('size', QSize(800, 600), QSize))
+        self.comboboxColormap.setCurrentText(self.settings.value('colormap', 'plasma', str))
+        self.sliderAlpha.setValue(self.settings.value('alpha', 255, int))
+        self.settings.endGroup()
 
         self.wplot = PlotWindow(wheel=wheel)
         self.wplot.show()
         self.tableModel.dataChanged.connect(self.wplot.canvas.draw)
 
         self.wheel = wheel
+
+    def closeEvent(self, _) -> bool:
+        self.settings.beginGroup('MainWindow')
+        self.settings.setValue('size', self.size())
+        self.settings.setValue('colormap', self.tableModel.colormap)
+        self.settings.setValue('alpha', self.tableModel.alpha)
+        self.settings.endGroup()
+        self.wplot.close()
 
     def showEvent(self, a0: QShowEvent) -> None:
         super().showEvent(a0)
@@ -151,8 +170,10 @@ class GraphWindow(QtWidgets.QWidget):
         self.changeFilter(self.lineEditFilter.text())
 
     def changeFilter(self, string: str):
-        headers = [self.tableModel.headerData(x, Qt.Horizontal, Qt.DisplayRole).value().lower()
-                   for x in range(self.tableModel.columnCount())]
+        headers = [
+            self.tableModel.headerData(x, Qt.Horizontal, Qt.DisplayRole).value().lower()
+            for x in range(self.tableModel.columnCount())
+        ]
         tokens = [y.lower() for y in (x.strip() for x in string.split(',')) if len(y)]
         showAll = len(tokens) == 0
         for idx, column in enumerate(headers):
@@ -160,27 +181,53 @@ class GraphWindow(QtWidgets.QWidget):
             self.tableView.setColumnHidden(idx, not show)
 
     def loadFile(self):
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "Open File", "", "CSV Files (*.csv)"
-        )
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', '', 'CSV Files (*.csv)')
         if len(fileName) == 0:
             return
         df = pd.read_csv(fileName)
         self.updateDataframe(df)
 
-    def updateDataframe(self, dataFrame: pd.DataFrame):
-        self.tableModel.setDataFrame(dataFrame)
+    def updateDataframe(self, df: pd.DataFrame):
+        # clear pinned columns
+        self._pinnedColumns = []
+
+        # try to identify and sort columns containing timestamps
+        col_names = df.columns
+        df_interp = df.replace([-np.inf, np.inf], np.nan)
+        df_interp = df_interp.interpolate(limit_direction='both')
+        cols_mono = col_names[[df_interp[c].is_monotonic_increasing for c in col_names]]
+        cols_mono = [c for c in cols_mono if df[c].nunique() > 1]
+        cols_mono = df_interp[cols_mono].mean().sort_values().keys()
+        for idx, col_name in enumerate(cols_mono):
+            df.insert(idx, col_name, df.pop(col_name))
+
+        # columns containing boolean values are sorted to the end
+        cols_bool = list(df.select_dtypes('bool').columns)
+        cols_pass = [cols_bool.pop(i) for i, c in enumerate(cols_bool) if 'pass' in c]
+        cols_bool += cols_pass
+        for col_name in cols_bool:
+            df = df.join(df.pop(col_name))
+
+        # trial_no should always be the first column
+        if 'trial_no' in col_names:
+            df.insert(0, 'trial_no', df.pop('trial_no'))
+
+        # define columns that should be pinned by default
+        for col in ['trial_no']:
+            self._pinnedColumns.append(df.columns.get_loc(col))
+
+        self.tableModel.setDataFrame(df)
 
     def tv_double_clicked(self, index: QModelIndex):
         data = self.tableModel.dataFrame.iloc[index.row()]
-        t0 = data["intervals_0"]
-        t1 = data["intervals_1"]
+        t0 = data['intervals_0']
+        t1 = data['intervals_1']
         dt = t1 - t0
         if self.wheel:
-            idx = np.searchsorted(self.wheel["re_ts"], np.array([t0 - dt / 10, t1 + dt / 10]))
-            period = self.wheel["re_pos"][idx[0] : idx[1]]
+            idx = np.searchsorted(self.wheel['re_ts'], np.array([t0 - dt / 10, t1 + dt / 10]))
+            period = self.wheel['re_pos'][idx[0] : idx[1]]
             if period.size == 0:
-                _logger.warning("No wheel data during trial #%i", index.row())
+                _logger.warning('No wheel data during trial #%i', index.row())
             else:
                 min_val, max_val = np.min(period), np.max(period)
                 self.wplot.canvas.ax2.set_ylim(min_val - 1, max_val + 1)
@@ -191,10 +238,11 @@ class GraphWindow(QtWidgets.QWidget):
 
 
 def viewqc(qc=None, title=None, wheel=None):
+    app = qt.create_app()
+    app.setStyle('Fusion')
     QCoreApplication.setOrganizationName('International Brain Laboratory')
     QCoreApplication.setOrganizationDomain('internationalbrainlab.org')
     QCoreApplication.setApplicationName('QC Viewer')
-    qt.create_app()
     qcw = GraphWindow(wheel=wheel)
     qcw.setWindowTitle(title)
     if qc is not None:
