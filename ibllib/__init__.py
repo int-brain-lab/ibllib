2365a7ce0936ab48796aeece1ce3b47a705d56e6
k1o0 <k1o0@5tk.co>
2024-10-08 16:21:19 +0300

    iterate only plane folders in suite2p dir (#860)

--- ibllib/__init__.py
+++ ibllib/__init__.py
@@ -2,7 +2,7 @@
 import logging
 import warnings
 
-__version__ = '2.38.0'
+__version__ = '2.39.rc1'
 warnings.filterwarnings('always', category=DeprecationWarning, module='ibllib')
 
 # if this becomes a full-blown library we should let the logging configuration to the discretion of the dev
