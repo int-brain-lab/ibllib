1f86801d8ea28e03d303ca20e47560a63b4b69f4
Georg Raiser <grg2rsr@gmail.com>
2025-09-29 17:38:00 +0100

    bugfix: attempted daq sync for passive sessions in either case

--- ibllib/pipes/dynamic_pipeline.py
+++ ibllib/pipes/dynamic_pipeline.py
@@ -613,9 +613,10 @@ def make_pipeline(session_path, **pkwargs):
             case 'daqami':
                 # for synchronization with the DAQami receiving the sync pulses from the individual bpods
                 # as well as the frame clock from the FP3002
-                tasks['FibrePhotometryDAQSync'] = type('FibrePhotometryDAQSync', (ptasks.FibrePhotometryDAQSync,), {})(
-                    **kwargs,
-                )
+                if 'passive' not in protocol: # excluding passive session
+                    tasks['FibrePhotometryDAQSync'] = type('FibrePhotometryDAQSync', (ptasks.FibrePhotometryDAQSync,), {})(
+                        **kwargs,
+                    )
 
     p = mtasks.Pipeline(session_path=session_path, **pkwargs)
     p.tasks = tasks
