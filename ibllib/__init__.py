83b66c5c7c378eab0297cf7c300cac08f8271d80
Miles Wells <k1o0@3tk.co>
2025-10-03 14:43:56 +0300

    Bump version

--- ibllib/__init__.py
+++ ibllib/__init__.py
@@ -2,7 +2,7 @@
 import logging
 import warnings
 
-__version__ = '3.5rc20'
+__version__ = '3.5rc21'
 warnings.filterwarnings('always', category=DeprecationWarning, module='ibllib')
 
 # if this becomes a full-blown library we should let the logging configuration to the discretion of the dev
