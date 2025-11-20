a9a638c207dc21f836f4e019e504894149645240
Olivier Winter <olivier.winter@hotmail.fr>
2025-11-20 09:33:02 +0000

    spikesorting: exclude pngs from task output

--- ibllib/pipes/ephys_tasks.py
+++ ibllib/pipes/ephys_tasks.py
@@ -791,7 +791,8 @@ class SpikeSorting(base_tasks.EphysTask, CellQCMixin):
         if logfile.exists():
             shutil.copyfile(logfile, probe_out_path.joinpath(f"_ibl_log.info_{self._sortername}.log"))
         # recover the QC files from the spike sorting output and copy them
-        for file_qc in sorter_dir.glob('_iblqc_*.*'):
+        qc_files = list(sorter_dir.glob('_iblqc_*.*.npy')) + list(sorter_dir.glob('_iblqc_*.*.pqt'))
+        for file_qc in qc_files:
             shutil.move(file_qc, file_qc_out := ap_file.parent.joinpath(file_qc.name))
             out_files.append(file_qc_out)
         # Sync spike sorting with the main behaviour clock: the nidq for 3B+ and the main probe for 3A
